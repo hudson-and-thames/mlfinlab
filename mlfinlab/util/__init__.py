7dc8b655b9ff622d416384855ac68456d5696017
jackal08 <jacques@quantsportal.com>
2019-04-18 15:38:23 +0100

    * Update imports

--- mlfinlab/util/__init__.py
+++ mlfinlab/util/__init__.py
@@ -5,3 +5,4 @@ Utility functions. In particular Chapter20 code on Multiprocessing and Vectoriza
 from mlfinlab.util.fast_ewma import ewma
 from mlfinlab.util.multiprocess import expand_call, lin_parts, mp_pandas_obj, nested_parts, process_jobs, process_jobs_, \
     report_progress
+from mlfinlab.util.utils import get_daily_vol
