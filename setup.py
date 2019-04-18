c3ddcc5fd0587d94406a72de9f728ea3612e1e6c
jackal08 <jacques@quantsportal.com>
2019-04-18 14:50:59 +0100

    * Update __init__ files with imports * Added Docstings * Updated numpy and pandas versions * bumped version for test PyPi

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.6',  # Required
+    version='0.1.7',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
