cad5acd27d19328504b32ba3e9fd12dac7de4858
proskurin <proskurinolexandr@gmail.com>
2019-05-17 18:17:10 +0300

    First forest fit without errors (need to debug the results)

--- mlfinlab/ensemble/forest.py
+++ mlfinlab/ensemble/forest.py
@@ -528,7 +528,46 @@ class SequentialForestRegressor(SequentialBaseForest, RegressorMixin, metaclass=
             warm_start=warm_start)
 
     def predict(self, X):
-        super().predict(X)
+        """Predict regression target for X.
+
+        The predicted regression target of an input sample is computed as the
+        mean predicted regression targets of the trees in the forest.
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The input samples. Internally, its dtype will be converted to
+            ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csr_matrix``.
+
+        Returns
+        -------
+        y : array of shape = [n_samples] or [n_samples, n_outputs]
+            The predicted values.
+        """
+        check_is_fitted(self, 'estimators_')
+        # Check data
+        X = self._validate_X_predict(X)
+
+        # Assign chunk of trees to jobs
+        n_jobs, _, _ = _partition_estimators(self.n_estimators, self.n_jobs)
+
+        # avoid storing the output of every estimator by summing them here
+        if self.n_outputs_ > 1:
+            y_hat = np.zeros((X.shape[0], self.n_outputs_), dtype=np.float64)
+        else:
+            y_hat = np.zeros((X.shape[0]), dtype=np.float64)
+
+        # Parallel loop
+        lock = threading.Lock()
+        Parallel(n_jobs=n_jobs, verbose=self.verbose,
+                 **_joblib_parallel_args(require="sharedmem"))(
+            delayed(_accumulate_prediction)(e.predict, X, [y_hat], lock)
+            for e in self.estimators_)
+
+        y_hat /= len(self.estimators_)
+
+        return y_hat
 
     def _set_oob_score(self, X, y, triple_barrier_events):
         """Compute out-of-bag scores"""
