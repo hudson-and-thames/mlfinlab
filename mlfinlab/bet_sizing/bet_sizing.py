d664002fe427f382216d60c2f6abb999626bb29e
jackal08 <jacques@quantsportal.com>
2019-07-10 17:18:03 +0100

    * comments to fit 160 line margin

--- mlfinlab/bet_sizing/bet_sizing.py
+++ mlfinlab/bet_sizing/bet_sizing.py
@@ -1,33 +1,24 @@
 """
-This module contains functionality for determining bet sizes for
-investments based on machine learning predictions. These implementations are
-based on bet sizing approaches described in Chapter 10.
+This module contains functionality for determining bet sizes for investments based on machine learning predictions.
+These implementations are based on bet sizing approaches described in Chapter 10.
 """
 
-
-# imports
 from mlfinlab.bet_sizing.ch10_snippets import get_signal, avg_active_signals, discrete_signal
 
 
-def bet_size_probability(events, prob, num_classes, pred=None, step_size=0.0,
-                         average_active=False, num_threads=1):
+def bet_size_probability(events, prob, num_classes, pred=None, step_size=0.0, average_active=False, num_threads=1):
     """
-    Calculates the bet size using the predicted probability. Note that if
-    'average_active' is True, the returned pandas.Series will be twice
-    the length of the orginal since the average is calculated at each
-    bet's open and close.
+    Calculates the bet size using the predicted probability. Note that if 'average_active' is True, the returned
+    pandas.Series will be twice the length of the original since the average is calculated at each bet's open and close.
 
-    :param prob: (pandas.Series) The predicted probabiility.
+    :param prob: (pandas.Series) The predicted probability.
     :param num_classes: (int) The number of predicted bet sides.
-    :param pred: (pd.Series) The predicted bet side. Default value is None
-        which will return a relative bet size (i.e. without multiplying
-        by the side).
-    :param step_size: (float) The step size at which the bet size is
-        discretized, default is 0.0 which imposes no discretization.
-    :param average_active: (bool) Option to average the size of active bets,
-        default value is False.
-    :param num_threads: (int) The number of processing threads to utilize for
-        multiprocessing, default value is 1.
+    :param pred: (pd.Series) The predicted bet side. Default value is None which will return a relative bet size
+     (i.e. without multiplying by the side).
+    :param step_size: (float) The step size at which the bet size is discretized, default is 0.0 which imposes no
+     discretization.
+    :param average_active: (bool) Option to average the size of active bets, default value is False.
+    :param num_threads: (int) The number of processing threads to utilize for multiprocessing, default value is 1.
     :return: (pandas.Series) The bet size, with the time index.
     """
     signal_0 = get_signal(prob, num_classes, pred)
--- mlfinlab/bet_sizing/ch10_snippets.py
+++ mlfinlab/bet_sizing/ch10_snippets.py
@@ -1,14 +1,10 @@
 """
-This module contains the code snippets found in Chapter 10 of
-"Advances in Financial Machine Learning" by Marcos López de Prado.
-The code has been ammended for readability, to conform to PEP8
-rules, to keep the snippets as manageable single-units of
-functionality, as well as to account for depracation of functions
-originally used, but is otherwise unaltered.
+This module contains the code snippets found in Chapter 10 of "Advances in Financial Machine Learning" by
+Marcos López de Prado. The code has been amended for readability, to conform to PEP8 rules, to keep the snippets as
+manageable single-units of functionality, as well as to account for deprecation of functions originally used, but is
+otherwise unaltered.
 """
 
-
-# imports
 import pandas as pd
 from scipy.stats import norm
 from mlfinlab.util.multiprocess import mp_pandas_obj
@@ -17,16 +13,13 @@ from mlfinlab.util.multiprocess import mp_pandas_obj
 def get_signal(prob, num_classes, pred=None):
     """
     SNIPPET 10.1 - FROM PROBABILITIES TO BET SIZE
-    Calculates the given size of the bet given the side and the
-    probability (i.e. confidence) of the prediction. In this
-    representation, the probability will always be between
-    1/num_classes and 1.0.
+    Calculates the given size of the bet given the side and the probability (i.e. confidence) of the prediction. In this
+    representation, the probability will always be between 1/num_classes and 1.0.
 
     :param prob: (pd.Series) The probability of the predicted bet side.
     :param num_classes: (int) The number of predicted bet sides.
-    :param pred: (pd.Series) The predicted bet side. Default value is None
-        which will return a relative bet size (i.e. without multiplying
-        by the side).
+    :param pred: (pd.Series) The predicted bet side. Default value is None which will return a relative bet size
+     (i.e. without multiplying by the side).
     :return: (pd.Series) The bet size.
     """
     # Get signals from predictions.
@@ -43,15 +36,13 @@ def get_signal(prob, num_classes, pred=None):
         # signal = size only
         signal0 = signal0.apply(lambda s: 2 * norm.cdf(s) - 1)
 
-    # Note 1: In the book, this function contains a conditional
-    # statment checking for a column named 'side', then executes
-    # what is essentially the above line. This has been removed
-    # as it appears to be redundant and simplifies the function.
+    # Note 1: In the book, this function contains a conditional statement checking for a column named 'side',
+    # then executes what is essentially the above line. This has been removed as it appears to be redundant
+    # and simplifies the function.
 
-    # Note 2: In the book, this function includes the averaging and
-    # discretization steps, which are omitted here. The functions
-    # for performing these are included in this file, and can be
-    # applied as options in the user-level functions in bet_sizing.py.
+    # Note 2: In the book, this function includes the averaging and discretization steps, which are omitted here.
+    # The functions for performing these are included in this file, and can be applied as options in the user-level
+    # functions in bet_sizing.py.
 
     return signal0
 
@@ -59,17 +50,16 @@ def get_signal(prob, num_classes, pred=None):
 def avg_active_signals(signals, num_threads=1):
     """
     SNIPPET 10.2 - BETS ARE AVERAGED AS LONG AS THEY ARE STILL ACTIVE
-    Function averages the bet sizes of all concurrently active bets.
-    This function makes use of multiprocessing.
+    Function averages the bet sizes of all concurrently active bets. This function makes use of multiprocessing.
 
     :param signals: (pandas.DataFrame) Contains at least the following
         columns:
             'signal' - the bet size
             't1' - the closing time of the bet
-    :param num_threads: (int) Number of threads to use in multiprocessing,
-        default value is 1.
+    :param num_threads: (int) Number of threads to use in multiprocessing, default value is 1.
     :return: (pandas.Series) The averaged bet sizes.
     """
+
     # 1) Time points where signals change (either one start or one ends).
     t_pnts = set(signals['t1'].dropna().to_numpy())
     t_pnts = t_pnts.union(signals.index.to_numpy())
@@ -83,8 +73,7 @@ def avg_active_signals(signals, num_threads=1):
 def mp_avg_active_signals(signals, molecule):
     """
     Part of SNIPPET 10.2
-    A function to be passed to the 'mp_pandas_obj' function to allow the
-    bet sizes to be averaged using multiprocessing.
+    A function to be passed to the 'mp_pandas_obj' function to allow the bet sizes to be averaged using multiprocessing.
 
     At time loc, average signal among those still active.
     Signal is active if:
@@ -96,8 +85,7 @@ def mp_avg_active_signals(signals, molecule):
         columns:
             'signal' - the bet size
             't1' - the closing time of the bet
-    :param molecule: (list) Indivisible tasks to be passed to 'mp_pandas_obj',
-        in this case a list of datetimes.
+    :param molecule: (list) Indivisible tasks to be passed to 'mp_pandas_obj', in this case a list of datetimes.
     :return: (pandas.Series) The averaged bet size sub-series.
     """
     out = pd.Series()
@@ -124,6 +112,6 @@ def discrete_signal(signal0, step_size):
     :return: (pandas.Series) The discretized signal.
     """
     signal1 = (signal0 / step_size).round() * step_size
-    signal1[signal1 > 1] = 1  # cap
-    signal1[signal1 < -1] = -1  # floor
+    signal1[signal1 > 1] = 1  # Cap
+    signal1[signal1 < -1] = -1  # Floor
     return signal1
