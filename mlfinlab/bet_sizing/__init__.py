7c12254a95ef136f32c68292a00239094b747338
jackal08 <jacques@quantsportal.com>
2019-07-10 17:21:51 +0100

    * Expose functions

--- mlfinlab/bet_sizing/__init__.py
+++ mlfinlab/bet_sizing/__init__.py
@@ -3,3 +3,4 @@ Functions derived from Chapter 10: Bet Sizing
 """
 
 from mlfinlab.bet_sizing.bet_sizing import bet_size_probability
+from mlfinlab.bet_sizing.ch10_snippets import get_signal, avg_active_signals, mp_avg_active_signals, discrete_signal
