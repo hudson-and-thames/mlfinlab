4200e1b33efb8156db642ca5cb067a6e06827629
proskurin <proskurinolexandr@gmail.com>
2019-07-26 15:06:59 +0300

    First successful run of SB sample

--- mlfinlab/ensemble/sequential_bootstrap_resampler.py
+++ mlfinlab/ensemble/sequential_bootstrap_resampler.py
@@ -2,6 +2,10 @@ from imblearn.under_sampling.base import BaseUnderSampler
 from sklearn.utils import check_random_state
 from sklearn.utils import safe_indexing
 from ..sampling.bootstrapping import get_ind_matrix, seq_bootstrap
+from sklearn.utils.multiclass import check_classification_targets
+from imblearn.utils import check_sampling_strategy
+from sklearn.preprocessing import label_binarize
+import numpy as np
 
 
 class SequentialBootstrappingSampler(BaseUnderSampler):
@@ -14,14 +18,32 @@ class SequentialBootstrappingSampler(BaseUnderSampler):
             sampling_strategy=sampling_strategy, ratio=ratio)
         self.random_state = random_state
         self.return_indices = return_indices
+        self.sample_indices_ = None
 
-    def _fit_resample(self, X, triple_barrier_events, dollar_bars):
-        y = triple_barrier_events.bin
+    def fit_resample(self, X, triple_barrier_events):
+        y = triple_barrier_events.side.values
+        check_classification_targets(y)
+        X, y, binarize_y = self._check_X_y(X, y)
+
+        self.sampling_strategy_ = check_sampling_strategy(
+            self.sampling_strategy, y, self._sampling_type)
+
+        output = self._fit_resample(X, triple_barrier_events)
+
+        if binarize_y:
+            y_sampled = label_binarize(output[1], np.unique(y))
+            if len(output) == 2:
+                return output[0], y_sampled
+            return output[0], y_sampled, output[2]
+        return output
+
+    def _fit_resample(self, X, triple_barrier_events):
+        y = triple_barrier_events.side.values
         random_state = check_random_state(self.random_state)
         ind_matrix = get_ind_matrix(triple_barrier_events)
-        num_samples_to_draw = X.shape * self.ratio
+        num_samples_to_draw = None
         bootstrapped_samples_idx = seq_bootstrap(ind_matrix, sample_length=num_samples_to_draw,
-                                                 random_state=random_state)
+                                                 )
 
         self.sample_indices_ = bootstrapped_samples_idx
 
