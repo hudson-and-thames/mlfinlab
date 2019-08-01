179801c1b7c24bbe19fab711707b44ab90df485a
lmfischer <lee.m.fischer@gmail.com>
2019-08-01 21:43:13 +0200

    implemented bet_size_budget function

--- mlfinlab/bet_sizing/__init__.py
+++ mlfinlab/bet_sizing/__init__.py
@@ -2,6 +2,6 @@
 Functions derived from Chapter 10: Bet Sizing
 """
 
-from mlfinlab.bet_sizing.bet_sizing import bet_size_probability, bet_size_dynamic
+from mlfinlab.bet_sizing.bet_sizing import bet_size_probability, bet_size_dynamic, get_concurrent_sides, bet_size_budget
 from mlfinlab.bet_sizing.ch10_snippets import get_signal, avg_active_signals, mp_avg_active_signals, discrete_signal
 from mlfinlab.bet_sizing.ef3m import M2N, centered_moment, raw_moment, most_likely_parameters
