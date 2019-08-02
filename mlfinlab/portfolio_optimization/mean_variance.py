ca01df4b4bb4d38e14142827e911a9eac079e65e
Aditya Vyas <adityavyas17@gmail.com>
2019-08-02 09:00:23 -0400

    add MeanVarianceOptimisation class

---
+++ mlfinlab/portfolio_optimization/mean_variance.py
@@ -0,0 +1,16 @@
+class MeanVarianceOptimisation:
+
+    def __init__(self):
+        return
+
+    def allocate(self):
+        return
+
+    def _inverse_variance(self):
+        return
+
+    def _min_volatility(self):
+        return
+
+    def _max_sharpe(self):
+        return
