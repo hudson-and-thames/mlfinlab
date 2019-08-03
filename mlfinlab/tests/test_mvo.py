2e8fcb6e76642820d16e107460d47213f910d155
Aditya Vyas <adityavyas17@gmail.com>
2019-08-03 17:55:18 -0400

    add test for inverse_variance

---
+++ mlfinlab/tests/test_mvo.py
@@ -0,0 +1,27 @@
+"""
+Tests for the Mean Variance Optimisation module
+"""
+
+import unittest
+import os
+import numpy as np
+import pandas as pd
+from mlfinlab.portfolio_optimization.mean_variance import MeanVarianceOptimisation
+
+
+class TestMVO(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set the file path for the tick data csv
+        """
+        project_path = os.path.dirname(__file__)
+        data_path = project_path + '/test_data/stock_prices.csv'
+        self.data = pd.read_csv(data_path, parse_dates = True, index_col = "date")
+
+    def test_inverse_variance(self):
+        mvo = MeanVarianceOptimisation()
+        mvo.allocate(asset_prices = self.data)
+        assert len(mvo.weights) > 0
+        assert len(mvo.weights) == self.data.shape[1]
+        np.testing.assert_almost_equal(np.sum(mvo.weights), 1)
