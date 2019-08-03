1f7762b430fcb157e12d79b18bbac1c5918a02ec
Aditya Vyas <adityavyas17@gmail.com>
2019-08-03 17:37:12 -0400

    add IVP algorithm

--- mlfinlab/portfolio_optimization/mean_variance.py
+++ mlfinlab/portfolio_optimization/mean_variance.py
@@ -1,13 +1,38 @@
+import numpy as np
+import pandas as pd
+
+
 class MeanVarianceOptimisation:
 
     def __init__(self):
         return
 
-    def allocate(self):
-        return
+    def allocate(self, asset_prices, solution='inverse_variance'):
+        '''
 
-    def _inverse_variance(self):
-        return
+        :param asset_prices:
+        :param solution:
+        :return:
+        '''
+
+        if not isinstance(asset_prices, pd.DataFrame):
+            asset_prices = pd.DataFrame(asset_prices)
+
+        self.weights = []
+        if solution == 'inverse_variance':
+            self.weights = self._inverse_variance(asset_prices=asset_prices)
+
+    def _inverse_variance(self, asset_prices):
+        '''
+
+        :param asset_prices:
+        :return:
+        '''
+
+        cov = asset_prices.cov()
+        ivp = 1. / np.diag(cov)
+        ivp /= ivp.sum()
+        return ivp
 
     def _min_volatility(self):
         return
