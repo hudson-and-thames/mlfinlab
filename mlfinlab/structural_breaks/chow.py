08acf710ea495833210272c64c553e78bdd319c9
proskurin <proskurinolexandr@gmail.com>
2019-10-16 12:18:00 +0300

    Add Chow-Type test

---
+++ mlfinlab/structural_breaks/chow.py
@@ -0,0 +1,41 @@
+"""
+Implementation of Chow-Type Dickey-Fuller Test
+"""
+
+import pandas as pd
+from mlfinlab.structural_breaks.sadf import _get_betas
+from mlfinlab.util import mp_pandas_obj
+
+
+def _get_dfc_for_t(df, molecule):
+    """
+    Get Chow-Type Dickey-Fuller Test statistics for each index in molecule
+    """
+
+    dfc_series = pd.Series(index=molecule)
+
+    for index in molecule:
+        df_diff = df.diff().dropna()
+        df_shifted_diff = df.shift(1).dropna()
+        df_shifted_diff[:index] = 0  # D_t* indicator: before t* D_t* = 0
+
+        y = df_diff.loc[df_shifted_diff.index].values
+        x = df_shifted_diff.values
+        coefs, coef_vars = _get_betas(y, x.reshape(-1, 1))
+        b_estimate, b_var = coefs[0], coef_vars[0][0]
+        dfc_series[index] = b_estimate / b_var
+    return dfc_series
+
+
+def get_chow_type_stat(df, min_length, num_threads=8):
+    """
+    Multithread implementation of Chow-Type Dickey-Fuller Test
+    """
+    molecule = df.index[
+               min_length:df.shape[0] - min_length]  # Indices to test. We drop min_length first and last values
+    dfc_series = mp_pandas_obj(func=_get_dfc_for_t,
+                               pd_obj=('molecule', molecule),
+                               df=df,
+                               num_threads=num_threads,
+                               )
+    return dfc_series
--- mlfinlab/structural_breaks/sadf.py
+++ mlfinlab/structural_breaks/sadf.py
@@ -2,28 +2,28 @@
 Explosivness tests: SADF
 """
 
+import pandas as pd
 import numpy as np
 from mlfinlab.util.multiprocess import mp_pandas_obj
 
 
-import numpy as np
-
-def _get_bsadf_at_t(log_prices, min_length, constant, lags):
+def _get_sadf_at_t(log_prices, min_length, constant, lags):
     """
     Snippet 17.2, page 258. SADF's Inner Loop (get sadf value at t)
     """
     log_prices_df = pd.DataFrame(log_prices)
     y, x = _get_y_x(log_prices_df, constant=constant, lags=lags)
-    start_points, bsadf, all_adf = range(0, y.shape[0]+lags-min_length+1), -np.inf, []
+    start_points, bsadf, all_adf = range(0, y.shape[0] + lags - min_length + 1), -np.inf, []
     for start in start_points:
         y_, x_ = y[start:], x[start:]
         b_mean_, b_std_ = _get_betas(y_, x_)
-        b_mean_, b_std_ = b_mean_[0, 0], b_std_[0, 0]**0.5
-        all_adf.append(b_mean_/b_std_)
+        b_mean_, b_std_ = b_mean_[0, 0], b_std_[0, 0] ** 0.5
+        all_adf.append(b_mean_ / b_std_)
         if all_adf[-1] > bsadf:
             bsadf = all_adf[-1]
     out = log_prices.index[-1], bsadf
     return out
+
 
 def _get_y_x(series, constant, lags):
     """
@@ -31,7 +31,7 @@ def _get_y_x(series, constant, lags):
     """
     series_diff = series.diff().dropna()
     x = _lag_df(series_diff, lags).dropna()
-    x.iloc[:, 0] = series.values[-x.shape[0] - 1:-1, 0] # lagged level
+    x.iloc[:, 0] = series.values[-x.shape[0] - 1:-1, 0]  # lagged level
     y = series_diff.iloc[-x.shape[0]:].values
 
     if constant != 'const':
@@ -40,8 +40,9 @@ def _get_y_x(series, constant, lags):
             trend = np.arange(x.shape[0]).reshape(-1, 1)
             x = np.append(x, trend, axis=1)
         if constant == 'quadratic':
-            x = np.append(x, trend**2, axis=1)
+            x = np.append(x, trend ** 2, axis=1)
     return y, x
+
 
 def _lag_df(df, lags):
     """
@@ -49,52 +50,53 @@ def _lag_df(df, lags):
     """
     df_lagged = pd.DataFrame()
     if isinstance(lags, int):
-        lags = range(lags+1)
+        lags = range(lags + 1)
     else:
         lags = [int(lag) for lag in lags]
 
     for lag in lags:
         temp_df = df.shift(lag).copy(deep=True)
         temp_df.columns = [str(i) + '_' + str(lag) for i in temp_df.columns]
-        df_lagged = df_lagged.join(temp_df, how = 'outer')
+        df_lagged = df_lagged.join(temp_df, how='outer')
     return df_lagged
+
 
 def _get_betas(y, x):
     """
-    Snippet 17.4, page 259. Fitting The ADF Specidication
+    Snippet 17.4, page 259. Fitting The ADF Specification
     """
     xy = np.dot(x.T, y)
     xx = np.dot(x.T, x)
     xx_inv = np.linalg.inv(xx)
     b_mean = np.dot(xx_inv, xy)
     err = y - np.dot(x, b_mean)
-    b_var = np.dot(err.T, err) / (x.shape[0] - x.shape[1])*xx_inv
+    b_var = np.dot(err.T, err) / (x.shape[0] - x.shape[1]) * xx_inv
     return b_mean, b_var
+
 
 def _sadf_outer_loop(df, min_length, constant, lags, molecule):
     """
     This function gets SADF for t times from molecule
     """
-    sadf_series = pd.Series(index=df.index)
+    sadf_series = pd.Series(index=molecule)
     for index in molecule:
-        index, value = get_sadf_at_t(df.loc[:index], min_length, constant, lags)
+        index, value = _get_sadf_at_t(df.loc[:index], min_length, constant, lags)
         sadf_series[index] = value
     return sadf_series
+
 
 def get_sadf(df, min_length, constant, lags, num_threads=8):
     """
     Multithread implementation of SADF
     """
-    molecule = []
-    for i in range(min_length,df.shape[0]):
-        molecule.append(df.index[i])
+    molecule = df.index[min_length:df.shape[0]]
 
     sadf_series = mp_pandas_obj(func=_sadf_outer_loop,
-                                  pd_obj=('molecule', molecule),
-                                  df = df,
-                                  min_length=min_length,
-                                  constant=constant,
-                                  lags=lags,
-                                  num_threads=num_threads,
-                                  )
+                                pd_obj=('molecule', molecule),
+                                df=df,
+                                min_length=min_length,
+                                constant=constant,
+                                lags=lags,
+                                num_threads=num_threads,
+                                )
     return sadf_series
