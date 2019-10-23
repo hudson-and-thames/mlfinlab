5a6682772b2b74c7ff2eeb242ad8e24455cb162b
proskurin <proskurinolexandr@gmail.com>
2019-10-23 16:42:49 +0300

    SADF performance increase

--- mlfinlab/structural_breaks/sadf.py
+++ mlfinlab/structural_breaks/sadf.py
@@ -7,28 +7,27 @@ import numpy as np
 from mlfinlab.util.multiprocess import mp_pandas_obj
 
 
-def _get_sadf_at_t(log_prices, min_length, model, add_const, lags):
+def _get_sadf_at_t(X, y, min_length):
     """
     Snippet 17.2, page 258. SADF's Inner Loop (get sadf value at t)
     """
-    log_prices_df = pd.DataFrame(log_prices)
-    y, x = _get_y_x(log_prices_df, model=model, add_const=add_const, lags=lags)
-    start_points, bsadf = range(0, y.shape[0] + lags - min_length + 1), -np.inf
+    start_points, bsadf = range(0, y.shape[0] - min_length + 1), -np.inf
     for start in start_points:
-        y_, x_ = y[start:], x[start:]
+        y_, x_ = y[start:], X[start:]
         b_mean_, b_std_ = _get_betas(y_, x_)
-        b_mean_, b_std_ = b_mean_[0, 0], b_std_[0, 0] ** 0.5
-        all_adf = b_mean_ / b_std_
-        if all_adf > bsadf:
-            bsadf = all_adf
-    out = log_prices.index[-1], bsadf
-    return out
+        if not np.isnan(b_mean_[0]):
+            b_mean_, b_std_ = b_mean_[0, 0], b_std_[0, 0] ** 0.5
+            all_adf = b_mean_ / b_std_
+            if all_adf > bsadf:
+                bsadf = all_adf
+    return bsadf
 
 
 def _get_y_x(series, model, lags, add_const):
     """
     Snippet 17.2, page 258-259. Preparing The Datasets
     """
+    series = pd.DataFrame(series)
     series_diff = series.diff().dropna()
     x = _lag_df(series_diff, lags).dropna()
     x['y_lagged'] = series.shift(1).loc[x.index]  # add y_(t-1) column
@@ -36,7 +35,6 @@ def _get_y_x(series, model, lags, add_const):
 
     if add_const is True:
         x['const'] = 1
-
     if model == 'linear':
         x['trend'] = np.arange(x.shape[0])  # Add t to the model (0, 1, 2, 3, 4, 5, .... t)
     elif model == 'quadratic':
@@ -46,7 +44,7 @@ def _get_y_x(series, model, lags, add_const):
     columns = list(x.columns)
     columns.insert(0, columns.pop(columns.index('y_lagged')))
     x = x[columns]
-    return y.values.reshape(-1, 1), x.values
+    return x, y
 
 
 def _lag_df(df, lags):
@@ -72,20 +70,25 @@ def _get_betas(y, x):
     """
     xy = np.dot(x.T, y)
     xx = np.dot(x.T, x)
-    xx_inv = np.linalg.inv(xx)
+    try:
+        xx_inv = np.linalg.inv(xx)
+    except np.linalg.LinAlgError:
+        return [np.nan], [[np.nan, np.nan]]
     b_mean = np.dot(xx_inv, xy)
     err = y - np.dot(x, b_mean)
     b_var = np.dot(err.T, err) / (x.shape[0] - x.shape[1]) * xx_inv
     return b_mean, b_var
 
 
-def _sadf_outer_loop(df, min_length, model, add_const, lags, molecule):
+def _sadf_outer_loop(X, y, min_length, molecule):
     """
     This function gets SADF for t times from molecule
     """
     sadf_series = pd.Series(index=molecule)
     for index in molecule:
-        index, value = _get_sadf_at_t(df.loc[:index], min_length, model, add_const, lags)
+        X_subset = X.loc[:index].values
+        y_subset = y.loc[:index].values.reshape(-1, 1)
+        value = _get_sadf_at_t(X_subset, y_subset, min_length)
         sadf_series[index] = value
     return sadf_series
 
@@ -94,15 +97,19 @@ def get_sadf(df, min_length, model, add_const, lags, num_threads=8):
     """
     Multithread implementation of SADF
     """
-    molecule = df.index[min_length:df.shape[0]]
+    X, y = _get_y_x(df, model, lags, add_const)
+    molecule = y.index[min_length:y.shape[0]]
 
     sadf_series = mp_pandas_obj(func=_sadf_outer_loop,
                                 pd_obj=('molecule', molecule),
-                                df=df,
+                                X=X,
+                                y=y,
                                 min_length=min_length,
-                                model=model,
-                                add_const=add_const,
-                                lags=lags,
                                 num_threads=num_threads,
                                 )
     return sadf_series
+
+
+df = pd.read_csv('../../../../Downloads/pnl_and_commissions.csv', index_col=0, parse_dates=[0])
+df['equity'] = 1e6 + df.realised_pnl + df.unrealised_pnl
+sadf = get_sadf(np.log(df.equity), min_length=20, model='linear', add_const=True, lags=6)
