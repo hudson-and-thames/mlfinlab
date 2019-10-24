0a8fc014c603f0f3440ac8e0e0833779ed9fc161
proskurin <proskurinolexandr@gmail.com>
2019-10-24 15:36:49 +0300

    Add Const run bars tests

--- mlfinlab/tests/test_run_data_structures.py
+++ mlfinlab/tests/test_run_data_structures.py
@@ -196,28 +196,31 @@ class TestDataStructures(unittest.TestCase):
         # Delete generated csv file (if it wasn't generated test would fail)
         os.remove('test.csv')
 
-    def test_const_imbalance_dollar_bars(self):
+    def test_const_run_dollar_bars(self):
         """
-        Tests the Const imbalance dollar bars implementation.
+        Tests the Const run dollar bars implementation.
         """
         exp_num_ticks_init = 1000
+        num_prev_bars = 3
 
-        db1, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=2e7, verbose=False, )
-        db2, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=50, verbose=False)
-        db3, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=10, verbose=False)
-        ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                           expected_imbalance_window=10000,
-                                           batch_size=50, verbose=False,
-                                           to_csv=True, output_path='test.csv')
+        db1, thresh_1 = ds.get_const_dollar_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                     expected_imbalance_window=10000,
+                                                     num_prev_bars=num_prev_bars, batch_size=2e7, verbose=False,
+                                                     analyse_thresholds=True)
+        db2, thresh_2 = ds.get_const_dollar_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                     expected_imbalance_window=10000,
+                                                     num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                                     analyse_thresholds=True)
+        db3, _ = ds.get_const_dollar_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                              expected_imbalance_window=10000,
+                                              num_prev_bars=num_prev_bars, batch_size=10, verbose=False)
+        ds.get_const_dollar_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                     expected_imbalance_window=10000,
+                                     num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                     to_csv=True, output_path='test.csv')
         db4 = pd.read_csv('test.csv')
 
-        self.assertEqual(db1.shape, (204, 9))
+        self.assertEqual(db1.shape, (9, 9))
 
         # Assert diff batch sizes have same number of bars
         self.assertTrue(db1.shape == db2.shape)
@@ -229,47 +232,52 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db3.values))
         self.assertTrue(np.all(db1.values == db4.values))
 
+        self.assertTrue(np.all(thresh_1.cum_theta_buy == thresh_2.cum_theta_buy))
+        self.assertTrue(np.all(thresh_1.cum_theta_sell == thresh_2.cum_theta_sell))
+
         # Assert OHLC is correct (the first value)
         self.assertEqual(db1.loc[0, 'open'], 1306.0)
         self.assertEqual(db1.loc[0, 'high'], 1306.0)
-        self.assertEqual(db1.loc[0, 'low'], 1304.25)
-        self.assertEqual(db1.loc[0, 'close'], 1304.5)
+        self.assertEqual(db1.loc[0, 'low'], 1303.00)
+        self.assertEqual(db1.loc[0, 'close'], 1305.75)
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[2, 'open'], 1306.0)
+        self.assertEqual(db1.loc[2, 'high'], 1307.75)
+        self.assertEqual(db1.loc[2, 'low'], 1305.75)
+        self.assertEqual(db1.loc[2, 'close'], 1307.75)
 
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
-
-        # Assert OHLC is correct (some index)
-        self.assertEqual(db1.loc[200, 'open'], 1304.25)
-        self.assertEqual(db1.loc[200, 'high'], 1304.25)
-        self.assertEqual(db1.loc[200, 'low'], 1303.5)
-        self.assertEqual(db1.loc[200, 'close'], 1303.5)
-
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
         # Delete generated csv file (if it wasn't generated test would fail)
         os.remove('test.csv')
 
-    def test_const_imbalance_volume_bars(self):
+    def test_const_run_volume_bars(self):
         """
-        Tests the Const imbalance volume bars implementation.
+        Tests the Const run volume bars implementation.
         """
         exp_num_ticks_init = 1000
+        num_prev_bars = 3
 
-        db1, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=2e7, verbose=False, )
-        db2, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=50, verbose=False)
-        db3, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                    expected_imbalance_window=10000,
-                                                    batch_size=10, verbose=False)
-        ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                           expected_imbalance_window=10000,
-                                           batch_size=50, verbose=False,
-                                           to_csv=True, output_path='test.csv')
+        db1, thresh_1 = ds.get_const_volume_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                     expected_imbalance_window=10000,
+                                                     num_prev_bars=num_prev_bars, batch_size=2e7, verbose=False,
+                                                     analyse_thresholds=True)
+        db2, thresh_2 = ds.get_const_volume_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                     expected_imbalance_window=10000,
+                                                     num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                                     analyse_thresholds=True)
+        db3, _ = ds.get_const_volume_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                              expected_imbalance_window=10000,
+                                              num_prev_bars=num_prev_bars, batch_size=10, verbose=False)
+        ds.get_const_volume_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                     expected_imbalance_window=10000,
+                                     num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                     to_csv=True, output_path='test.csv')
         db4 = pd.read_csv('test.csv')
 
-        self.assertEqual(db1.shape, (215, 9))
+        self.assertEqual(db1.shape, (9, 9))
 
         # Assert diff batch sizes have same number of bars
         self.assertTrue(db1.shape == db2.shape)
@@ -281,47 +289,52 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db3.values))
         self.assertTrue(np.all(db1.values == db4.values))
 
+        self.assertTrue(np.all(thresh_1.cum_theta_buy == thresh_2.cum_theta_buy))
+        self.assertTrue(np.all(thresh_1.cum_theta_sell == thresh_2.cum_theta_sell))
+
         # Assert OHLC is correct (the first value)
         self.assertEqual(db1.loc[0, 'open'], 1306.0)
         self.assertEqual(db1.loc[0, 'high'], 1306.0)
-        self.assertEqual(db1.loc[0, 'low'], 1304.25)
-        self.assertEqual(db1.loc[0, 'close'], 1304.5)
+        self.assertEqual(db1.loc[0, 'low'], 1303.00)
+        self.assertEqual(db1.loc[0, 'close'], 1305.75)
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[2, 'open'], 1306.0)
+        self.assertEqual(db1.loc[2, 'high'], 1307.75)
+        self.assertEqual(db1.loc[2, 'low'], 1305.75)
+        self.assertEqual(db1.loc[2, 'close'], 1307.75)
 
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
-
-        # Assert OHLC is correct (some index)
-        self.assertEqual(db1.loc[200, 'open'], 1305.00)
-        self.assertEqual(db1.loc[200, 'high'], 1305.00)
-        self.assertEqual(db1.loc[200, 'low'], 1305.00)
-        self.assertEqual(db1.loc[200, 'close'], 1305.00)
-
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
         # Delete generated csv file (if it wasn't generated test would fail)
         os.remove('test.csv')
 
-    def test_const_imbalance_tick_bars(self):
+    def test_const_run_tick_bars(self):
         """
-        Tests the Const imbalance tick bars implementation.
+        Tests the Const run dollar bars implementation.
         """
         exp_num_ticks_init = 1000
+        num_prev_bars = 3
 
-        db1, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                  expected_imbalance_window=10000,
-                                                  batch_size=2e7, verbose=False, )
-        db2, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                  expected_imbalance_window=10000,
-                                                  batch_size=50, verbose=False)
-        db3, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                                  expected_imbalance_window=10000,
-                                                  batch_size=10, verbose=False)
-        ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
-                                         expected_imbalance_window=10000,
-                                         batch_size=50, verbose=False,
-                                         to_csv=True, output_path='test.csv')
+        db1, thresh_1 = ds.get_const_tick_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                   expected_imbalance_window=10000,
+                                                   num_prev_bars=num_prev_bars, batch_size=2e7, verbose=False,
+                                                   analyse_thresholds=True)
+        db2, thresh_2 = ds.get_const_tick_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                   expected_imbalance_window=10000,
+                                                   num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                                   analyse_thresholds=True)
+        db3, _ = ds.get_const_tick_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                            expected_imbalance_window=10000,
+                                            num_prev_bars=num_prev_bars, batch_size=10, verbose=False)
+        ds.get_const_tick_run_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                   expected_imbalance_window=10000,
+                                   num_prev_bars=num_prev_bars, batch_size=50, verbose=False,
+                                   to_csv=True, output_path='test.csv')
         db4 = pd.read_csv('test.csv')
 
-        self.assertEqual(db1.shape, (78, 9))
+        self.assertEqual(db1.shape, (9, 9))
 
         # Assert diff batch sizes have same number of bars
         self.assertTrue(db1.shape == db2.shape)
@@ -333,20 +346,22 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db3.values))
         self.assertTrue(np.all(db1.values == db4.values))
 
+        self.assertTrue(np.all(thresh_1.cum_theta_buy == thresh_2.cum_theta_buy))
+        self.assertTrue(np.all(thresh_1.cum_theta_sell == thresh_2.cum_theta_sell))
+
         # Assert OHLC is correct (the first value)
         self.assertEqual(db1.loc[0, 'open'], 1306.0)
         self.assertEqual(db1.loc[0, 'high'], 1306.0)
-        self.assertEqual(db1.loc[0, 'low'], 1304.25)
-        self.assertEqual(db1.loc[0, 'close'], 1304.25)
+        self.assertEqual(db1.loc[0, 'low'], 1303.00)
+        self.assertEqual(db1.loc[0, 'close'], 1305.75)
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[2, 'open'], 1306.0)
+        self.assertEqual(db1.loc[2, 'high'], 1307.5)
+        self.assertEqual(db1.loc[2, 'low'], 1305.75)
+        self.assertEqual(db1.loc[2, 'close'], 1307.5)
 
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
-
-        # Assert OHLC is correct (some index)
-        self.assertEqual(db1.loc[70, 'open'], 1305.5)
-        self.assertEqual(db1.loc[70, 'high'], 1305.75)
-        self.assertEqual(db1.loc[70, 'low'], 1305.25)
-        self.assertEqual(db1.loc[70, 'close'], 1305.75)
-
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
         # Delete generated csv file (if it wasn't generated test would fail)
