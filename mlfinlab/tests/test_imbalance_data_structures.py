73150949bda727772f68400fa0bf6d77bb527e4c
proskurin <proskurinolexandr@gmail.com>
2019-10-24 13:50:18 +0300

    Add Const imbalance bars tests

--- mlfinlab/tests/test_imbalance_data_structures.py
+++ mlfinlab/tests/test_imbalance_data_structures.py
@@ -60,18 +60,18 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db4.values))
 
         # Assert OHLC is correct (the first value)
-        self.assertEquals(db1.loc[0, 'open'], 1306.0)
-        self.assertEquals(db1.loc[0, 'high'], 1306.0)
-        self.assertEquals(db1.loc[0, 'low'], 1304.25)
-        self.assertEquals(db1.loc[0, 'close'], 1304.5)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.5)
 
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
 
         # Assert OHLC is correct (some index)
-        self.assertEquals(db1.loc[1387, 'open'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'high'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'low'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'close'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'open'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'high'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'low'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'close'], 1303.5)
 
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
@@ -113,16 +113,16 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db4.values))
 
         # Assert OHLC is correct (the first value)
-        self.assertEquals(db1.loc[0, 'open'], 1306.0)
-        self.assertEquals(db1.loc[0, 'high'], 1306.0)
-        self.assertEquals(db1.loc[0, 'low'], 1304.25)
-        self.assertEquals(db1.loc[0, 'close'], 1304.5)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.5)
 
         # Assert OHLC is correct (some index)
-        self.assertEquals(db1.loc[1387, 'open'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'high'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'low'], 1303.5)
-        self.assertEquals(db1.loc[1387, 'close'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'open'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'high'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'low'], 1303.5)
+        self.assertEqual(db1.loc[1387, 'close'], 1303.5)
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
@@ -164,17 +164,173 @@ class TestDataStructures(unittest.TestCase):
         self.assertTrue(np.all(db1.values == db4.values))
 
         # Assert OHLC is correct (the first value)
-        self.assertEquals(db1.loc[0, 'open'], 1306.0)
-        self.assertEquals(db1.loc[0, 'high'], 1306.0)
-        self.assertEquals(db1.loc[0, 'low'], 1304.25)
-        self.assertEquals(db1.loc[0, 'close'], 1304.25)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.25)
 
         # Assert OHLC is correct (some index)
-        self.assertEquals(db1.loc[1387, 'open'], 1308.0)
-        self.assertEquals(db1.loc[1387, 'high'], 1308.0)
-        self.assertEquals(db1.loc[1387, 'low'], 1308.0)
-        self.assertEquals(db1.loc[1387, 'close'], 1308.0)
+        self.assertEqual(db1.loc[1387, 'open'], 1308.0)
+        self.assertEqual(db1.loc[1387, 'high'], 1308.0)
+        self.assertEqual(db1.loc[1387, 'low'], 1308.0)
+        self.assertEqual(db1.loc[1387, 'close'], 1308.0)
         self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
+        self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
+
+        # Delete generated csv file (if it wasn't generated test would fail)
+        os.remove('test.csv')
+
+    def test_const_imbalance_dollar_bars(self):
+        """
+        Tests the Const imbalance dollar bars implementation.
+        """
+        exp_num_ticks_init = 1000
+
+        db1, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=2e7, verbose=False, )
+        db2, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=50, verbose=False)
+        db3, _ = ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=10, verbose=False)
+        ds.get_const_dollar_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                           expected_imbalance_window=10000,
+                                           batch_size=50, verbose=False,
+                                           to_csv=True, output_path='test.csv')
+        db4 = pd.read_csv('test.csv')
+
+        self.assertEqual(db1.shape, (204, 9))
+
+        # Assert diff batch sizes have same number of bars
+        self.assertTrue(db1.shape == db2.shape)
+        self.assertTrue(db1.shape == db3.shape)
+        self.assertTrue(db1.shape == db4.shape)
+
+        # Assert same values
+        self.assertTrue(np.all(db1.values == db2.values))
+        self.assertTrue(np.all(db1.values == db3.values))
+        self.assertTrue(np.all(db1.values == db4.values))
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.5)
+
+        self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
+
+        # Assert OHLC is correct (some index)
+        self.assertEqual(db1.loc[200, 'open'], 1304.25)
+        self.assertEqual(db1.loc[200, 'high'], 1304.25)
+        self.assertEqual(db1.loc[200, 'low'], 1303.5)
+        self.assertEqual(db1.loc[200, 'close'], 1303.5)
+
+        self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
+
+        # Delete generated csv file (if it wasn't generated test would fail)
+        os.remove('test.csv')
+
+    def test_const_imbalance_volume_bars(self):
+        """
+        Tests the Const imbalance volume bars implementation.
+        """
+        exp_num_ticks_init = 1000
+
+        db1, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=2e7, verbose=False, )
+        db2, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=50, verbose=False)
+        db3, _ = ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                    expected_imbalance_window=10000,
+                                                    batch_size=10, verbose=False)
+        ds.get_const_volume_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                           expected_imbalance_window=10000,
+                                           batch_size=50, verbose=False,
+                                           to_csv=True, output_path='test.csv')
+        db4 = pd.read_csv('test.csv')
+
+        self.assertEqual(db1.shape, (215, 9))
+
+        # Assert diff batch sizes have same number of bars
+        self.assertTrue(db1.shape == db2.shape)
+        self.assertTrue(db1.shape == db3.shape)
+        self.assertTrue(db1.shape == db4.shape)
+
+        # Assert same values
+        self.assertTrue(np.all(db1.values == db2.values))
+        self.assertTrue(np.all(db1.values == db3.values))
+        self.assertTrue(np.all(db1.values == db4.values))
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.5)
+
+        self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
+
+        # Assert OHLC is correct (some index)
+        self.assertEqual(db1.loc[200, 'open'], 1305.00)
+        self.assertEqual(db1.loc[200, 'high'], 1305.00)
+        self.assertEqual(db1.loc[200, 'low'], 1305.00)
+        self.assertEqual(db1.loc[200, 'close'], 1305.00)
+
+        self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
+
+        # Delete generated csv file (if it wasn't generated test would fail)
+        os.remove('test.csv')
+
+    def test_const_imbalance_tick_bars(self):
+        """
+        Tests the Const imbalance tick bars implementation.
+        """
+        exp_num_ticks_init = 1000
+
+        db1, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                  expected_imbalance_window=10000,
+                                                  batch_size=2e7, verbose=False, )
+        db2, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                  expected_imbalance_window=10000,
+                                                  batch_size=50, verbose=False)
+        db3, _ = ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                                  expected_imbalance_window=10000,
+                                                  batch_size=10, verbose=False)
+        ds.get_const_tick_imbalance_bars(self.path, exp_num_ticks_init=exp_num_ticks_init,
+                                         expected_imbalance_window=10000,
+                                         batch_size=50, verbose=False,
+                                         to_csv=True, output_path='test.csv')
+        db4 = pd.read_csv('test.csv')
+
+        self.assertEqual(db1.shape, (78, 9))
+
+        # Assert diff batch sizes have same number of bars
+        self.assertTrue(db1.shape == db2.shape)
+        self.assertTrue(db1.shape == db3.shape)
+        self.assertTrue(db1.shape == db4.shape)
+
+        # Assert same values
+        self.assertTrue(np.all(db1.values == db2.values))
+        self.assertTrue(np.all(db1.values == db3.values))
+        self.assertTrue(np.all(db1.values == db4.values))
+
+        # Assert OHLC is correct (the first value)
+        self.assertEqual(db1.loc[0, 'open'], 1306.0)
+        self.assertEqual(db1.loc[0, 'high'], 1306.0)
+        self.assertEqual(db1.loc[0, 'low'], 1304.25)
+        self.assertEqual(db1.loc[0, 'close'], 1304.25)
+
+        self.assertTrue((db1.loc[:, 'high'] >= db1.loc[:, 'low']).all())
+
+        # Assert OHLC is correct (some index)
+        self.assertEqual(db1.loc[70, 'open'], 1305.5)
+        self.assertEqual(db1.loc[70, 'high'], 1305.75)
+        self.assertEqual(db1.loc[70, 'low'], 1305.25)
+        self.assertEqual(db1.loc[70, 'close'], 1305.75)
+
         self.assertTrue((db1.loc[:, 'volume'] >= db1.loc[:, 'cum_buy_volume']).all())
 
         # Delete generated csv file (if it wasn't generated test would fail)
