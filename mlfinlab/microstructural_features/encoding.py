9be284091a454a6ab80cf9d15d546ef66f22c998
proskurin <proskurinolexandr@gmail.com>
2019-10-31 17:17:44 +0200

    Add encoding docstrings

--- mlfinlab/microstructural_features/encoding.py
+++ mlfinlab/microstructural_features/encoding.py
@@ -1,21 +1,33 @@
+"""
+Various functions for message encoding (quantile)
+"""
 import numpy as np
 
 
-def encode_tick_rule_array(tick_rule_array):
+def encode_tick_rule_array(tick_rule_array: list) -> str:
+    """
+    Encode array of tick signs (-1, 1, 0)
+    :param tick_rule_array: (list) of tick rules
+    :return: (str) encoded message
+    """
     message = ''
-    for el in tick_rule_array:
-        if el == 1:
+    for element in tick_rule_array:
+        if element == 1:
             message += 'a'
-        elif el == -1:
+        elif element == -1:
             message += 'b'
-        elif el == 0:
+        elif element == 0:
             message += 'c'
         else:
             raise ValueError('Unknown value for tick rule: {}'.format(el))
     return message
 
 
-def _get_ascii_table():
+def _get_ascii_table() -> list:
+    """
+    Get all ASCII symbols
+    :return: (list) of ASCII symbols
+    """
     # ASCII table consists of 256 characters
     table = []
     for i in range(256):
@@ -23,27 +35,51 @@ def _get_ascii_table():
     return table
 
 
-def quantile_mapping(array, num_letters=26):
+def quantile_mapping(array: list, num_letters: int = 26) -> dict:
+    """
+    Generate dictionary of quantile-letters based on values from array and dictionary length (num_letters)
+    :param array: (list) of values to split on quantiles
+    :param num_letters: (int) number of letters(quantiles) to encode
+    :return: (dict) of quantile-symbol
+    """
     encoding_dict = {}
     ascii_table = _get_ascii_table()
     alphabet = ascii_table[:num_letters]
-    for q, l in zip(np.linspace(0.01, 1, len(alphabet)), alphabet):
-        encoding_dict[np.quantile(array, q)] = l
+    for quant, letter in zip(np.linspace(0.01, 1, len(alphabet)), alphabet):
+        encoding_dict[np.quantile(array, quant)] = letter
     return encoding_dict
 
 
-def _find_nearest(array, value):
+def _find_nearest(array: list, value: float) -> float:
+    """
+    Find the nearest element from array to an element
+    :param array: (list) of values
+    :param value: (float) value for which the nearest element needs to be found
+    :return: (float) the nearest to the value element in array
+    """
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
 
-def _get_letter_from_encoding(value, encoding_dict):
+def _get_letter_from_encoding(value: float, encoding_dict: dict) -> str:
+    """
+    Get letter for float/int value from encoding dict
+    :param value: (float/int)
+    :param encoding_dict: (dict)
+    :return: (str): letter from encoding dict
+    """
     return encoding_dict[_find_nearest(list(encoding_dict.keys()), value)]
 
 
-def encode_array(array, encoding_dict):
+def encode_array(array: list, encoding_dict: dict) -> str:
+    """
+    Encode array with strings using encoding dict
+    :param array: (list) of values to encode
+    :param encoding_dict: (dict) of quantile-symbol
+    :return: (str): encoded message
+    """
     message = ''
-    for el in array:
-        message += _get_letter_from_encoding(el, encoding_dict)
+    for element in array:
+        message += _get_letter_from_encoding(element, encoding_dict)
     return message
--- mlfinlab/microstructural_features/second_generation.py
+++ mlfinlab/microstructural_features/second_generation.py
@@ -1,21 +1,49 @@
+"""
+Second generation models features: Kyle lambda, Amihud Lambda, Hasbrouck lambda (bar and trade based)
+"""
+
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 
-def get_bar_based_kyle_lambda(close, volume, window=20):
+# pylint: disable=invalid-name
+
+def get_bar_based_kyle_lambda(close: pd.Series, volume: pd.Series, window: int = 20) -> pd.Series:
+    """
+    Get Kyle lambda from bars data
+    :param close: (pd.Series) Close prices
+    :param volume: (pd.Series) Bar volume
+    :param window: (int) rolling window used for estimation
+    :return: (pd.Series) of Kyle lambdas
+    """
     close_diff = close.diff()
     close_diff_sign = np.sign(close_diff)
-    close_diff_sign.replace(0, method='pad', inplace=True) # Replace 0 values with previous
+    close_diff_sign.replace(0, method='pad', inplace=True)  # Replace 0 values with previous
     volume_mult_trade_signs = volume * close_diff_sign  # bt * Vt
     return (close_diff / volume_mult_trade_signs).rolling(window=window).mean()
 
 
-def get_bar_based_amihud_lambda(close, dollar_volume, window=20):
+def get_bar_based_amihud_lambda(close: pd.Series, dollar_volume: pd.Series, window: int = 20) -> pd.Series:
+    """
+    Get Amihud lambda from bars data
+    :param close: (pd.Series) Close prices
+    :param dollar_volume: (pd.Series) Dollar volumes
+    :param window: (int) rolling window used for estimation
+    :return: (pd.Series) of Amihud lambda
+    """
     returns_abs = np.log(close / close.shift(1)).abs()
     return (returns_abs / dollar_volume).rolling(window=window).mean()
 
 
-def get_bar_based_hasbrouck_lambda(close, dollar_volume, window=20):
+def get_bar_based_hasbrouck_lambda(close: pd.Series, dollar_volume: pd.Series, window: int = 20) -> pd.Series:
+    """
+    Get Hasbrouck lambda from bars data
+    :param close: (pd.Series) Close prices
+    :param dollar_volume: (pd.Series) Dollar volumes
+    :param window: (int) rolling window used for estimation
+    :return: (pd.Series) of Hasbrouck lambda
+    """
     log_ret = np.log(close / close.shift(1))
     log_ret_sign = np.sign(log_ret).replace(0, method='pad')
 
@@ -23,7 +51,15 @@ def get_bar_based_hasbrouck_lambda(close, dollar_volume, window=20):
     return (log_ret / signed_dollar_volume_sqrt).rolling(window=window).mean()
 
 
-def get_trades_based_kyle_lambda(price_diff, volume, aggressor_flags):
+def get_trades_based_kyle_lambda(price_diff: list, volume: list, aggressor_flags: list) -> float:
+    """
+    Get Kyle lambda from trades data
+    :param price_diff: (list) of price diffs
+    :param volume: (list) of trades sizes
+    :param aggressor_flags: (list) of trade directions [-1, 1]  (tick rule or aggressor side can be used to define)
+    :return: (float) Kyle lambda for a bar
+    """
+
     model = LinearRegression(fit_intercept=False, copy_X=False)
     signed_volume = np.array(volume) * np.array(aggressor_flags)
     X = np.array(signed_volume).reshape(-1, 1)
@@ -32,7 +68,14 @@ def get_trades_based_kyle_lambda(price_diff, volume, aggressor_flags):
     return model.coef_[0]
 
 
-def get_trades_based_amihud_lambda(log_ret, dollar_volume):
+def get_trades_based_amihud_lambda(log_ret: list, dollar_volume: list) -> float:
+    """
+    Get Amihud lambda from trades data
+    :param log_ret: (list) of log returns
+    :param dollar_volume: (list) of dollar volumes (price * size)
+    :return: (float) Amihud lambda for a bar
+    """
+
     model = LinearRegression(fit_intercept=False, copy_X=False)
     X = np.array(dollar_volume).reshape(-1, 1)
     y = np.abs(np.array(log_ret))
@@ -40,7 +83,14 @@ def get_trades_based_amihud_lambda(log_ret, dollar_volume):
     return model.coef_[0]
 
 
-def get_trades_based_hasbrouck_lambda(log_ret, dollar_volume, aggressor_flags):
+def get_trades_based_hasbrouck_lambda(log_ret: list, dollar_volume: list, aggressor_flags: list) -> float:
+    """
+    Get Amihud lambda from trades data
+    :param log_ret: (list) of log returns
+    :param dollar_volume: (list) of dollar volumes (price * size)
+    :param aggressor_flags: (list) of trade directions [-1, 1]  (tick rule or aggressor side can be used to define)
+    :return: (float) Amihud lambda for a bar
+    """
     model = LinearRegression(fit_intercept=False, copy_X=False)
     X = (np.sqrt(np.array(dollar_volume)) * np.array(aggressor_flags)).reshape(-1, 1)
     y = np.abs(np.array(log_ret))
