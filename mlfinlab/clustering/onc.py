c58b2e7bb8eca8fe4066d3b50ea346a5b3d97a49
Alex <proskurinolexandr@gmail.com>
2019-11-16 12:30:08 +0200

    Fix pylint errors

--- mlfinlab/clustering/onc.py
+++ mlfinlab/clustering/onc.py
@@ -11,7 +11,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples
 
 
-def get_clusters_quality_scores(clusters:dict, scores:np.array) -> dict:
+def get_clusters_quality_scores(clusters: dict, scores: np.array) -> dict:
     """
     Calculates clusters quality scores
 
@@ -22,7 +22,15 @@ def get_clusters_quality_scores(clusters:dict, scores:np.array) -> dict:
     return {i: np.mean(scores[clusters[i]]) / np.std(scores[clusters[i]]) for i in clusters.keys()}
 
 
-def _improve_clusters(corr_mat, clusters, top_clusters):
+def _improve_clusters(corr_mat: pd.DataFrame, clusters: dict, top_clusters: dict) -> Union[
+        pd.DataFrame, dict, pd.Series] :
+    """
+    Improve number clusters using silh scores
+    :param corr_mat: (pd.DataFrame) correlation matrix
+    :param clusters: (dict) clusters elements
+    :param top_clusters: (dict) of improved clusters elements
+    :return: 3 tuple (ordered correlation matrix, clusters, silh scores)
+    """
     clusters_new, new_idx = {}, []
     for i in clusters.keys():
         clusters_new[len(clusters_new.keys())] = list(clusters[i])
@@ -36,7 +44,7 @@ def _improve_clusters(corr_mat, clusters, top_clusters):
     dist = ((1 - corr_mat.fillna(0)) / 2.0) ** 0.5
 
     kmeans_labels = np.zeros(len(dist.columns))
-    for i in clusters_new.keys():
+    for i in clusters_new:
         idxs = [dist.index.get_loc(k) for k in clusters_new[i]]
         kmeans_labels[idxs] = i
 
@@ -44,7 +52,8 @@ def _improve_clusters(corr_mat, clusters, top_clusters):
     return corr_new, clusters_new, silh_scores_new
 
 
-def _cluster_kmeans_base(corr_mat:pd.DataFrame, max_num_clusters:int=10, repeat:int=10) -> Union[pd.DataFrame, dict, pd.Series]:
+def _cluster_kmeans_base(corr_mat: pd.DataFrame, max_num_clusters: int = 10, repeat: int = 10) -> Union[
+        pd.DataFrame, dict, pd.Series]:
     """
     Initial clustering step using KMeans.
 
@@ -60,7 +69,7 @@ def _cluster_kmeans_base(corr_mat:pd.DataFrame, max_num_clusters:int=10, repeat:
     silh = pd.Series()
 
     # Get optimal num clusters
-    for init in range(repeat):
+    for _ in range(repeat):
         for num_clusters in range(2, max_num_clusters + 1):
             kmeans_ = KMeans(n_clusters=num_clusters, n_jobs=1, n_init=1)
             kmeans_ = kmeans_.fit(distance)
@@ -87,7 +96,7 @@ def _cluster_kmeans_base(corr_mat:pd.DataFrame, max_num_clusters:int=10, repeat:
     return corr1, clusters, silh
 
 
-def cluster_kmeans_top(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFrame, dict, pd.Series]:
+def cluster_kmeans_top(corr_mat: pd.DataFrame, repeat: int = 10) -> Union[pd.DataFrame, dict, pd.Series]:
     """
     Improve the initial clustering by leaving clusters with high scores unchanged and modifying clusters with
     below average scores.
@@ -96,6 +105,8 @@ def cluster_kmeans_top(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFra
     :param repeat: number of clustering algorithm repetitions.
     :return: 3 tuple (correlation matrix, optimized clusters, silh scores)
     """
+    # pylint: disable=no-else-return
+
     max_num_clusters = corr_mat.shape[1] - 1
     corr1, clusters, silh = _cluster_kmeans_base(corr_mat, max_num_clusters=max_num_clusters, repeat=repeat)
 
@@ -108,14 +119,13 @@ def cluster_kmeans_top(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFra
         # If 2 or less clusters have a quality rating less than the average then stop.
         return corr1, clusters, silh
     else:
-
         keys_redo = []
         for i in redo_clusters:
             keys_redo.extend(clusters[i])
 
         corr_tmp = corr_mat.loc[keys_redo, keys_redo]
         mean_redo_tstat = np.mean([cluster_quality[i] for i in redo_clusters])
-        corr2, top_clusters, silh_score_2 = cluster_kmeans_top(corr_tmp, repeat=repeat)
+        _, top_clusters, _ = cluster_kmeans_top(corr_tmp, repeat=repeat)
 
         # Make new clusters (improved)
         corr_new, clusters_new, silh_new = _improve_clusters(corr_mat,
@@ -123,7 +133,7 @@ def cluster_kmeans_top(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFra
                                                               i not in redo_clusters},
                                                              top_clusters)
         new_tstat_mean = np.mean(
-            [np.mean(silh_new[clusters_new[i]]) / np.std(silh_new[clusters_new[i]]) for i in clusters_new.keys()])
+            [np.mean(silh_new[clusters_new[i]]) / np.std(silh_new[clusters_new[i]]) for i in clusters_new])
 
         if new_tstat_mean <= mean_redo_tstat:
             return corr1, clusters, silh, cluster_quality
@@ -131,12 +141,12 @@ def cluster_kmeans_top(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFra
             return corr_new, clusters_new, silh_new
 
 
-def onc_cluster(corr_mat:pd.DataFrame, repeat:int=10) -> Union[pd.DataFrame, dict, pd.Series]:
+def get_onc_clusters(corr_mat: pd.DataFrame, repeat: int = 10) -> Union[pd.DataFrame, dict, pd.Series]:
     """
     ONC algorithm (Optimal Number of Clusters) using correlation matrix of features.
 
     :param corr_mat: (pd.DataFrame) correlation matrix of features
-    :param repeat (int): number of clustering algorithm repetitions.
+    :param repeat: (int) number of clustering algorithm repetitions.
     :return: 3 tuple (correlation matrix, optimized clusters, silh scores)
     """
 
