1d284f47cff53de34341cbec84321034c0f3eda5
proskurin <proskurinolexandr@gmail.com>
2019-11-19 16:54:29 +0200

    Add feature_importance, ensemble to init

--- mlfinlab/__init__.py
+++ mlfinlab/__init__.py
@@ -12,3 +12,5 @@ import mlfinlab.sample_weights as sample_weights
 import mlfinlab.sampling as sampling
 import mlfinlab.bet_sizing as bet_sizing
 import mlfinlab.util as util
+import mlfinlab.feature_importance as feature_importance
+import mlfinlab.ensemble as ensemble
