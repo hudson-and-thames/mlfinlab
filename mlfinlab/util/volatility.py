11ca21fff3b7991259d7c781c34d7a7cdc567616
proskurin <proskurinolexandr@gmail.com>
2020-01-04 11:31:17 +0200

    Solve bug in volatility calculations

--- mlfinlab/util/volatility.py
+++ mlfinlab/util/volatility.py
@@ -7,22 +7,17 @@ import numpy as np
 
 # pylint: disable=redefined-builtin
 
-def get_daily_vol(close: pd.Series, lookback: int = 100) -> pd.Series:
+def get_daily_vol(close, lookback=100):
     """
     Snippet 3.1, page 44, Daily Volatility Estimates
-
     Computes the daily volatility at intraday estimation points.
-
     In practice we want to set profit taking and stop-loss limits that are a function of the risks involved
     in a bet. Otherwise, sometimes we will be aiming too high (tao ≫ sigma_t_i,0), and sometimes too low
     (tao ≪ sigma_t_i,0 ), considering the prevailing volatility. Snippet 3.1 computes the daily volatility
     at intraday estimation points, applying a span of lookback days to an exponentially weighted moving
     standard deviation.
-
     See the pandas documentation for details on the pandas.Series.ewm function.
-
     Note: This function is used to compute dynamic thresholds for profit taking and stop loss limits.
-
     :param close: Closing prices
     :param lookback: lookback period to compute volatility
     :return: series of daily volatility value
@@ -32,7 +27,7 @@ def get_daily_vol(close: pd.Series, lookback: int = 100) -> pd.Series:
     df0 = df0[df0 > 0]
     df0 = (pd.Series(close.index[df0 - 1], index=close.index[close.shape[0] - df0.shape[0]:]))
 
-    df0 = close.loc[df0.index] / close.loc[df0.values].values - 1  # daily returns
+    df0 = close.loc[df0.index] / close.loc[df0.array].array - 1  # daily returns
     df0 = df0.ewm(span=lookback).std()
     return df0
 
