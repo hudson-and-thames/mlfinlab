200ac4db2225e2f1ffc44ee4ce9d3395e8811f47
Illya Barziy <panpipp@gmail.com>
2020-03-06 16:16:32 +0100

    Backtest statistics with Sharpe ratios

--- mlfinlab/backtest_statistics/__init__.py
+++ mlfinlab/backtest_statistics/__init__.py
@@ -3,4 +3,5 @@ Functions derived from Chapter 14: Backtest Statistics
 """
 
 from mlfinlab.backtest_statistics.statistics import (timing_of_flattening_and_flips, average_holding_period, bets_concentration
-                                                    positive_negative_bets_concentration, compute_drawdown_and_time_under_water)
+                                                    positive_negative_bets_concentration, compute_drawdown_and_time_under_water,
+                                                    sharpe_ratio, probabalistic_sharpe_ratio, deflated_sharpe_ratio)
--- mlfinlab/backtest_statistics/statistics.py
+++ mlfinlab/backtest_statistics/statistics.py
@@ -3,6 +3,8 @@ Implements the book Chapter 14 on Backtest Statistics
 """
 
 import pandas as pd
+import scipy.stats as ss
+import numpy as np
 
 
 def timing_of_flattening_and_flips(target_positions: pd.Series) -> pd.DatetimeIndex:
@@ -13,15 +15,15 @@ def timing_of_flattening_and_flips(target_positions: pd.Series) -> pd.DatetimeIn
     :param target_positions: (pd.Series) target position series with timestamps as indices
     :return: (pd.DatetimeIndex) timestamps of trades flattening, flipping and last bet
     """
-    empty_positions = target_positions[(target_positions==0)].index # empty positions index
-    previous_positions = target_positions.shift(1) # timestamps pointing at previous positions
-    previous_positions = previous_positions[(previous_positions!=0)].index # index of positions where previous one wasn't empty
+    empty_positions = target_positions[(target_positions==0)].index # Empty positions index
+    previous_positions = target_positions.shift(1) # Timestamps pointing at previous positions
+    previous_positions = previous_positions[(previous_positions!=0)].index # Index of positions where previous one wasn't empty
     flattening = empty_positions.intersection(previous_positions) # FLATTENING - if previous position was open, but current is empty
-    multiplied_posions = target_positions.iloc[1:] * target_positions.iloc[:-1].values # multiplies current position with value of next one
+    multiplied_posions = target_positions.iloc[1:] * target_positions.iloc[:-1].values # Multiplies current position with value of next one
     flips = multiplied_posions[(multiplied_posions<0)].index # FLIPS - if current position has another direction compared to the next
     res = flattening.union(flips).sort_values()
     if target_positions.index[-1] not in res:
-        res=res.append(target_positions.index[-1:]) # appending with last bet
+        res=res.append(target_positions.index[-1:]) # Appending with last bet
     return res
 
 
@@ -34,31 +36,31 @@ def average_holding_period(target_positions: pd.Series) -> float:
     - entry_time = (previous_time * weight_of_previous_position + 
                     time_since_beginning_of_trade * increase_in_position ) /
                     weight_of_current_position
-    - holding_period ['dT' = time a position was held, 'w' = weight of position closed]
+    - holding_period ['holding_time' = time a position was held, 'weight' = weight of position closed]
     - res = weighted average time a trade was held
     
     :param target_positions: (pd.Series) target position series with timestamps as indices
     :return: (float) estimated average holding period, NaN if zero or unpredicted
     """
-    holding_period, entry_time = pd.DataFrame(columns=['dT','w']), 0
+    holding_period, entry_time = pd.DataFrame(columns=['holding_time','weight']), 0
     position_difference = target_positions.diff() 
     
-    # time elapsed from the starting time for each position
+    # Time elapsed from the starting time for each position
     time_difference = (target_positions.index-target_positions.index[0]) / np.timedelta64(1,'D')
     for i in range(1, target_positions.shape[0]):
-        if float(position_difference.iloc[i] * target_positions.iloc[i-1]) >= 0: # increased or unchanged position
-            if float(target_positions.iloc[i]) != 0: # and not an empty position
+        if float(position_difference.iloc[i] * target_positions.iloc[i-1]) >= 0: # Increased or unchanged position
+            if float(target_positions.iloc[i]) != 0: # And not an empty position
                 entry_time = (entry_time * target_positions.iloc[i-1] + 
                               time_difference[i] * position_difference.iloc[i]) / target_positions.iloc[i]
-        else: # decreased
+        else: # Decreased
             if float(target_positions.iloc[i] * target_positions.iloc[i-1]) < 0: # Flip of a position
-                holding_period.loc[target_positions.index[i], ['dT','w']] = (time_difference[i] - entry_time, abs(target_positions.iloc[i-1]))
-                entry_time = time_difference[i] # reset entry time
+                holding_period.loc[target_positions.index[i], ['holding_time','weight']] = (time_difference[i] - entry_time, abs(target_positions.iloc[i-1]))
+                entry_time = time_difference[i] # Reset entry time
             else: # Only a part of position is closed
-                holding_period.loc[target_positions.index[i], ['dT','w']] = (time_difference[i] - entry_time, abs(position_difference.iloc[i]))
+                holding_period.loc[target_positions.index[i], ['holding_time','weight']] = (time_difference[i] - entry_time, abs(position_difference.iloc[i]))
                 
-    if float(holding_period['w'].sum()) > 0: # If there were closed trades at all
-        res = float((holding_period['dT'] * holding_period['w']).sum() / holding_period['w'].sum())
+    if float(holding_period['weight'].sum()) > 0: # If there were closed trades at all
+        res = float((holding_period['holding_time'] * holding_period['weight']).sum() / holding_period['weight'].sum())
     else:
         res = float('nan')
         
@@ -77,8 +79,8 @@ def bets_concentration(returns: pd.Series) -> float:
     :return: (float) concentration of returns (nan if less than 3 returns)
     """  
     if returns.shape[0]<=2:
-        return float('nan') #if less than 3 bets
-    weights = returns / returns.sum() #weights of each bet
+        return float('nan') # If less than 3 bets
+    weights = returns / returns.sum() # Weights of each bet
     hhi = (weights ** 2).sum() # Herfindahl-Hirschman Index for weights
     hhi = float((hhi - returns.shape[0] **(-1)) / (1 - returns.shape[0]**(-1)))
     return hhi
@@ -101,12 +103,12 @@ def positive_negative_bets_concentration(returns: pd.Series, freq: str = 'M') ->
     :return: (tuple of floats) concentration of positive, negative
                             and time grouped concentrations
     """  
-    # concentration of positive returns per bet
+    # Concentration of positive returns per bet
     positive_concentration = bets_concentration(returns[returns >= 0])
-    # concentration of negative returns per bet
+    # Concentration of negative returns per bet
     negative_concentration = bets_concentration(returns[returns < 0]) 
-    # concentration of bets/time period (month by default)
-    time_concentration = bets_concentration(returns.groupby(pd.Grouper(freq='M')).count()) # concentr. bets/month
+    # Concentration of bets/time period (month by default)
+    time_concentration = bets_concentration(returns.groupby(pd.Grouper(freq='M')).count()) 
     return (positive_concentration, negative_concentration, time_concentration)
 
 
@@ -132,13 +134,13 @@ def compute_drawdown_and_time_under_water(returns: pd.Series, dollars: bool = Fa
             
     """
     frame = returns.to_frame('pnl')
-    frame['hwm'] = returns.expanding().max() # adding high watermarks as column
-    #grouped as min returns by high watermarks
+    frame['hwm'] = returns.expanding().max() # Adding high watermarks as column
+    # Grouped as min returns by high watermarks
     high_watermarks = frame.groupby('hwm').min().reset_index()
     high_watermarks.columns = ['hwm','min']
-    # time high watermark occured
+    # Time high watermark occured
     high_watermarks.index = frame['hwm'].drop_duplicates(keep='first').index
-    #picking ones that had a drawdown after high watermark
+    # Picking ones that had a drawdown after high watermark
     high_watermarks = high_watermarks[high_watermarks['hwm'] > high_watermarks['min']]
     if dollars:
         drawdown = high_watermarks['hwm'] - high_watermarks['min']
@@ -149,3 +151,83 @@ def compute_drawdown_and_time_under_water(returns: pd.Series, dollars: bool = Fa
     time_under_water = pd.Series(time_under_water, index = high_watermarks.index[:-1])
     return drawdown, time_under_water
 
+
+def sharpe_ratio(returns: pd.Series, cumulative: bool = False, 
+                 entries_per_year:int = 252, risk_free_rate:float = 0) -> float:
+    """
+    Calculates Annualized Sharpe Ratio for pd.Series of returns.
+        
+    :param returns: (pd.Series) returns
+    :param cumulative: (bool) flag if returns are cumulative (no by default)
+    :param entries_per_year: (int) times returns are recorded per year (days by default)
+    :param risk_free_rate: (float) risk-free rate (0 by default)
+    :return: (float) Annualized Sharpe Ratio
+    """  
+    if cumulative:
+        returns = returns.diff() # Converting to simple returns
+    sharpe_ratio = (returns.mean() - risk_free_rate) / returns.std() * (entries_per_year)**(1/2)
+
+    return sharpe_ratio
+
+
+def probabalistic_sharpe_ratio(observed_SR: float, benchmark_SR: float, 
+                               number_of_returns:int , skewness_of_returns:float = 0, 
+                               kurtosis_of_returns: float = 3) -> float:
+    """
+    Calculates the probabilistic Sharpe ratio (PSR) that provides an adjusted estimate of SR, 
+    by removing the inflationary effect caused by short series with skewed and/or 
+    fat-tailed returns.
+    
+    Given a user-defined benchmark Sharpe ratio and an observed Sharpe ratio,
+    PSR estimates the probability that SR ̂is greater than a hypothetical SR. 
+    
+    - It should exceed 0.95, for the standard significance level of 5%. 
+    - It can be computed on absolute or relative returns.
+    
+    :param observed_SR: (float) Sharpe Ratio that is observed
+    :param benchmark_SR: (float) Sharpe Ratio to which observed_SR is tested against
+    :param  number_of_returns: (int) times returns are recorded for observed_SR
+    :param skewness_of_returns: (float) skewness of returns (as Gaussian by default)
+    :param kurtosis_of_returns: (float) kurtosis of returns (as Gaussian by default)
+    :return: (float) Probabalistic Sharpe Ratio
+    """  
+    
+    
+    PSR = ss.norm.cdf(((observed_SR - benchmark_SR) * (number_of_returns - 1)**(1/2)) / \
+                      (1 - skewness_of_returns * observed_SR + 
+                       (kurtosis_of_returns - 1)/4 * observed_SR**2)**(1/2))
+
+    return PSR
+
+
+def deflated_sharpe_ratio(observed_SR: float, SR_estimates: list, 
+                          number_of_returns:int , skewness_of_returns:float = 0, 
+                          kurtosis_of_returns: float = 3) -> float:
+    """
+    Calculates the deflated Sharpe ratio (DSR) - a PSR where the rejection threshold is 
+    adjusted to reflect the multiplicity of trials. DSR is estimated as PSR[SR∗], where 
+    the benchmark Sharpe ratio, SR∗, is no longer user-defined, but calculated from
+    SR estimate trails.
+    
+    DSR corrects SR for inflationary effects caused by non-Normal returns, track record 
+    length, and multiple testing/selection bias.
+  
+    - It should exceed 0.95, for the standard significance level of 5%. 
+    - It can be computed on absolute or relative returns.
+    
+    :param observed_SR: (list) Sharpe Ratios that is being tested
+    :param SR_estimates: (float) Sharpe Ratios estimates trials
+    :param  number_of_returns: (int) times returns are recorded for observed_SR
+    :param skewness_of_returns: (float) skewness of returns (as Gaussian by default)
+    :param kurtosis_of_returns: (float) kurtosis of returns (as Gaussian by default)
+    :return: (float) Probabalistic Sharpe Ratio
+    """  
+    # Calculating benchmark_SR from a list of estimates
+    benchmark_SR = np.array(SR_estimates).std() * ((1 - np.euler_gamma) * 
+                                                   ss.norm.ppf(1 - 1/len(SR_estimates)) + 
+                                                   np.euler_gamma * ss.norm.ppf(1 - 1/len(SR_estimates) * np.e**(-1)))
+    
+    DSR = probabalistic_sharpe_ratio(observed_SR, benchmark_SR, number_of_returns, skewness_of_returns, 
+                                     kurtosis_of_returns)
+
+    return DSR
