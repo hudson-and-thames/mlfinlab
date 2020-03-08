a2d4ce79d39ed0847a17e6101329dda72a3d90a7
proskurin <proskurinolexandr@gmail.com>
2020-03-08 22:07:39 +0200

    Add t-values to labelling output

--- mlfinlab/labeling/trend_scanning.py
+++ mlfinlab/labeling/trend_scanning.py
@@ -22,13 +22,14 @@ def get_trend_scanning_labels(price_series: pd.Series, t_events: list, look_forw
 
     t1_array = []  # Array of label end times
     ret_array = []  # Array of returns
+    t_values_array = []  # Array of trend t-values
 
     for index in t_events:
         subset = price_series.loc[index:].iloc[:look_forward_window]  # Take t:t+L window
         if subset.shape[0] >= look_forward_window:
             # Loop over possible look-ahead windows to get the one which yields maximum t values for b_1 regression coef
             max_t_value = -np.inf  # Maximum t-value of b_1 coefficient among l values
-            max_t_value_l = None  # Index with maximum t-value
+            max_t_value_index = None  # Index with maximum t-value
 
             # Get optimal label endtime value based on regression t-statistics
             for forward_window in np.arange(subset.shape[0]):
@@ -46,18 +47,19 @@ def get_trend_scanning_labels(price_series: pd.Series, t_events: list, look_forw
                     t_beta_1 = abs(b_mean_[1] / b_std_[1, 1])[0]
                     if t_beta_1 > max_t_value:
                         max_t_value = t_beta_1
-                        max_t_value_l = forward_window
+                        max_t_value_index = forward_window
 
             # Store label information (t1, return)
-            label_endtime_index = subset.index[max_t_value_l]
+            label_endtime_index = subset.index[max_t_value_index]
             price_pct_change = subset.loc[label_endtime_index] / subset.iloc[0] - 1
             t1_array.append(label_endtime_index)
             ret_array.append(float(price_pct_change))
+            t_values_array.append(max_t_value)
 
         else:
             t1_array.append(None)
             ret_array.append(None)
 
-    labels = pd.DataFrame({'t1': t1_array, 'ret': ret_array}, index=t_events)
+    labels = pd.DataFrame({'t1': t1_array, 'ret': ret_array, 't_value': t_values_array}, index=t_events)
     labels['bin'] = np.sign(labels.ret)
     return labels
