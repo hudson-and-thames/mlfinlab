f91133db9081fdc6ff40ce3949bce90dc413b8bd
Jacques Joubert <jacques@quantsportal.com>
2020-03-11 23:01:39 +0000

    Update __init__.py

--- mlfinlab/__init__.py
+++ mlfinlab/__init__.py
@@ -36,10 +36,12 @@ except webbrowser.Error as error:
 print()
 print()
 print(textwrap.dedent("""\
-MlFinLab needs you! We need your help to keep on maintaining and implementing academic research based on financial 
-machine learning, for open-source. In order for us to continue we need to raise $4000 of monthly donations via Patreon -
-by December 2020. If we can't reach our goal, we intend to leave the package as open-source but to make the documentation
-a paid for service. 
+MlFinLab needs you! We need your help for us to keep on maintaining and implementing academic research based on 
+financial machine learning (for open-source). In order for us to continue we need to raise $4000 of monthly donations
+via Patreon - by December 2020. If we can't reach our goal, we will need to adopt more of a paid for service. We thought
+that the best and least impactful course of action (should we not reach our goal) is to leave the package as open-source
+but to make the documentation (ReadTheDocs) a paid for service. This is the ultimate litmus test, if the package is a 
+value add, then we need the community to help us keep it going.
 
 Our road map for 2020 is to implement the text book: Machine Learning for Asset Managers by Marcos Lopez de Prado, 
 as well as a few papers from the Journal of Financial Data Science. We are hiring a full time developer for 3 months 
