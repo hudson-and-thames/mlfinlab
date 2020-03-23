5793112343ce8efd59244be982675648de81fa99
Harkishan-99 <harkishansinghbaniya@gmail.com>
2020-03-23 14:34:53 +0530

    Review Updates and Feature & Documentation Enhancement

--- mlfinlab/feature_importance/cfi.py
+++ mlfinlab/feature_importance/cfi.py
@@ -1,35 +1,97 @@
 """
-An Implementation of the Clustered Feature Importance algorithm described by
-Dr Marcos Lopez de Prado in 'Clustered Feature Importance (Presentation Slides)'
-(https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3517595)
+This script implements the creation of dependence and distance matrix for the
+methods of Information Codependence.
 """
 
 #Imports
 import numpy as np
 import pandas as pd
 import mlfinlab as ml
-from sklearn.metrics import log_loss
-from scipy.cluster.hierarchy import linkage, fcluster
-from information_matrix import  get_dependence_matrix , get_distance_matrix
+from sklearn.metrics import *
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import linkage,fcluster
+from mlfinlab.codependence import variation_of_information_score,get_mutual_info,distance_correlation
+from mlfinlab.codependence.correlation import angular_distance,absolute_angular_distance,\
+                                              squared_angular_distance,distance_correlation
+
+def get_dependence_matrix(df : pd.DataFrame, dependence_method : str ):
+    """
+    This function returns a dependence matrix for the given method of dependence method.
+
+    :param df: (pd.DataFrame) of features
+    :param dependence_method: (str) the algorithm to be use for generating dependence_matrix,
+                                either 'information_variation' or 'mutual_information' or 'distance_correlation' .
+    :return: (pd.DataFrame)  of dependence_matrix
+    """
+
+    features_cols = df.columns.values #Get the feature names
+    #Defining the dependence function
+    if dependence_method == 'information_variation':
+        dep_function = variation_of_information_score
+    elif dependence_method == 'mutual_information':
+        dep_function = get_mutual_info
+    elif dependence_method == 'distance_correlation':
+        dep_function = distance_correlation
+    else:
+        raise ValueError(f"{base_algorithm} is not a valid method. Use either 'information_variation' \
+                            or 'mutual_information' or 'distance_correlation'.")
+    #Generating the dependence_matrix for the defined method
+    if dependence_method != 'distance_correlation':
+        dependence_matrix = [[dep_function(df[x].values,df[y].values,normalize=True) for x in features_cols ] for y in features_cols]
+    else:
+        dependence_matrix = [[dep_function(df[x].values,df[y].values) for x in features_cols ] for y in features_cols]
+    dependence_df = pd.DataFrame(data=dependence_matrix,index=features_cols,columns=features_cols) # dependence_matrix converted to a DataFrame
+    if dependence_method == 'information_variation':
+        return 1 - dependence_df  # IV is reverse, 1 - independent, 0 - similar
+    else:
+        return dependence_df
 
 
-def cluster_features(X : pd.DataFrame , linkage_method : str , n_clusters : int = None ):
+def get_distance_matrix(X:pd.DataFrame, distance_metric:str = 'angular'):
+    """
+    Apply distance operator to dependence matrix.
+
+    :param X: (pd.DataFrame) to which distance operator to be applied.
+    :param distance_metric: (str) the distance operator to be used for generating the distance matrix.
+                                The methods that can be applied are -
+                                'angular' , 'squared_angular' and 'absolute_angular'.
+    :return: (pd.DataFrame) of distance matrix
+    """
+    if distance_metric == 'angular':
+        distfun = lambda x: ((1 - x).round(5) / 2.) ** .5
+    elif distance_metric == 'abs_angular':
+        distfun = lambda x: ((1 - abs(x)).round(5) / 2.) ** .5
+    elif distance_metric == 'squared_angular':
+        distfun = lambda x: ((1 - x ** 2).round(5) / 2.) ** .5
+    else:
+        raise ValueError('Unknown distance metric')
+    return distfun(X).fillna(0)
+
+def get_feature_clusters(X : pd.DataFrame, dependence_metric:str, distance_metric:str, linkage_method : str , n_clusters : int = None):
     '''
     Get Clusters containing features subsets
 
-    (param) X : (pd.DataFrame) of features
-    (param) linkage_method : (str) method of linkage to be used for clustering. Methods include -
+    :param X : (pd.DataFrame) of features
+    :param dependence_metric : (str) method to be use for generating dependence_matrix,
+                                either 'linear' or 'information_variation' or 'mutual_information' or 'distance_correlation' .
+    :param distance_metric: (str) the distance operator to be used for generating the distance matrix.
+                                The methods that can be applied are -
+                                'angular' , 'squared_angular' and 'absolute_angular'.
+    :param linkage_method : (str) method of linkage to be used for clustering. Methods include -
                                   'single' , 'ward' , 'complete' , 'average' , 'weighted' and 'centroid'.
-    (param) C : (int) number of clusters to form.
-                      Must be less the total number of features.
-                      If None then it returns optimal number of clusters
-                      decided by the ONC Algorithm.
+    :param n_clusters : (int) number of clusters to form. Must be less the total number of features.
+                      If None then it returns optimal number of clusters decided by the ONC Algorithm.
 
     (return) : (array) of feature subsets
     '''
-    vi_matrix = get_dependence_matrix(df =X , base_algorithm = 'variation_of_information')
-    dist_matrix = get_distance_matrix(vi_matrix)
-    link = linkage(dist_matrix, method = linkage_method)
+    #Get the dependence metrix
+    if dependence_metric != 'linear':
+        dep_matrix = get_dependence_matrix(X , dependence_method = dependence_metric)
+    else:
+        dep_matrix = X.corr()
+    #Apply distance operator on the dependence matrix
+    dist_matrix = get_distance_matrix(dep_matrix)
+    link = linkage(squareform(dist_matrix), method = linkage_method)
 
     if n_clusters is not None:
         if n_clusters >= len(X.columns):
@@ -39,20 +101,18 @@ def cluster_features(X : pd.DataFrame , linkage_method : str , n_clusters : int 
         return clustered_subsets
 
     else:
-        n_clusters = len(ml.clustering.get_onc_clusters(vi_matrix)[1])
+        n_clusters = len(ml.clustering.get_onc_clusters(dep_matrix)[1])
         clusters = fcluster(link, t = n_clusters, criterion='maxclust')
         clustered_subsets = [[f for c, f in zip(clusters, X.columns) if c == ci] for ci in range(1, n_clusters+1)]
         return clustered_subsets
 
-def clustered_feature_importance(model, X, y, cv_gen, n_clusters=None, linkage_method='single', sample_weight=None, scoring=log_loss):
+def clustered_feature_importance(model, X, y, cv_gen, n_clusters=None, dependence_metric:str='information_variation', distance_metric:str='angular', linkage_method='single', sample_weight=None, scoring=log_loss):
     """
-    Clustered Feature Importnance or Clustered MDA is modified verison of MDA (Mean Decreased Accuracy). It is very
+    Clustered Feature Importnance or Clustered MDA is the modified verison of MDA (Mean Decreased Accuracy). It is
     robust to substitution effect that takes place when two or more explanatory variables share a substantial amount of
-    information (predictive power), while other feature importance method may struggle.
-
-    First, apply a single linkage agglomerative clustering algorithm on variation of information (VI) matix, since VI can
-    effectively measure linear and non-linear codependence. Secondly, instead of shuffling (permutating) all variables
-    individually (like in MDA), we shuffle all variables in cluster together.Next, we follow all the rest of the steps
+    information (predictive power).
+    First, apply a single linkage agglomerative clustering algorithm on a dependence matrix. Secondly, instead of shuffling (permutating)
+    all variables individually (like in MDA), we shuffle all variables in cluster together. Next, we follow all the rest of the steps
     as in MDA.
 
     :param model: (sklearn.Classifier): Any sklearn classifier.
@@ -61,24 +121,28 @@ def clustered_feature_importance(model, X, y, cv_gen, n_clusters=None, linkage_m
     :param cv_gen: (cross_validation.PurgedKFold): Cross-validation object.
     :param n_clusters: (int): number of clustered subsets of features to form,
                               if None then optimal number of clusters is decided by the ONC Algorithm.
+    :param dependence_metric : (str) method to be use for generating dependence_matrix,
+                                either 'linear' or 'information_variation' or 'mutual_information' or 'distance_correlation' .
+                                Default is 'information_variation' which is robust to both linear and non-linear substitution effect.
+    :param distance_metric: (str) the distance operator to be used for generating the distance matrix.
+                                The methods that can be applied are -
+                                'angular' , 'squared_angular' and 'absolute_angular'.
     :param linkage_method : (str) method of linkage to be used for clustering. Methods include -
                                   'single' , 'ward' , 'complete' , 'average' , 'weighted' and 'centroid'.
     :param sample_weight: (np.array): Sample weights, if None equal to ones.
-    :param scoring: (function): Scoring function used to determine importance.
+    :param scoring: (function): Scoring function used to determine importance. Any sklearn scoring function.  Default is log_loss
     :return: (pd.DataFrame): Mean and standard deviation of feature importance.
     """
-
-    clustered_subsets = cluster_features(X,linkage_method,n_clusters)
+    #Generating the feature subsets
+    clustered_subsets = get_feature_clusters(X, dependence_metric,distance_metric,linkage_method,n_clusters)
 
     if sample_weight is None:
         sample_weight = np.ones((X.shape[0],))
 
     fold_metrics_values, features_metrics_values = pd.Series(), pd.DataFrame(columns=X.columns)
-
     for i, (train, test) in enumerate(cv_gen.split(X=X)):
         fit = model.fit(X=X.iloc[train, :], y=y.iloc[train], sample_weight=sample_weight[train])
         pred = fit.predict(X.iloc[test, :])
-
         # Get overall metrics value on out-of-sample fold
         if scoring == log_loss:
             prob = fit.predict_proba(X.iloc[test, :])
@@ -86,7 +150,6 @@ def clustered_feature_importance(model, X, y, cv_gen, n_clusters=None, linkage_m
                                                   labels=model.classes_)
         else:
             fold_metrics_values.loc[i] = scoring(y.iloc[test], pred, sample_weight=sample_weight[test])
-
         # Get feature specific metric on out-of-sample fold
         for j in clustered_subsets: #Instead of Individual Features , clustered subsets are used
             X1_ = X.iloc[test, :].copy(deep=True)
