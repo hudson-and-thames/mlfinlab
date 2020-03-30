894104e83e60c1b8b261a0eb53b25ec299ce3d06
Harkishan-99 <harkishansinghbaniya@gmail.com>
2020-03-30 20:34:33 +0530

    CFI is shifted to importance module

--- mlfinlab/feature_importance/__init__.py
+++ mlfinlab/feature_importance/__init__.py
@@ -4,9 +4,8 @@ from the Journal of Financial Data Science.
 """
 
 from mlfinlab.feature_importance.importance import (mean_decrease_impurity, mean_decrease_accuracy,
-                                                    single_feature_importance, plot_feature_importance)
+                                                    single_feature_importance,clustered_feature_importance,
+                                                    plot_feature_importance)
 from mlfinlab.feature_importance.orthogonal import (feature_pca_analysis, get_pca_rank_weighted_kendall_tau,
                                                     get_orthogonal_features)
 from mlfinlab.feature_importance.fingerpint import RegressionModelFingerprint, ClassificationModelFingerprint
-
-from mlfinlab.feature_importance.cfi import clustered_feature_importance
