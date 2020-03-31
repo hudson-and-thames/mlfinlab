cfaecd3e2236ddb8b272ce8a2fea62584421fb45
Illya Barziy <panpipp@gmail.com>
2020-03-31 16:54:51 +0200

    Unit tests for MCOS and Sample Data generating functions

--- mlfinlab/tests/test_nco.py
+++ mlfinlab/tests/test_nco.py
@@ -3,12 +3,8 @@ Tests the Nested Clustered Optimization (NCO) algorithm.
 """
 
 import unittest
-#import os
-
 import numpy as np
 import pandas as pd
-
-
 from mlfinlab.portfolio_optimization.nco import NCO
 
 
@@ -72,7 +68,6 @@ class TestNCO(unittest.TestCase):
         # Testing that the distribution curve is right
         self.assertTrue(pdf_mp.values[1] > pdf_mp.values[2] > pdf_mp.values[3])
 
-
     def test_fit_kde(self):
         """
         Test the kernel fitting to a series of observations.
@@ -117,9 +112,6 @@ class TestNCO(unittest.TestCase):
         # Testing the SSE value
         self.assertAlmostEqual(pdf_kde, 50.51326, delta=1e-5)
 
-
-
-
     def test_find_max_eval(self):
         """
         Test the search for maximum random eigenvalue.
@@ -139,8 +131,8 @@ class TestNCO(unittest.TestCase):
         self.assertAlmostEqual(maximum_eigen, 2.41011, delta=1e-5)
         self.assertAlmostEqual(var, 0.82702, delta=1e-5)
 
-
-    def test_corr_to_cov(self):
+    @staticmethod
+    def test_corr_to_cov():
         """
         Test the recovering of the covariance matrix from the correlation matrix.
         """
@@ -154,9 +146,9 @@ class TestNCO(unittest.TestCase):
         std_vec = np.array([0.1, 0.2, 0.1])
 
         # Expected covariance matrix
-        expected_matrix = np.array([[ 0.01 ,  0.002, -0.001],
-                                    [ 0.002,  0.04 , -0.006],
-                                    [-0.001, -0.006,  0.01 ]])
+        expected_matrix = np.array([[0.01, 0.002, -0.001],
+                                    [0.002, 0.04, -0.006],
+                                    [-0.001, -0.006, 0.01]])
 
         # Finding the covariance matrix
         cov_matrix = nco.corr_to_cov(corr_matrix, std_vec)
@@ -164,7 +156,8 @@ class TestNCO(unittest.TestCase):
         # Testing the first row of the matrix
         np.testing.assert_almost_equal(cov_matrix, expected_matrix, decimal=5)
 
-    def test_cov_to_corr(self):
+    @staticmethod
+    def test_cov_to_corr():
         """
         Test the deriving of the correlation matrix from a covariance matrix.
         """
@@ -188,8 +181,8 @@ class TestNCO(unittest.TestCase):
         # Testing the first row of the matrix
         np.testing.assert_almost_equal(corr_matrix, expected_matrix, decimal=5)
 
-
-    def test_get_pca(self):
+    @staticmethod
+    def test_get_pca():
         """
         Test the calculation of eigenvalues and eigenvectors from a Hermitian matrix.
         """
@@ -202,10 +195,10 @@ class TestNCO(unittest.TestCase):
                                 [-0.1, -0.3, 1]])
 
         # Expected correlation matrix
-        expected_eigenvalues = np.array([[1.3562, 0,        0],
-                                         [0,      0.9438,   0],
-                                         [0,      0,      0.7]])
-        first_eigenvector = np.array([-3.69048184e-01, -9.29410263e-01,  1.10397126e-16])
+        expected_eigenvalues = np.array([[1.3562, 0, 0],
+                                         [0, 0.9438, 0],
+                                         [0, 0, 0.7]])
+        first_eigenvector = np.array([-3.69048184e-01, -9.29410263e-01, 1.10397126e-16])
 
         # Finding the eigenvalues
         eigenvalues, eigenvectors = nco.get_pca(corr_matrix)
@@ -214,7 +207,8 @@ class TestNCO(unittest.TestCase):
         np.testing.assert_almost_equal(eigenvalues, expected_eigenvalues, decimal=4)
         np.testing.assert_almost_equal(eigenvectors[0], first_eigenvector, decimal=5)
 
-    def test_denoised_corr(self):
+    @staticmethod
+    def test_denoised_corr():
         """
         Test the shrinkage the eigenvalues associated with noise.
         """
@@ -225,14 +219,14 @@ class TestNCO(unittest.TestCase):
         eigenvalues = np.array([[1.3562, 0, 0],
                                 [0, 0.9438, 0],
                                 [0, 0, 0.7]])
-        eigenvectors = np.array([[-3.69048184e-01, -9.29410263e-01,  1.10397126e-16],
-                                [-6.57192300e-01,  2.60956474e-01,  7.07106781e-01],
-                                [ 6.57192300e-01, -2.60956474e-01,  7.07106781e-01]])
+        eigenvectors = np.array([[-3.69048184e-01, -9.29410263e-01, 1.10397126e-16],
+                                 [-6.57192300e-01, 2.60956474e-01, 7.07106781e-01],
+                                 [6.57192300e-01, -2.60956474e-01, 7.07106781e-01]])
 
         # Expected correlation matrix
-        expected_corr = np.array([[1,  0.13353165, -0.13353165],
-                                  [0.13353165,  1, -0.21921986],
-                                  [-0.13353165, -0.21921986,  1]])
+        expected_corr = np.array([[1, 0.13353165, -0.13353165],
+                                  [0.13353165, 1, -0.21921986],
+                                  [-0.13353165, -0.21921986, 1]])
 
 
         # Finding the eigenvalues
@@ -241,7 +235,8 @@ class TestNCO(unittest.TestCase):
         # Testing if the de-noised correlation matrix is right
         np.testing.assert_almost_equal(corr_matrix, expected_corr, decimal=4)
 
-    def de_noised_cov(self):
+    @staticmethod
+    def de_noised_cov():
         """
         Test the shrinkage the eigenvalues associated with noise.
         """
@@ -256,9 +251,9 @@ class TestNCO(unittest.TestCase):
         kde_bwidth = 0.25
 
         # Expected de-noised covariance matrix
-        expected_cov = np.array([[ 0.01,  0.00267029, -0.00133514],
-                                 [ 0.00267029,  0.04, -0.00438387],
-                                 [-0.00133514, -0.00438387,  0.01]])
+        expected_cov = np.array([[0.01, 0.00267029, -0.00133514],
+                                 [0.00267029, 0.04, -0.00438387],
+                                 [-0.00133514, -0.00438387, 0.01]])
 
 
         # Finding the de-noised covariance matrix
@@ -284,9 +279,9 @@ class TestNCO(unittest.TestCase):
         n_init = 10
 
         # Expected correlation matrix of clustered elements, clusters and, Silhouette Coefficient series
-        expected_corr = pd.DataFrame([[1,  0.1, -0.1],
-                                     [0.1,   1,  -0.3],
-                                     [-0.1,- 0.3,   1]])
+        expected_corr = pd.DataFrame([[1, 0.1, -0.1],
+                                      [0.1, 1, -0.3],
+                                      [-0.1, - 0.3, 1]])
         expected_clust = {0: [0, 1], 1: [2]}
         expected_silh_coef = pd.Series([0.100834, 0.167626, 0], index=[0, 1, 2])
 
@@ -299,7 +294,8 @@ class TestNCO(unittest.TestCase):
         np.testing.assert_almost_equal(np.array(corr), np.array(expected_corr), decimal=4)
         np.testing.assert_almost_equal(np.array(silh_coef), np.array(expected_silh_coef), decimal=4)
 
-    def test_opt_port(self):
+    @staticmethod
+    def test_opt_port():
         """
         Test the estimates of the Convex Optimization Solution (CVO).
         """
@@ -307,9 +303,9 @@ class TestNCO(unittest.TestCase):
         nco = NCO()
 
         # Covariance matrix
-        cov_matrix = np.array([[0.01,   0.002, -0.001],
-                               [0.002,   0.04, -0.006],
-                               [-0.001, -0.006,  0.01]])
+        cov_matrix = np.array([[0.01, 0.002, -0.001],
+                               [0.002, 0.04, -0.006],
+                               [-0.001, -0.006, 0.01]])
 
         # Expected weights for minimum variance allocation
         w_expected = np.array([[0.37686939],
@@ -322,7 +318,8 @@ class TestNCO(unittest.TestCase):
         # Testing if the optimal allocation is right
         np.testing.assert_almost_equal(w_cvo, w_expected, decimal=4)
 
-    def test_opt_port_nco(self):
+    @staticmethod
+    def test_opt_port_nco():
         """
         Test the estimates the optimal allocation using the (NCO) algorithm
         """
@@ -330,19 +327,146 @@ class TestNCO(unittest.TestCase):
         nco = NCO()
 
         # Covariance matrix
-        cov_matrix = np.array([[0.01,   0.002, -0.001],
-                               [0.002,   0.04, -0.006],
-                               [-0.001, -0.006,  0.01]])
+        np.random.seed(0)
+        cov_matrix = np.array([[0.01, 0.002, -0.001],
+                               [0.002, 0.04, -0.006],
+                               [-0.001, -0.006, 0.01]])
 
         # Expected weights for minimum variance allocation
         w_expected = np.array([[0.43875825],
                                [0.09237016],
-                               [0.4688716 ]])
+                               [0.4688716]])
         max_num_clusters = 2
 
         # Finding the optimal weights
-        w_nco = nco.opt_port_nco(cov_matrix, max_num_clusters = max_num_clusters)
+        w_nco = nco.opt_port_nco(cov_matrix, max_num_clusters=max_num_clusters)
 
         # Testing if the optimal allocation is right
         np.testing.assert_almost_equal(w_nco, w_expected, decimal=4)
 
+    @staticmethod
+    def test_opt_port_mcos():
+        """
+        Test the estimates of the optimal allocation using the Monte Carlo optimization selection
+        """
+
+        nco = NCO()
+
+        # Covariance matrix, mean vector and other variables for the method
+        np.random.seed(0)
+        mu_vec = np.array([0, 0.1, 0.2, 0.3])
+        cov_mat = np.array([[1, 0.1, 0.2, 0.3],
+                            [0.1, 1, 0.1, 0.2],
+                            [0.2, 0.1, 1, 0.1],
+                            [0.3, 0.2, 0.1, 1]])
+        num_obs = 100
+        num_sims = 2
+        kde_bwidth = 0.25
+        min_var_portf = True
+        lw_shrinkage = False
+
+        # Expected weights for minimum variance allocation
+        w_cvo_expected = pd.DataFrame([[0.249287, 0.256002, 0.242593, 0.252118],
+                                       [0.257547, 0.265450, 0.242453, 0.234551]])
+
+        w_nco_expected = pd.DataFrame([[0.248396, 0.243172, 0.250751, 0.257680],
+                                       [0.257547, 0.265450, 0.242453, 0.234551]])
+
+        # Finding the optimal weights
+        w_cvo, w_nco = nco.opt_port_mcos(mu_vec, cov_mat, num_obs, num_sims, kde_bwidth, min_var_portf, lw_shrinkage)
+
+        # Testing if the optimal allocation simulations are right
+        np.testing.assert_almost_equal(np.array(w_cvo), np.array(w_cvo_expected), decimal=4)
+        np.testing.assert_almost_equal(np.array(w_nco), np.array(w_nco_expected), decimal=4)
+
+    @staticmethod
+    def test_estim_errors_mcos():
+        """
+        Test the computation the true optimal allocation w, and compares that result with the estimated ones by MCOS.
+        """
+
+        nco = NCO()
+
+        # Weights from CVO and NCO, and data to estimate the true optimal weights
+        np.random.seed(0)
+        w_cvo = pd.DataFrame([[0.249287, 0.256002, 0.242593, 0.252118],
+                              [0.257547, 0.265450, 0.242453, 0.234551]])
+
+        w_nco = pd.DataFrame([[0.248396, 0.243172, 0.250751, 0.257680],
+                              [0.257547, 0.265450, 0.242453, 0.234551]])
+
+        mu_vec = np.array([0, 0.1, 0.2, 0.3])
+        cov_mat = np.array([[1, 0.1, 0.2, 0.3],
+                            [0.1, 1, 0.1, 0.2],
+                            [0.2, 0.1, 1, 0.1],
+                            [0.3, 0.2, 0.1, 1]])
+        min_var_portf = True
+
+        # Expected errors
+        err_cvo_expected = 0.0062604
+        err_nco_expected = 0.0111115
+
+        # Finding the errors in estimations
+        err_cvo, err_nco = nco.estim_errors_mcos(w_cvo, w_nco, mu_vec, cov_mat, min_var_portf)
+
+        # Testing if the error computation is right
+        np.testing.assert_almost_equal(err_cvo, err_cvo_expected, decimal=4)
+        np.testing.assert_almost_equal(err_nco, err_nco_expected, decimal=4)
+
+    @staticmethod
+    def test_form_block_matrix():
+        """
+        Test the creation of a block correlation matrix with given parameters.
+        """
+
+        nco = NCO()
+
+        # Parameters to build the block correlation matrix with
+        num_blocks = 2
+        block_size = 2
+        block_corr = 0.3
+
+        # Expected corr matrix
+        corr_expected = np.array([[1, 0.3, 0, 0],
+                                  [0.3, 1, 0, 0],
+                                  [0, 0, 1, 0.3],
+                                  [0, 0, 0.3, 1]])
+
+        # Finding the errors in estimations
+        corr_matrix = nco.form_block_matrix(num_blocks, block_size, block_corr)
+
+        # Testing if the error computation is right
+        np.testing.assert_almost_equal(corr_matrix, corr_expected, decimal=4)
+
+    @staticmethod
+    def test_form_true_matrix():
+        """
+        Test the creation of a random vector of means and a random covariance matrix.
+        """
+
+        nco = NCO()
+
+        # Parameters to build the vector of means and a covariance matrix
+        np.random.seed(0)
+        num_blocks = 2
+        block_size = 2
+        block_corr = 0.3
+        std = 0.3
+
+        # Expected vector of means and covariance matrix
+        mu_expected = np.array([[0.5936214],
+                                [0.97226796],
+                                [0.8602674],
+                                [0.00681664]])
+
+        cov_expected = pd.DataFrame([[0.09, 0.027, 0, 0],
+                                     [0.027, 0.09, 0, 0],
+                                     [0, 0, 0.09, 0.027],
+                                     [0, 0, 0.027, 0.09]])
+
+        # Finding the random vector of means and covariance matrix
+        mu_vec, cov_matrix = nco.form_true_matrix(num_blocks, block_size, block_corr, std)
+
+        # Testing if the results are right
+        np.testing.assert_almost_equal(mu_vec, mu_expected, decimal=4)
+        np.testing.assert_almost_equal(np.array(cov_matrix), np.array(cov_expected), decimal=4)
