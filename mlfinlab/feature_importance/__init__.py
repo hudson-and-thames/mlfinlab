913e673c2df2cf31eecb4b33379d1c59b695e1fa
Harkishan-99 <harkishansinghbaniya@gmail.com>
2020-04-01 12:26:38 +0530

    white space fix

--- mlfinlab/feature_importance/__init__.py
+++ mlfinlab/feature_importance/__init__.py
@@ -4,7 +4,7 @@ from the Journal of Financial Data Science.
 """
 
 from mlfinlab.feature_importance.importance import (mean_decrease_impurity, mean_decrease_accuracy,
-                                                    single_feature_importance,clustered_feature_importance,
+                                                    single_feature_importance, clustered_feature_importance,
                                                     plot_feature_importance)
 from mlfinlab.feature_importance.orthogonal import (feature_pca_analysis, get_pca_rank_weighted_kendall_tau,
                                                     get_orthogonal_features)
