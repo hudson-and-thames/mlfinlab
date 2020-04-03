0347da7e7ddb22c16efefd3061b0350ef9f5c25a
Aditya Vyas <adityavyas17@gmail.com>
2020-04-03 17:49:27 -0400

    Make asset_names complimentary

--- mlfinlab/portfolio_optimization/nco.py
+++ mlfinlab/portfolio_optimization/nco.py
@@ -23,6 +23,182 @@ class NCO:
         return
 
     @staticmethod
+    def allocate_cvo(cov, mu_vec=None):
+        """
+        Estimates the Convex Optimization Solution (CVO).
+
+        Uses the covariance matrix and the mu - optimal solution.
+        If mu is the vector of expected values from variables, the result will be
+        a vector of weights with maximum Sharpe ratio.
+        If mu is a vector of ones, the result will be a vector of weights with
+        minimum variance.
+
+        :param cov: (np.array) Covariance matrix of the variables.
+        :param mu_vec: (np.array) Expected value of draws from the variables for maximum Sharpe ratio.
+                              None if outputting the minimum variance portfolio.
+        :return: (np.array) Weights for optimal allocation.
+        """
+
+        # Calculating the inverse covariance matrix
+        inv_cov = np.linalg.inv(cov)
+
+        # Generating a vector of size of the inverted covariance matrix
+        ones = np.ones(shape=(inv_cov.shape[0], 1))
+
+        if mu_vec is None:  # To output the minimum variance portfolio
+            mu_vec = ones
+
+        # Calculating the analytical solution using CVO - weights
+        w_cvo = np.dot(inv_cov, mu_vec)
+        w_cvo /= np.dot(mu_vec.T, w_cvo)
+
+        return w_cvo
+
+    def allocate_nco(self, cov, mu_vec=None, max_num_clusters=None):
+        """
+        Estimates the optimal allocation using the nested clustered optimization (NCO) algorithm.
+
+        First, it clusters the covariance matrix into subsets of highly correlated variables.
+        Second, it computes the optimal allocation for each of the clusters separately.
+        This allows collapsing of the original covariance matrix into a reduced covariance matrix,
+        where each cluster is represented by a single variable.
+        Third, we compute the optimal allocations across the reduced covariance matrix.
+        Fourth, the final allocations are the dot-product of the intra-cluster (step 2) allocations and
+        the inter-cluster (step 3) allocations.
+
+        For the Convex Optimization Solution (CVO), a mu - optimal solution parameter is needed.
+        If mu is the vector of expected values from variables, the result will be
+        a vector of weights with maximum Sharpe ratio.
+        If mu is a vector of ones (pass None value), the result will be a vector of weights with
+        minimum variance.
+
+        :param cov: (np.array) Covariance matrix of the variables.
+        :param mu_vec: (np.array) Expected value of draws from the variables for maximum Sharpe ratio.
+                              None if outputting the minimum variance portfolio.
+        :param max_тum_сlusters: (int) Allowed maximum number of clusters.
+        :return: (np.array) Optimal allocation using the NCO algorithm.
+        """
+
+        # Using pd.DataFrame instead of np.array
+        cov = pd.DataFrame(cov)
+
+        # Optimal solution for minimum variance
+        if mu_vec is not None:
+            mu_vec = pd.Series(mu_vec[:, 0])
+
+        # Class with function to calculate correlation from covariance function
+        risk_estimators = RiskEstimators()
+
+        # Calculating correlation matrix
+        corr = risk_estimators.cov_to_corr(cov)
+
+        # Optimal partition of clusters (step 1)
+        corr, clusters, _ = self._cluster_kmeans_base(corr, max_num_clusters, n_init=10)
+
+        # Weights inside clusters
+        w_intra_clusters = pd.DataFrame(0, index=cov.index, columns=clusters.keys())
+
+        # Iterating over clusters
+        for i in clusters:
+            # Covariance matrix of elements in cluster
+            cov_cluster = cov.loc[clusters[i], clusters[i]].values
+
+            # Optimal solution vector for the cluster
+            mu_cluster = (None if mu_vec is None else mu_vec.loc[clusters[i]].values.reshape(-1, 1))
+
+            # Estimating the Convex Optimization Solution in a cluster (step 2)
+            w_intra_clusters.loc[clusters[i], i] = self.allocate_cvo(cov_cluster, mu_cluster).flatten()
+
+        # Reducing new covariance matrix to calculate inter-cluster weights
+        cov_inter_cluster = w_intra_clusters.T.dot(np.dot(cov, w_intra_clusters))
+        mu_inter_cluster = (None if mu_vec is None else w_intra_clusters.T.dot(mu_vec))
+
+        # Optimal allocations across the reduced covariance matrix (step 3)
+        w_inter_clusters = pd.Series(self.allocate_cvo(cov_inter_cluster, mu_inter_cluster).flatten(),
+                                     index=cov_inter_cluster.index)
+
+        # Final allocations - dot-product of the intra-cluster and inter-cluster allocations (step 4)
+        w_nco = w_intra_clusters.mul(w_inter_clusters, axis=1).sum(axis=1).values.reshape(-1, 1)
+
+        return w_nco
+
+    def allocate_mcos(self, mu_vec, cov, num_obs, num_sims, kde_bwidth, min_var_portf, lw_shrinkage):
+        """
+        Estimates the optimal allocation using the Monte Carlo optimization selection (MCOS) algorithm.
+
+        Repeats the CVO and the NCO algorithms multiple times on the empirical values to get a dataframe of trials
+        in order to later compare them to a true optimal weights allocation and compare the robustness of the NCO
+        and CVO methods.
+
+        :param mu_vec: (np.array) The original vector of expected outcomes.
+        :param cov: (np.array )The original covariance matrix of outcomes.
+        :param num_obs: (int) The number of observations T used to compute mu_vec and cov.
+        :param num_sims: (int) The number of Monte Carlo simulations to run.
+        :param kde_bwidth: (float) The bandwidth of the KDE used to de-noise the covariance matrix.
+        :param min_var_portf: (bool) When True, the minimum variance solution is computed. Otherwise, the
+                                     maximum Sharpe ratio solution is computed.
+        :param lw_shrinkage: (bool) When True, the covariance matrix is subjected to the Ledoit-Wolf shrinkage
+                                    procedure.
+        :return: (pd.dataframe, pd.dataframe) DataFrames with allocations for CVO and NCO algorithms.
+        """
+
+        # Creating DataFrames for CVO results and NCO results
+        w_cvo = pd.DataFrame(columns=range(cov.shape[0]), index=range(num_sims), dtype=float)
+        w_nco = w_cvo.copy(deep=True)
+
+        # Iterating thorough simulations
+        for simulation in range(num_sims):
+            # Deriving empirical vector of means and an empirical covariance matrix
+            mu_simulation, cov_simulation = self._simulate_covariance(mu_vec, cov, num_obs, lw_shrinkage)
+
+            # If goal is minimum variance
+            if min_var_portf:
+                mu_simulation = None
+
+            # Class with de-noising function
+            risk_estimators = RiskEstimators()
+
+            # De-noising covariance matrix
+            if kde_bwidth > 0:
+                cov_simulation = risk_estimators.denoise_covariance(cov_simulation, num_obs / cov_simulation.shape[1],
+                                                                    kde_bwidth)
+
+            # Writing the results to corresponding dataframes
+            w_cvo.loc[simulation] = self.allocate_cvo(cov_simulation, mu_simulation).flatten()
+            w_nco.loc[simulation] = self.allocate_nco(cov_simulation, mu_simulation,
+                                                      int(cov_simulation.shape[0] / 2)).flatten()
+
+        return w_cvo, w_nco
+
+    def estim_errors_mcos(self, w_cvo, w_nco, mu_vec, cov, min_var_portf):
+        """
+        Computes the true optimal allocation w, and compares that result with the estimated ones by MCOS.
+
+        The result is the mean standard deviation between the true weights and the ones obtained from the simulation
+        for each algorithm - CVO and NCO.
+
+        :param w_cvo: (pd.dataframe) DataFrame with weights from the CVO algorithm.
+        :param w_nco: (pd.dataframe) DataFrame with weights from the NCO algorithm.
+        :param mu_vec: (np.array) The original vector of expected outcomes.
+        :param cov: (np.array)The original covariance matrix of outcomes.
+        :param min_var_portf: (bool) When True, the minimum variance solution was computed. Otherwise, the
+                                     maximum Sharpe ratio solution was computed.
+        :return: (float, float) Mean standard deviation of weights for CVO and NCO algorithms.
+        """
+
+        # Calculating the true optimal weights allocation
+        w_true = self.allocate_cvo(cov, None if min_var_portf else mu_vec)
+        w_true = np.repeat(w_true.T, w_cvo.shape[0], axis=0)
+
+        # Mean standard deviation between the weights from CVO algorithm and the true weights
+        err_cvo = (w_cvo - w_true).std(axis=0).mean()
+
+        # Mean standard deviation between the weights from NCO algorithm and the true weights
+        err_nco = (w_nco - w_true).std(axis=0).mean()
+
+        return err_cvo, err_nco
+
+    @staticmethod
     def _simulate_covariance(mu_vector, cov_matrix, num_obs, lw_shrinkage=False):
         """
         Derives an empirical vector of means and an empirical covariance matrix.
@@ -119,181 +295,6 @@ class NCO:
         silh_coef_optimal = pd.Series(silh_coef_optimal, index=dist_matrix.index)
 
         return corr, clusters, silh_coef_optimal
-
-    @staticmethod
-    def allocate_cvo(cov, mu_vec=None):
-        """
-        Estimates the Convex Optimization Solution (CVO).
-
-        Uses the covariance matrix and the mu - optimal solution.
-        If mu is the vector of expected values from variables, the result will be
-        a vector of weights with maximum Sharpe ratio.
-        If mu is a vector of ones, the result will be a vector of weights with
-        minimum variance.
-
-        :param cov: (np.array) Covariance matrix of the variables.
-        :param mu_vec: (np.array) Expected value of draws from the variables for maximum Sharpe ratio.
-                              None if outputting the minimum variance portfolio.
-        :return: (np.array) Weights for optimal allocation.
-        """
-
-        # Calculating the inverse covariance matrix
-        inv_cov = np.linalg.inv(cov)
-
-        # Generating a vector of size of the inverted covariance matrix
-        ones = np.ones(shape=(inv_cov.shape[0], 1))
-
-        if mu_vec is None:  # To output the minimum variance portfolio
-            mu_vec = ones
-
-        # Calculating the analytical solution using CVO - weights
-        w_cvo = np.dot(inv_cov, mu_vec)
-        w_cvo /= np.dot(mu_vec.T, w_cvo)
-
-        return w_cvo
-
-
-    def allocate_nco(self, cov, mu_vec=None, max_num_clusters=None):
-        """
-        Estimates the optimal allocation using the nested clustered optimization (NCO) algorithm.
-
-        First, it clusters the covariance matrix into subsets of highly correlated variables.
-        Second, it computes the optimal allocation for each of the clusters separately.
-        This allows collapsing of the original covariance matrix into a reduced covariance matrix,
-        where each cluster is represented by a single variable.
-        Third, we compute the optimal allocations across the reduced covariance matrix.
-        Fourth, the final allocations are the dot-product of the intra-cluster (step 2) allocations and
-        the inter-cluster (step 3) allocations.
-
-        For the Convex Optimization Solution (CVO), a mu - optimal solution parameter is needed.
-        If mu is the vector of expected values from variables, the result will be
-        a vector of weights with maximum Sharpe ratio.
-        If mu is a vector of ones (pass None value), the result will be a vector of weights with
-        minimum variance.
-
-        :param cov: (np.array) Covariance matrix of the variables.
-        :param mu_vec: (np.array) Expected value of draws from the variables for maximum Sharpe ratio.
-                              None if outputting the minimum variance portfolio.
-        :param max_тum_сlusters: (int) Allowed maximum number of clusters.
-        :return: (np.array) Optimal allocation using the NCO algorithm.
-        """
-
-        # Using pd.DataFrame instead of np.array
-        cov = pd.DataFrame(cov)
-
-        # Optimal solution for minimum variance
-        if mu_vec is not None:
-            mu_vec = pd.Series(mu_vec[:, 0])
-
-        # Class with function to calculate correlation from covariance function
-        risk_estimators = RiskEstimators()
-
-        # Calculating correlation matrix
-        corr = risk_estimators.cov_to_corr(cov)
-
-        # Optimal partition of clusters (step 1)
-        corr, clusters, _ = self._cluster_kmeans_base(corr, max_num_clusters, n_init=10)
-
-        # Weights inside clusters
-        w_intra_clusters = pd.DataFrame(0, index=cov.index, columns=clusters.keys())
-
-        # Iterating over clusters
-        for i in clusters:
-            # Covariance matrix of elements in cluster
-            cov_cluster = cov.loc[clusters[i], clusters[i]].values
-
-            # Optimal solution vector for the cluster
-            mu_cluster = (None if mu_vec is None else mu_vec.loc[clusters[i]].values.reshape(-1, 1))
-
-            # Estimating the Convex Optimization Solution in a cluster (step 2)
-            w_intra_clusters.loc[clusters[i], i] = self.allocate_cvo(cov_cluster, mu_cluster).flatten()
-
-        # Reducing new covariance matrix to calculate inter-cluster weights
-        cov_inter_cluster = w_intra_clusters.T.dot(np.dot(cov, w_intra_clusters))
-        mu_inter_cluster = (None if mu_vec is None else w_intra_clusters.T.dot(mu_vec))
-
-        # Optimal allocations across the reduced covariance matrix (step 3)
-        w_inter_clusters = pd.Series(self.allocate_cvo(cov_inter_cluster, mu_inter_cluster).flatten(), index=cov_inter_cluster.index)
-
-        # Final allocations - dot-product of the intra-cluster and inter-cluster allocations (step 4)
-        w_nco = w_intra_clusters.mul(w_inter_clusters, axis=1).sum(axis=1).values.reshape(-1, 1)
-
-        return w_nco
-
-    def allocate_mcos(self, mu_vec, cov, num_obs, num_sims, kde_bwidth, min_var_portf, lw_shrinkage):
-        """
-        Estimates the optimal allocation using the Monte Carlo optimization selection (MCOS) algorithm.
-
-        Repeats the CVO and the NCO algorithms multiple times on the empirical values to get a dataframe of trials
-        in order to later compare them to a true optimal weights allocation and compare the robustness of the NCO
-        and CVO methods.
-
-        :param mu_vec: (np.array) The original vector of expected outcomes.
-        :param cov: (np.array )The original covariance matrix of outcomes.
-        :param num_obs: (int) The number of observations T used to compute mu_vec and cov.
-        :param num_sims: (int) The number of Monte Carlo simulations to run.
-        :param kde_bwidth: (float) The bandwidth of the KDE used to de-noise the covariance matrix.
-        :param min_var_portf: (bool) When True, the minimum variance solution is computed. Otherwise, the
-                                     maximum Sharpe ratio solution is computed.
-        :param lw_shrinkage: (bool) When True, the covariance matrix is subjected to the Ledoit-Wolf shrinkage
-                                    procedure.
-        :return: (pd.dataframe, pd.dataframe) DataFrames with allocations for CVO and NCO algorithms.
-        """
-
-        # Creating DataFrames for CVO results and NCO results
-        w_cvo = pd.DataFrame(columns=range(cov.shape[0]), index=range(num_sims), dtype=float)
-        w_nco = w_cvo.copy(deep=True)
-
-        # Iterating thorough simulations
-        for simulation in range(num_sims):
-            # Deriving empirical vector of means and an empirical covariance matrix
-            mu_simulation, cov_simulation = self._simulate_covariance(mu_vec, cov, num_obs, lw_shrinkage)
-
-            # If goal is minimum variance
-            if min_var_portf:
-                mu_simulation = None
-
-            # Class with de-noising function
-            risk_estimators = RiskEstimators()
-
-            # De-noising covariance matrix
-            if kde_bwidth > 0:
-                cov_simulation = risk_estimators.denoise_covariance(cov_simulation, num_obs/cov_simulation.shape[1], kde_bwidth)
-
-            # Writing the results to corresponding dataframes
-            w_cvo.loc[simulation] = self.allocate_cvo(cov_simulation, mu_simulation).flatten()
-            w_nco.loc[simulation] = self.allocate_nco(cov_simulation, mu_simulation,
-                                                      int(cov_simulation.shape[0] / 2)).flatten()
-
-        return w_cvo, w_nco
-
-    def estim_errors_mcos(self, w_cvo, w_nco, mu_vec, cov, min_var_portf):
-        """
-        Computes the true optimal allocation w, and compares that result with the estimated ones by MCOS.
-
-        The result is the mean standard deviation between the true weights and the ones obtained from the simulation
-        for each algorithm - CVO and NCO.
-
-        :param w_cvo: (pd.dataframe) DataFrame with weights from the CVO algorithm.
-        :param w_nco: (pd.dataframe) DataFrame with weights from the NCO algorithm.
-        :param mu_vec: (np.array) The original vector of expected outcomes.
-        :param cov: (np.array)The original covariance matrix of outcomes.
-        :param min_var_portf: (bool) When True, the minimum variance solution was computed. Otherwise, the
-                                     maximum Sharpe ratio solution was computed.
-        :return: (float, float) Mean standard deviation of weights for CVO and NCO algorithms.
-        """
-
-        # Calculating the true optimal weights allocation
-        w_true = self.allocate_cvo(cov, None if min_var_portf else mu_vec)
-        w_true = np.repeat(w_true.T, w_cvo.shape[0], axis=0)
-
-        # Mean standard deviation between the weights from CVO algorithm and the true weights
-        err_cvo = (w_cvo - w_true).std(axis=0).mean()
-
-        # Mean standard deviation between the weights from NCO algorithm and the true weights
-        err_nco = (w_nco - w_true).std(axis=0).mean()
-
-        return err_cvo, err_nco
 
     @staticmethod
     def _form_block_matrix(num_blocks, block_size, block_corr):
