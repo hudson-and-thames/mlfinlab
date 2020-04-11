e59e8c420449486d8f909f9c4d35e3c3dc1676b2
Alex Kwon <akwon@berkeley.edu>
2020-04-11 15:02:05 -0700

    Modularizing Portfolio Initiation

--- mlfinlab/online_portfolio_selection/OLPS.py
+++ mlfinlab/online_portfolio_selection/OLPS.py
@@ -9,10 +9,18 @@ class OLPS(object):
         Constructor
         :param weights: (pd.DataFrame) final weight of portfolio
         :param all_weights: (pd.DataFrame) all weights of portfolio
+        :param portfolio_return: (pd.DataFrame) all returns of portfolio
         """
         self.weights = None
         self.all_weights = None
         self.portfolio_return = None
+        self.asset_name = None
+        self.asset_time = None
+        self.number_of_assets = None
+        self.number_of_time = None
+        self.relative_return = None
+        self.portfolio_start = None
+
 
         # self.asset_prices = None
         # self.covariance_matrix = None
@@ -21,46 +29,35 @@ class OLPS(object):
         # self.expected_returns = None
         # self.returns_estimator = ReturnsEstimation()
 
-    # if a weight isn't specified, original OLPS will just buy a random stock and never rebalance afterwards
+    # public method idea
+    # OLPS.allocate(some_data)
+    # OLPS.weights
+    # OLPS.summary()
+    # OLPS.returns
     def allocate(self,
-                 asset_names,
                  asset_prices,
                  weights=None,
+                 portfolio_start=0,
                  resample_by=None):
         """
-        :param asset_names: (list) a list of strings containing the asset names
         :param asset_prices: (pd.Dataframe) a dataframe of historical asset prices (daily close)
         :param weights: any weights
+        :param portfolio_start: (int) delay the portfolio by n number of time
         :param resample_by: (str) specifies how to resample the prices - weekly, daily, monthly etc.. Defaults to
                                   None for no resampling
         """
-        # Data prep
+        # resample function
+        asset_prices = asset_prices.resample(resample_by).last()
 
+        # Data Check
+        self.__check_asset(asset_prices)
+        # Data prep
         # Some sort of initial check to make sure data fits the standards
         # asset name in right format
         # asset price in right format
         # resample_by in right format
         # weights add up to 1
-
-        # calculate number of assets
-        number_of_assets = len(asset_names)
-
-        # split index and columns
-        idx = asset_prices.index
-        asset_names = asset_prices.columns
-
-        # calculate number of time periods
-        time_period = asset_prices.shape[0]
-
-        # make asset_prices a numpy array (maybe faster for calculation)
-        np_asset_prices = np.array(asset_prices)
-
-        # calculate relative price i.e. week 1's price/week 0's price
-        # relative_price is a dataframe
-        relative_price = self.relative_price_change(asset_prices)
-
-        # cumulative product matrix
-        cumulative_product = np.array(relative_price).cumprod(axis=0)
+        self.__initialize(asset_prices)
 
         # Actual weight calculation
 
@@ -83,6 +80,37 @@ class OLPS(object):
         self.conversion(_all_weights=self.all_weights, _portfolio_return=self.portfolio_return, _index=idx,
                         _asset_names=asset_names)
 
+    # valid dataset?
+    def __check_asset(self, _asset_prices):
+        # is the dataset actually valid
+        pass
+
+    def __initialize(self, _asset_prices):
+
+        # set asset names
+        self.asset_name = _asset_prices.columns
+
+        # set asset time
+        self.asset_time = _asset_prices.index
+
+        # calculate number of assets
+        self.number_of_assets = self.asset_name.size
+
+        # calculate number of time periods
+        self.number_of_time = self.asset_time.size
+
+        # calculate relative returns
+        self.relative_return = self.__relative_return(_asset_prices)
+
+    # calculate relative returns
+    def __relative_return(self, _asset_prices):
+        # percent change of each row
+        # first row is blank because no change, so make it 0
+        # add 1 to all values so that the values can be multiplied easily
+        # change to numpy array
+        relative_return = np.array(_asset_prices.pct_change().fillna(0) + 1)
+        return relative_return
+
     # for this one, it doesn't matter, but for subsequent complex selection problems, we might have to include a
     # separate run method for each iteration and not clog the allocate method.
     # after calculating the new weight add that to the all weights
@@ -93,8 +121,9 @@ class OLPS(object):
         self.weights = self.normalize(new_weight)
         self.all_weights = np.vstack((self.all_weights, self.weights))
 
-    def initialize_weight(self, _number_of_assets):
-        self.weights = np.ones(_number_of_assets) / _number_of_assets
+    # return uniform weights numpy array 1/n
+    def __uniform_weight(_number_of_assets):
+        return np.ones(_number_of_assets) / _number_of_assets
 
     # calculate portfolio returns
     def calculate_portfolio_returns(self, _all_weights, _relative_price):
@@ -114,15 +143,6 @@ class OLPS(object):
     def diag_mul(self, A, B):
         return (A * B.T).sum(-1)
 
-    # calculate percent change relative to the original price
-    def relative_price_change(self, _asset_prices):
-        # percent change of each row
-        relative_price = _asset_prices.pct_change()
-        # first row is blank because no change, so make it 0
-        relative_price = relative_price.fillna(0)
-        # add 1 to all values so that the values can be multiplied easily
-        relative_price = np.array(relative_price + 1)
-        return relative_price
 
     def conversion(self, _all_weights, _portfolio_return, _index, _asset_names):
         self.all_weights = pd.DataFrame(_all_weights, index=_index, columns=_asset_names)
@@ -132,8 +152,9 @@ class OLPS(object):
     def volatility(self):
         pass
 
-    # calculate the sharpe ratio based on the weights and returns
+    # Calculate Sharpe Ratio
     def sharpe_ratio(self):
+        # self.portfolio_sharpe_ratio = ((self.portfolio_return - risk_free_rate) / (self.portfolio_risk ** 0.5))
         pass
 
     # return maximum drawdown
@@ -143,23 +164,6 @@ class OLPS(object):
     # return summary of the portfolio
     def summary(self):
         pass
-
-    # Other idea that might be implemented later
-
-    # Calculate covariance of returns or use the user specified covariance matrix
-    # covariance_matrix = calculate_covariance(asset_names, asset_prices, covariance_matrix, resample_by, self.returns_estimator)
-
-    # Calculate the expected returns if the user does not supply any returns
-    # expected_asset_returns = calculate_expected_asset_returns(asset_prices, expected_asset_returns, resample_by)
-
-    # Calculate the portfolio risk and return if it has not been calculated
-    # self.portfolio_risk = calculate_portfolio_risk(self.portfolio_risk, covariance_matrix, self.weights)
-
-    # Calculate the portfolio return
-    # self.portfolio_return = calculate_portfolio_return(self.portfolio_return, self.weights, expected_asset_returns)
-
-    # Calculate Sharpe Ratio
-    # self.portfolio_sharpe_ratio = ((self.portfolio_return - risk_free_rate) / (self.portfolio_risk ** 0.5))
 
 def main():
     stock_price = pd.read_csv("../tests/test_data/stock_prices.csv", parse_dates=True, index_col='Date')
