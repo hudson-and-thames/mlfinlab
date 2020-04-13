a91660d643a2335eae6194ee9cee37ba9f57c043
Alex Kwon <akwon@berkeley.edu>
2020-04-12 18:11:09 -0700

    Fix CRP to match pylint

--- mlfinlab/online_portfolio_selection/CRP.py
+++ mlfinlab/online_portfolio_selection/CRP.py
@@ -1,20 +1,18 @@
-from mlfinlab.online_portfolio_selection.olps_utils import *
+# pylint: disable=missing-module-docstring
+import pandas as pd
 from mlfinlab.online_portfolio_selection.OLPS import OLPS
 
 
 class CRP(OLPS):
     """
-    This class implements the Constant Rebalanced Portfolio strategy.
+    Basically OLPS
     """
 
-    def __init__(self):
-        """
-        Constructor.
-        """
-        super().__init__()
-
-
 def main():
+    """
+    test run
+    :return:
+    """
     stock_price = pd.read_csv("../tests/test_data/stock_prices.csv", parse_dates=True, index_col='Date')
     stock_price = stock_price.dropna(axis=1)
     crp = CRP()
@@ -26,3 +24,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
