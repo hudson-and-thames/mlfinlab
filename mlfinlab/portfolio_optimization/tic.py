7ce559f53280ec1d71b28bea1e223c0ce27598f9
Illya Barziy <panpipp@gmail.com>
2020-04-13 12:00:58 +0200

    Improved dosctrings for get_linkage_corr

--- mlfinlab/portfolio_optimization/tic.py
+++ mlfinlab/portfolio_optimization/tic.py
@@ -21,94 +21,106 @@ class TIC:
 
     def get_linkage_corr(self, tree, corr):
         """
-        Fits the theoretical tree graph structure on the evidence presented by the empirical
-        correlation matrix. The result is a binary tree that sequentially clusters two items
+        Fits the theoretical tree graph structure of the assets in a portfolio on the evidence
+        presented by the empirical correlation matrix.
+
+        The result is a binary tree (dendrogram) that sequentially clusters two items
         together, while measuring how closely together the two items are, until all items are
         subsumed within the same cluster.
 
-        The first step of the TIC algorithm.
+        This is the first step of the TIC algorithm.
 
-        :param tree: (list) The tree graph that represents the economic theory
-        :param corr: (list) The empirical correlation matrix
+        :param tree: (list) The tree graph that represents the structure of the assets
+        :param corr: (list) The empirical correlation matrix of the assets
         :return: (list) Linkage object that characterizes the dendrogram
         """
 
-        # Adding top level to the tree if it's missing
+        # If the top level of the tree contains multiple elements, creating a level with just one element (tree root)
         if len(np.unique(tree.iloc[:, -1])) > 1:
             tree['All'] = 0
 
-        # Creating a matrix with link arrays.
-        # Information in each of the four elements  in the link array is:
-        # (1) and (2) - ids of two items clustered together
+        # Creating a linkage object (matrix with link elements).
+        # Each row represents a cluster and consists of the following columns:
+        # (1) and (2) - number ids of two items clustered together
         # (3) - distance between those items
         # (4) - number of original variables in this cluster
+        # Items in a cluster can be single elements or other clusters
         lnk0 = np.empty(shape=(0, 4))
 
-        # List of lists of two consecutive tree columns
+        # List with elements containing two consecutive tree levels
         lvls = [[tree.columns[i-1], tree.columns[i]] for i in range(1, tree.shape[1])]
 
-        # Calculating distance matrix from the correlation matrix
+        # Calculating distance matrix from the empirical correlation matrix
         dist0 = ((1 - corr) / 2)**(1/2)
 
-        # Mapping lnk0 to dist0
-
-        # Getting a list of all element names
+        # Getting a list of names of assets asset
         items0 = dist0.index.tolist()
 
-        # Iterating through levels
+        # Iterating through levels of the tree
         for cols in lvls:
 
-            # Groups in a tree
             # Taking two consecutive levels of the tree
-            # Removing duplicates from the higher level of the tree
-            # Setting the index of the elements in the higher level
-            # Grouping by elements in the lower level
+            # Removing duplicates from the lower level of the tree
+            # Setting the obtained unique elements from the lower level as index
+            # Grouping by elements in the higher level
             grps = tree[cols].drop_duplicates(cols[0]).set_index(cols[0]).groupby(cols[1])
 
-            # Iterating through two layers - taking the category and the items inside
+            # Iterating through the obtained two levels of a tree
             for cat, items1 in grps:
-                # Getting index of items inside the category
+                # cat contains the higher element
+                # items1 contain the elements from lower level, grouped under the higher level element
+
+                # Getting the names of the grouped elements
                 items1 = items1.index.tolist()
 
-                # If single item then rename
+                # If only one element grouped by the element from the higher level
                 if len(items1) == 1:
-                    # Changing the name of the element in the initial list to the name of the category
+                    # Changing the name of this element to the name of element from the higher level
+                    # As this element is now representing the higher level
+
+                    # Changing the name in the list of names
                     items0[items0.index(items1[0])] = cat
-                    # Changing the name of the element also in the correlation matrix rows and columns
+
+                    # Changing the name also in the correlation matrix rows and columns
                     dist0 = dist0.rename({items1[0]: cat}, axis=0)
                     dist0 = dist0.rename({items1[0]: cat}, axis=1)
+
                     continue
 
-                # Taking the distance of the element (?)
+                # Taking the part of the distance matrix containing the grouped elements in the lower level
                 dist1 = dist0.loc[items1, items1]
 
                 # Transforming distance matrix to distance vector
-                # Check for matrix symmetry is made
+                # Check for matrix symmetry is made - checking that the matrix given is a distance matrix
                 dist_vec = ssd.squareform(dist1, force='tovector', checks=(not np.allclose(dist1, dist1.T)))
 
-                # Doing hierarchical clustering of the distance vector
+                # Doing hierarchical clustering of the distance vector. Result is a linkage object
+                # Here we have created new clusters based on the grouped elements
                 lnk1 = sch.linkage(dist_vec, optimal_ordering=True)
 
-                # Creating a new link array (?)
+                # Transforming the linkage object from local (only containing the grouped elements)
+                # to global (containing all elements)
                 lnk_ = self.link_clusters(lnk0, lnk1, items0, items1)
 
-                # Adding new link array to the existing matrix of links
+                # Adding new link elements to the general linage object
                 lnk0 = np.append(lnk0, lnk_, axis=0)
 
-                # As we've created more clusters, their names are added to the global list
+                # As more clusters were created, their names are added to the global list of elements
                 items0 += range(len(items0), len(items0)+len(lnk_))
 
-                # Updating the general distance matrix to take the new cluster in to account
+                # Updating the general distance matrix to take the new clusters into account
+                # Now the grouped elements in the distance matrix will be replaced with new clusters as elements.
                 dist0 = self.update_dist(dist0, lnk0, lnk_, items0)
 
-                # Renaming the last cluster for next level
+                # The last added cluster is representing the element from the higher level
+                # So we're changing the name of that cluster to the name of the higher level element
                 items0[-1] = cat
 
-                # Changing the name of the cluster in the distance matrix
+                # Changing the name of the cluster also in the distance matrix
                 dist0.columns = dist0.columns[:-1].tolist() + [cat]
                 dist0.index = dist0.columns
 
-        # Changing from link arrays to link tuples
+        # Changing the linkage object from array of arrays to array of tuples with named fields
         lnk0 = np.array([*map(tuple, lnk0)], dtype=[('i0', int), ('i1', int), ('dist', float), ('num', int)])
 
         return lnk0
@@ -116,21 +128,24 @@ class TIC:
     @staticmethod
     def link_clusters(lnk0, lnk1, items0, items1):
         """
-        Creating a new link array.
+        Transforming linkage object from local link1 (based on dist1) into global link0 (based on dist0)
 
-        Transforming partial link1 (based on dist1) into global link0 (based on dist0)
+        Consists of changes of names for the elements in clusters and change of the number of
+        basic elements (atoms) contained inside a cluster. This is done to take into account the
+        already existing links.
 
-        :param lnk0: Current list of linkages
-        :param lnk1: Hierarchical clustering of the distance vector encoded as a linkage matrix
-        :param items0: List of all elements names
-        :param items1: Index of items inside the category
-        :return: New link array
+        :param lnk0: Global linkage object (previous links)
+        :param lnk1: Local linkage object (containing grouped elements and not global ones)
+        :param items0: List of names for all elements (global)
+        :param items1: List of grouped elements (local)
+        :return: Local linkage object changed to global one
         """
 
-        # Counting the number of atoms - basic elements  and not clusters
-        num_atoms = len(items0)-lnk0.shape[0]
+        # Counting the number of atoms - basic elements and not clusters
+        # It's the total number of elements minus the number of links (each link represents 1 cluster)
+        num_atoms = len(items0) - lnk0.shape[0]
 
-        # Copying the partial link
+        # Making a copy of a local linkage object
         lnk_ = lnk1.copy()
 
         # Iterating through elements in the partial link
