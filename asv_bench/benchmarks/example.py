2ac14c2225fee4b75d56a95402adb043e20ca0e6
David Cottrell <cottrell@users.noreply.github.com>
2020-04-15 15:41:52 +0100

    Add initial setup for airspeed velocity.

---
+++ asv_bench/benchmarks/example.py
@@ -0,0 +1,32 @@
+class TimeSuite:
+    """
+    An example benchmark that times the performance of various kinds
+    of iterating over dictionaries in Python.
+    """
+    def setup(self):
+        self.d = {}
+        for x in range(500):
+            self.d[x] = None
+
+    def time_keys(self):
+        for key in list(self.d.keys()):
+            pass
+
+    def time_iterkeys(self):
+        for key in self.d.keys():
+            pass
+
+    def time_range(self):
+        d = self.d
+        for key in range(500):
+            x = d[key]
+
+    def time_xrange(self):
+        d = self.d
+        for key in range(500):
+            x = d[key]
+
+
+class MemSuite:
+    def mem_list(self):
+        return [0] * 256
