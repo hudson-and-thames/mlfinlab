773dd8e6fe5c767a360cc08692e5fdd959cb0080
Alex Kwon <akwon@berkeley.edu>
2020-04-16 00:24:41 -0700

    Update BESTSTOCK.py

--- mlfinlab/online_portfolio_selection/benchmarks/BESTSTOCK.py
+++ mlfinlab/online_portfolio_selection/benchmarks/BESTSTOCK.py
@@ -1,21 +1,28 @@
 # pylint: disable=missing-module-docstring
 from mlfinlab.online_portfolio_selection.OLPS import OLPS
-import pandas as pd
 import numpy as np
 
 
 class BESTSTOCK(OLPS):
     """
-    returns best stock in hindsight
+    This class implements the Best Stock strategy. It is reproduced with modification from the following paper:
+    Li, B., Hoi, S. C.H., 2012. OnLine Portfolio Selection: A Survey. ACM Comput. Surv. V, N, Article A (December YEAR),
+    33 pages. DOI:http://dx.doi.org/10.1145/0000000.0000000.
+
+    The Best Stock strategy takes the best performing stock over the period in hindsight.
     """
 
-    # Buy the asset that increased the most for the given period
     def first_weight(self, _weights):
         """
-        :param _weights
+        Sets the initial weight to the best performing stock over the entire time period
+
+        :param _weights: (np.array) Given weights do not matter as new weights will be calculated
+        :return new_weight: (np.array) weight that allocates one to the best performing asset
         """
         # index of stock that increased the most
         best_idx = np.argmax(self.final_relative_return[-1])
+        # initialize array of zeros
         new_weight = np.zeros(self.number_of_assets)
+        # assign one to best performing stock
         new_weight[best_idx] = 1
         return new_weight
