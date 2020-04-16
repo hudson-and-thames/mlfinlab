e925282c5d8ed382f219e5c641c1de15ca1a3579
Alex Kwon <akwon@berkeley.edu>
2020-04-16 00:26:03 -0700

    Finish CRP comments

--- mlfinlab/online_portfolio_selection/benchmarks/CRP.py
+++ mlfinlab/online_portfolio_selection/benchmarks/CRP.py
@@ -5,5 +5,11 @@ from mlfinlab.online_portfolio_selection.OLPS import OLPS
 
 class CRP(OLPS):
     """
-    Basically OLPS
+    This class implements the Constant Rebalanced Portfolio strategy. It is reproduced with modification from the following paper:
+    Li, B., Hoi, S. C.H., 2012. OnLine Portfolio Selection: A Survey. ACM Comput. Surv. V, N, Article A (December YEAR),
+    33 pages. DOI:http://dx.doi.org/10.1145/0000000.0000000.
+
+    Constant Rebalanced Portfolio rebalances to a given weight for each time period.
+
+    There are no modifications in this class because the original OLPS had CRP in mind, so it is the exact same replica
     """
