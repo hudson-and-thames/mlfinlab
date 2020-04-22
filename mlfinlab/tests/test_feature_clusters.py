dcc3e7b198e6e56fd922f4eaff6df1a3d53f7166
Harkishan-99 <harkishansinghbaniya@gmail.com>
2020-04-22 14:39:07 +0530

    pylint fix

--- mlfinlab/tests/test_feature_clusters.py
+++ mlfinlab/tests/test_feature_clusters.py
@@ -8,7 +8,7 @@ from mlfinlab.util.generate_dataset import get_classification_data
 from mlfinlab.clustering.feature_clusters import get_feature_clusters
 
 
-
+# pylint: disable=invalid-name
 class TestFeatureClusters(unittest.TestCase):
     """
     Test get_feature_clusters function
@@ -18,7 +18,7 @@ class TestFeatureClusters(unittest.TestCase):
         """
         Create X, y datasets
         """
-        self.X, self.y = get_classification_data(40,5,30,1000,sigmaStd=2)
+        self.X, self.y = get_classification_data(40, 5, 30, 1000, sigmaStd=2)
 
     def test_get_feature_clusters(self):
         """
@@ -37,8 +37,7 @@ class TestFeatureClusters(unittest.TestCase):
         #so we will make a feature with some what lower silhouette score (near to zero) and set
         #the threshold higher (0.1) than that. Also we need a feature to trigger the low degree of freedom
         #condition so, we create a series of zero in the datasets
-        X_data = self.X
-        X_data['R_5c'] = X['R_5'] #this feature is add to introduce low DF in the regressor.
+        self.X['R_5c'] = self.X['R_5'] #this feature is add to introduce low DF in the regressor.
         clustered_subsets_distance = get_feature_clusters(X_data, dependence_metric='distance_correlation',
                                                           distance_metric='abs_angular', linkage_method='single',
                                                           n_clusters=None, critical_threshold=0.1)
@@ -64,6 +63,6 @@ class TestFeatureClusters(unittest.TestCase):
                                  n_clusters=2)
         #Number of clusters larger than number of features
         with self.assertRaises(ValueError):
-            get_feature_clusters(selfXa, dependence_metric='linear',
+            get_feature_clusters(self.X, dependence_metric='linear',
                                  distance_metric='angular', linkage_method='single',
                                  n_clusters=int(len(self.data)))
