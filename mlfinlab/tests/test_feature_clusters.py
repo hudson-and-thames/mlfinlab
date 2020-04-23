ea0635d6d9ff4240be211d6e10f6ec8c3679f455
Harkishan-99 <harkishansinghbaniya@gmail.com>
2020-04-23 19:49:58 +0530

    converge fix

--- mlfinlab/tests/test_feature_clusters.py
+++ mlfinlab/tests/test_feature_clusters.py
@@ -25,41 +25,34 @@ class TestFeatureClusters(unittest.TestCase):
         """
         #test for different dependence matrix
 
-        clustered_subsets_vi = get_feature_clusters(self.X, dependence_metric='information_variation',
-                                                    distance_metric='squared_angular', linkage_method='single',
-                                                    n_clusters=2)
-        clustered_subsets_mi = get_feature_clusters(self.X, dependence_metric='mutual_information',
+        clustered_subsets = get_feature_clusters(self.X, dependence_metric='linear',
                                                     distance_metric='angular', linkage_method='single',
                                                     n_clusters=2)
+        #hierarchical auto clustering
+        clustered_subsets_ha = get_feature_clusters(self.X, dependence_metric='linear',
+                                                    distance_metric='angular', linkage_method='single',
+                                                    n_clusters=None)
         #test for optimal number of clusters and  _check_for_low_silhouette_scores
         #since this is done on test dataset so there will be no features with low silhouette score
         #so we will make a feature with some what lower silhouette score (near to zero) and set
         #the threshold higher (0.2) than that. Also we need a feature to trigger the low degree of freedom
         #condition so, we create a series of zero in the datasets
         self.X['R_5c'] = self.X['R_5'] #this feature is add to introduce low DF in the regressor.
-        clustered_subsets_distance = get_feature_clusters(self.X, dependence_metric='distance_correlation',
-                                                          distance_metric='abs_angular', linkage_method='single',
+        clustered_subsets_distance = get_feature_clusters(self.X, dependence_metric='linear',
+                                                          distance_metric=None, linkage_method=None,
                                                           n_clusters=None, critical_threshold=0.2)
+        #assertions
         #output clusters must be 2
-        self.assertAlmostEqual(len(clustered_subsets_distance), 5, delta=0.001)
-        self.assertAlmostEqual(len(clustered_subsets_vi), 2, delta=0.001)
-        self.assertAlmostEqual(len(clustered_subsets_mi), 2, delta=0.001)
+        self.assertEqual(len(clustered_subsets), 2)
+        #The ONC should detect somwhere around 5 clusters
+        self.assertAlmostEqual(len(clustered_subsets_ha), 5, delta=1)
+        self.assertAlmostEqual(len(clustered_subsets_distance), 5, delta=1)
 
 
     def test_value_error_raise(self):
         """
-        Test get_feature_clusters , codependence_matrix and distance_matrix for invalid arguments
+        Test get_feature_clusters for invalid number of clusters arguments
         """
-        #Unkown dependence_metric
-        with self.assertRaises(ValueError):
-            get_feature_clusters(self.X, dependence_metric='information',
-                                 distance_metric='angular', linkage_method='single',
-                                 n_clusters=2)
-        #Unkown distance_metric
-        with self.assertRaises(ValueError):
-            get_feature_clusters(self.X, dependence_metric='linear',
-                                 distance_metric='serial', linkage_method='single',
-                                 n_clusters=2)
         #Number of clusters larger than number of features
         with self.assertRaises(ValueError):
             get_feature_clusters(self.X, dependence_metric='linear',
