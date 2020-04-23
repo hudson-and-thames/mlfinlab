61ce6fa696ea7a1eaf36c7d684bd721562c5bdb9
Alex <proskurinolexandr@gmail.com>
2020-04-23 12:25:42 +0300

    Solve paths issue

--- mlfinlab/datasets/load_datasets.py
+++ mlfinlab/datasets/load_datasets.py
@@ -3,6 +3,7 @@ The module implementing various functions loading tick, dollar, stock data sets 
 sandbox data
 """
 
+import os
 import pandas as pd
 
 
@@ -15,7 +16,8 @@ def load_stock_prices() -> pd.DataFrame:
     :return: (pd.DataFrame) stock_prices data frame
     """
 
-    prices_df = pd.read_csv('../tests/test_data/stock_prices.csv', index_col=0, parse_dates=[0])
+    project_path = os.path.dirname(__file__)
+    prices_df = pd.read_csv(os.path.join(project_path,'data/stock_prices.csv'), index_col=0, parse_dates=[0])
     return prices_df
 
 
@@ -26,7 +28,8 @@ def load_tick_sample() -> pd.DataFrame:
     :return: (pd.DataFrame) with tick data sample
     """
 
-    tick_df = pd.read_csv('../tests/test_data/tick_data.csv', index_col=0, parse_dates=[0])
+    project_path = os.path.dirname(__file__)
+    tick_df = pd.read_csv(os.path.join(project_path, 'data/tick_data.csv'), index_col=0, parse_dates=[0])
     return tick_df
 
 
@@ -37,5 +40,6 @@ def load_dollar_bar_sample() -> pd.DataFrame:
     :return: (pd.DataFrame) with dollar bar data sample
     """
 
-    bars_df = pd.read_csv('../tests/test_data/dollar_bar_sample.csv', index_col=0, parse_dates=[0])
+    project_path = os.path.dirname(__file__)
+    bars_df = pd.read_csv(os.path.join(project_path, 'data/dollar_bar_sample.csv'), index_col=0, parse_dates=[0])
     return bars_df
