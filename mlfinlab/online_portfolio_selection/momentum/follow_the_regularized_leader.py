1310e1a7fad6fe657f9dc40239c3ea3661841a87
Alex Kwon <akwon@berkeley.edu>
2020-04-27 16:32:54 -0700

    fix cyclic imports

--- mlfinlab/online_portfolio_selection/momentum/follow_the_regularized_leader.py
+++ mlfinlab/online_portfolio_selection/momentum/follow_the_regularized_leader.py
@@ -1,6 +1,7 @@
+# pylint: disable=missing-module-docstring
 import numpy as np
 import scipy.optimize as opt
-from mlfinlab.online_portfolio_selection import FollowTheLeader
+from mlfinlab.online_portfolio_selection.momentum.follow_the_leader import FollowTheLeader
 
 
 class FollowTheRegularizedLeader(FollowTheLeader):
