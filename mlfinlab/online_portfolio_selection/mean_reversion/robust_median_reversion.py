0c78b6f0d82270238a54988f9eb5439dcbd24bc0
Alex Kwon <akwon@berkeley.edu>
2020-05-03 01:30:41 -0700

    Update robust_median_reversion.py

--- mlfinlab/online_portfolio_selection/mean_reversion/robust_median_reversion.py
+++ mlfinlab/online_portfolio_selection/mean_reversion/robust_median_reversion.py
@@ -84,27 +84,27 @@ class RobustMedianReversion(OLPS):
                 break
         return new_median / curr_price
 
-    @staticmethod
-    def _helper(old_median):
-
-
-    y = np.mean(X, 0)
-
-    while True:
-        D = cdist(X, [y])
-        nonzeros = (D != 0)[:, 0]
-
-        Dinv = 1 / D[nonzeros]
-        Dinvs = np.sum(Dinv)
-        W = Dinv / Dinvs
-        T = np.sum(W * X[nonzeros], 0)
-        num_zeros = len(X) - np.sum(nonzeros)
-        if num_zeros == 0:
-            y1 = T
-        elif num_zeros == len(X):
-            return y
-        else:
-            R = (T - y) * Dinvs
-            r = np.linalg.norm(R)
-            rinv = 0 if r == 0 else num_zeros / r
-            y1 = max(0, 1 - rinv) * T + min(1, rinv) * y
+    # @staticmethod
+    # def _helper(old_median):
+    #
+    #
+    # y = np.mean(X, 0)
+    #
+    # while True:
+    #     D = cdist(X, [y])
+    #     nonzeros = (D != 0)[:, 0]
+    #
+    #     Dinv = 1 / D[nonzeros]
+    #     Dinvs = np.sum(Dinv)
+    #     W = Dinv / Dinvs
+    #     T = np.sum(W * X[nonzeros], 0)
+    #     num_zeros = len(X) - np.sum(nonzeros)
+    #     if num_zeros == 0:
+    #         y1 = T
+    #     elif num_zeros == len(X):
+    #         return y
+    #     else:
+    #         R = (T - y) * Dinvs
+    #         r = np.linalg.norm(R)
+    #         rinv = 0 if r == 0 else num_zeros / r
+    #         y1 = max(0, 1 - rinv) * T + min(1, rinv) * y
