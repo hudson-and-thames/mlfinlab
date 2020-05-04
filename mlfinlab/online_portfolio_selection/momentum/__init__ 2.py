de2f1b02a2a9037fe761362c46f862cad575e394
Alex Kwon <akwon@berkeley.edu>
2020-05-04 10:47:17 -0700

    Delete doubled files

--- mlfinlab/online_portfolio_selection/momentum/__init__ 2.py
+++
@@ -1,6 +0,0 @@
-"""
-Initialize Momentum Module.
-"""
-from mlfinlab.online_portfolio_selection.momentum.exponential_gradient import ExponentialGradient
-from mlfinlab.online_portfolio_selection.momentum.follow_the_leader import FollowTheLeader
-from mlfinlab.online_portfolio_selection.momentum.follow_the_regularized_leader import FollowTheRegularizedLeader
