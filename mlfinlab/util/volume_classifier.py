b754bdab13dbf2783f15d20fc63651f0777bfe28
Illya Barziy <panpipp@gmail.com>
2020-05-05 16:49:13 +0200

    Labelling docstrings same style

--- mlfinlab/util/volume_classifier.py
+++ mlfinlab/util/volume_classifier.py
@@ -8,10 +8,11 @@ import pandas as pd
 
 def get_bvc_buy_volume(close: pd.Series, volume: pd.Series, window: int = 20) -> pd.Series:
     """
+    Calculates the BVC buy volume
 
-    :param close: (pd.Series): series of close prices
-    :param volume: (pd.Series): series of bar volumes
-    :param window: (int); window for std estimation uses in BVC calculation
-    :return:
+    :param close: (pd.Series): Close prices
+    :param volume: (pd.Series): Bar volumes
+    :param window: (int): Window for std estimation uses in BVC calculation
+    :return: (pd.Series) BVC buy volume
     """
     return volume * norm.cdf(close.diff() / close.diff().rolling(window=window).std())
