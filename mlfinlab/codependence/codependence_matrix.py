7faee9858dd2f180d110ee82c6e4c7d6cb268f60
proskurin <proskurinolexandr@gmail.com>
2020-05-09 18:54:13 +0300

    Speed-up codependence matrix calculations

--- mlfinlab/codependence/codependence_matrix.py
+++ mlfinlab/codependence/codependence_matrix.py
@@ -4,6 +4,7 @@ Codependence  described in Cornell lecture notes: Codependence:
 https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3512994&download=yes
 """
 
+import numpy as np
 import pandas as pd
 
 from mlfinlab.codependence.information import variation_of_information_score, get_mutual_info
@@ -14,7 +15,6 @@ from mlfinlab.codependence.correlation import distance_correlation
 def get_dependence_matrix(df: pd.DataFrame, dependence_method: str) -> pd.DataFrame:
     """
     This function returns a dependence matrix for the given method of dependence method.
-
     :param df: (pd.DataFrame) of features.
     :param dependence_method: (str) the algorithm to be use for generating dependence_matrix, either
        'information_variation' or 'mutual_information' or 'distance_correlation'.
@@ -22,6 +22,8 @@ def get_dependence_matrix(df: pd.DataFrame, dependence_method: str) -> pd.DataFr
     """
     # Get the feature names.
     features_cols = df.columns.values
+    n = df.shape[0]
+    np_df = df.values.T  # Make columnar access, but for np.array
 
     # Defining the dependence function.
     if dependence_method == 'information_variation':
@@ -36,9 +38,27 @@ def get_dependence_matrix(df: pd.DataFrame, dependence_method: str) -> pd.DataFr
 
     # Generating the dependence_matrix for the defined method.
     if dependence_method != 'distance_correlation':
-        dependence_matrix = [[dep_function(df[x].values, df[y].values, normalize=True) for x in features_cols] for y in features_cols]
+        dependence_matrix = np.array([
+            [
+                dep_function(np_df[i], np_df[j], normalize=True) if j < i else
+                0.5 if j == 1 else  # Leave diagonal elements as 0.5 to later double them to 1
+                0  # Make upper triangle 0 to fill it later on
+                for j in range(n)
+            ]
+            for i in range(n)
+        ])
     else:
-        dependence_matrix = [[dep_function(df[x].values, df[y].values) for x in features_cols] for y in features_cols]
+        dependence_matrix = np.array([
+            [
+                dep_function(np_df[i], np_df[j]) if j < i else
+                0.5 if j == 1 else  # Leave diagonal elements as 0.5 to later double them to 1
+                0  # Make upper triangle 0 to fill it later on
+                for j in range(n)
+            ]
+            for i in range(n)
+        ])
+    # Make matrix symmetrical
+    dependence_matrix = dependence_matrix + dependence_matrix.T
 
     #  Dependence_matrix converted into a DataFrame.
     dependence_df = pd.DataFrame(data=dependence_matrix, index=features_cols, columns=features_cols)
@@ -47,7 +67,6 @@ def get_dependence_matrix(df: pd.DataFrame, dependence_method: str) -> pd.DataFr
         return 1 - dependence_df  # IV is reverse, 1 - independent, 0 - similar
 
     return dependence_df
-
 
 def get_distance_matrix(X: pd.DataFrame, distance_metric: str = 'angular') -> pd.DataFrame:
     """
