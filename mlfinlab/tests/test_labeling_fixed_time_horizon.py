303cde927f72678f4a345cb2ada06252348236d7
Ruifan Pei <ruifan.pei@gmail.com>
2020-05-10 16:29:37 -0400

    fixed some bugs and finished unit test Please enter the commit message for your changes. Lines starting

--- mlfinlab/tests/test_labeling_fixed_time_horizon.py
+++ mlfinlab/tests/test_labeling_fixed_time_horizon.py
@@ -14,9 +14,10 @@ class TestLabellingFixedTime(unittest.TestCase):
         """
         Set the file path for the sample dollar bars data.
         """
-        project_path = os.path.dirname(__file__)
+        # project_path = os.path.dirname(__file__)
+        project_path = os.getcwd()
         self.path = project_path + '/test_data/stock_prices.csv'
-        self.data = pd.read_csv(self.path, index_col='date_time')
+        self.data = pd.read_csv(self.path, index_col='Date')
         self.data.index = pd.to_datetime(self.data.index)
 
     def test_fixed_time_horizon(self):
@@ -25,12 +26,12 @@ class TestLabellingFixedTime(unittest.TestCase):
         Checks a range of static and dynamic thresholds.
         """
         close = self.data['SPY']
-        daily_ret = close.pct_change(periods=h)
-        forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
 
         # static threshold
         for threshold in [0, 0.005, 0.01, 0.015, 0.02, 0.03, 0.04]:
             for h in [1, 3]:
+                daily_ret = close.pct_change(periods=h)
+                forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
                 labels = fixed_time_horizon(close, threshold, h=h)
                 for i in range(len(labels)):
                     if labels[i] == 1:
@@ -41,16 +42,21 @@ class TestLabellingFixedTime(unittest.TestCase):
                         self.assertTrue(forward_ret[i] < -threshold)
 
         # dynamic threshold
-        threshold_d = np.random.random(len(self.data['SPY'] / 50))
+        threshold_dynamic = pd.Series(np.random.random(len(close)) / 50)
         for h in [1, 3]:
-            labels = fixed_time_horizon(close, threshold_d, h=h)
+            daily_ret = close.pct_change(periods=h)
+            forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
+            labels = fixed_time_horizon(close, threshold_dynamic, h=h)
             for i in range(len(labels)):
                 if labels[i] == 1:
-                    self.assertTrue(forward_ret[i] > threshold)
+                    self.assertTrue(forward_ret[i] > threshold_dynamic[i])
                 if labels[i] == 0:
-                    self.assertTrue(np.abs(forward_ret[i]) <= threshold)
+                    self.assertTrue(np.abs(forward_ret[i]) <= threshold_dynamic[i])
                 if labels[i] == -1:
-                    self.assertTrue(forward_ret[i] < -threshold)
+                    self.assertTrue(forward_ret[i] < -threshold_dynamic[i])
+        # check value error
+        with self.assertRaises(ValueError):
+            fixed_time_horizon(close, 'string', h=1)
 
 
 if __name__ == '__main__':
