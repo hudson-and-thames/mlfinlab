3703b2158f5a77b22739ca24b44903a70cb0a851
Ruifan Pei <ruifan.pei@gmail.com>
2020-05-12 09:50:21 -0400

    improved code style and expanded unit tests

--- mlfinlab/labeling/fixed_time_horizon.py
+++ mlfinlab/labeling/fixed_time_horizon.py
@@ -7,25 +7,59 @@ import numpy as np
 import pandas as pd
 
 
-def fixed_time_horizon(close, threshold, h=1, get_forward_ret=False):
+def get_forward_return(close, lookfwd):
+    """
+    Gets forward return of a series of close prices, given a specified number of ticks to look forward
+    :param close: (series) of Close prices
+    :param lookfwd: (int) Number of ticks to look forward when calculating forward return rate
+    :return: (series) of Forward returns
+    """
+    # Daily returns
+    daily_ret = close.pct_change(periods=lookfwd)
+
+    # "Forward" returns h time intervals in the future, to be compared against the threshold
+    forward_ret = pd.Series(list(daily_ret)[lookfwd:] + [float("NaN")] * lookfwd, index=close.index)
+
+    return forward_ret
+
+
+def standardize(forward_ret, standardized):
+    """
+    Applies standardization a pd.Series of returns for a stock when the market return and standard deviation are
+    known
+    :param forward_ret: (series) of Forward returns
+    :param standardized: (list) of Tuples (mean, stdev) of returns corresponding to each day in forward_ret
+    :return: (series) of Standardized returns
+    """
+    mean = pd.Series([i for i, j in standardized], index=forward_ret.index)
+    stdev = pd.Series([j for i, j in standardized], index=forward_ret.index)
+    forward_ret_adj = forward_ret.sub(mean)
+    forward_ret_adj = forward_ret_adj.div(stdev)
+
+    return forward_ret_adj
+
+
+def fixed_time_horizon(close, threshold, lookfwd=1, standardized=None):
     """
     Fixed-Time Horizon Labelling Method
     Returns 1 if return at h-th bar after t_0 is greater than the threshold, -1 if less, and 0 if in between
 
-    :param close: (series) of close prices
-    :param threshold: (float or pd.Series) when the abs(change) is larger than the threshold, it is labelled as 1 or -1.
+    :param close: (series) of Close prices
+    :param threshold: (float or pd.Series) When the abs(change) is larger than the threshold, it is labelled as 1 or -1.
                     If change is smaller, it's labelled as 0. Can be dynamic if threshold is pd.Series
-    :param h: (int) number of ticks to look forward when calculating future return rate
-    :param get_forward_ret: (bool) whether to get the forward returns as well as labels. False by default
-    :return: (series) series of -1, 0, or 1 denoting whether return is under/between/greater than the threshold
+    :param lookfwd: : (int) Number of ticks to look forward when calculating future return rate
+    :param standardized: (list) of Tuples (mean, stdev) of returns corresponding to each day in close. If not None, the
+                    forward returns are for each day are adjusted by the mean and stdev
+    :return: (series) Series of -1, 0, or 1 denoting whether return is under/between/greater than the threshold
     """
-    # daily returns
-    daily_ret = close.pct_change(periods=h)
+    # Forward returns
+    forward_ret = get_forward_return(close, lookfwd)
 
-    # "forward" returns h time intervals in the future, to be compared against the threshold
-    forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
+    # If standardization is applied, adjust forward_ret by mean and stdev
+    if standardized is not None:
+        forward_ret = standardize(forward_ret, standardized)
 
-    # compare forward return with the threshold, and returns -1, 0, 1 for lower than/between/greater than threshold
+    # Compare forward return with the threshold, and returns -1, 0, 1 for lower than/between/greater than threshold
     if isinstance(threshold, (float, int)):
         labels = forward_ret.apply(
             lambda row: 1 if row > threshold else (
@@ -48,7 +82,4 @@ def fixed_time_horizon(close, threshold, h=1, get_forward_ret=False):
     else:
         raise ValueError('threshold is neither float nor pd.Series!')
 
-    if get_forward_ret:
-        return labels, forward_ret
-    else:
-        return labels
+    return labels
--- mlfinlab/tests/test_labeling_fixed_time_horizon.py
+++ mlfinlab/tests/test_labeling_fixed_time_horizon.py
@@ -1,8 +1,10 @@
+# pylint: disable=missing-module-docstring
+
 import unittest
 import os
 import numpy as np
 import pandas as pd
-from mlfinlab.labeling import fixed_time_horizon
+from mlfinlab.labeling.fixed_time_horizon import get_forward_return, standardize, fixed_time_horizon
 
 
 class TestLabellingFixedTime(unittest.TestCase):
@@ -14,26 +16,47 @@ class TestLabellingFixedTime(unittest.TestCase):
         """
         Set the file path for the sample dollar bars data.
         """
-        # project_path = os.path.dirname(__file__)
-        project_path = os.getcwd()
+        project_path = os.path.dirname(__file__)
         self.path = project_path + '/test_data/stock_prices.csv'
         self.data = pd.read_csv(self.path, index_col='Date')
         self.data.index = pd.to_datetime(self.data.index)
+
+    def test_get_forward_returns(self):
+        """
+        Assert that the forward returns for the used test file are accurate
+        """
+        close = self.data['SPY'][:5]
+
+        # Correct values for the data used
+        actual_returns1 = pd.Series([-0.000483, -0.024506, -0.000849, -0.016148, float("NaN")], index=close.index)
+        actual_returns3 = pd.Series([-0.025805, -0.041074, float("NaN"), float("NaN"), float("NaN")], index=close.index)
+
+        pd.testing.assert_series_equal(actual_returns1, get_forward_return(close, 1), check_less_precise=3)
+        pd.testing.assert_series_equal(actual_returns3, get_forward_return(close, 3), check_less_precise=3)
+
+    def test_standardize(self):
+        """
+        Assert that standardization is accurate
+        """
+        returns = pd.Series([-0.000483, -0.024506, -0.000849, -0.016148, float("NaN")])
+        mean_std = [(0, 0.005), (0.01, 0.05), (0.02, 0.0007), (-0.1, 0.5), (0.01, 0.01)]
+        calculated = standardize(returns, mean_std)
+        actual = pd.Series([-0.0966, -0.69012, -29.7843, 0.1677, float("NaN")], index=calculated.index)
+        pd.testing.assert_series_equal(calculated, actual, check_less_precise=3)
 
     def test_fixed_time_horizon(self):
         """
         Assert that the fixed time horizon labelling works as expected.
         Checks a range of static and dynamic thresholds.
         """
-        close = self.data['SPY']
+        close = self.data['SPY'][:5]
 
-        # static threshold
+        # Static threshold, no standardization
         for threshold in [0, 0.005, 0.01, 0.015, 0.02, 0.03, 0.04]:
-            for h in [1, 3]:
-                daily_ret = close.pct_change(periods=h)
-                forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
-                labels = fixed_time_horizon(close, threshold, h=h)
-                for i in range(len(labels)):
+            for lookfwd in [1, 3]:
+                forward_ret = get_forward_return(close, lookfwd)
+                labels = fixed_time_horizon(close, threshold, lookfwd=lookfwd)
+                for i, _ in enumerate(labels):
                     if labels[i] == 1:
                         self.assertTrue(forward_ret[i] > threshold)
                     if labels[i] == 0:
@@ -41,22 +64,24 @@ class TestLabellingFixedTime(unittest.TestCase):
                     if labels[i] == -1:
                         self.assertTrue(forward_ret[i] < -threshold)
 
-        # dynamic threshold
+        # Dynamic threshold, also apply a standardization to the data
+        mean_std = [(0, 0.005), (0.01, 0.05), (0.02, 0.0007), (-0.1, 0.5), (0.01, 0.01)]
         threshold_dynamic = pd.Series(np.random.random(len(close)) / 50)
-        for h in [1, 3]:
-            daily_ret = close.pct_change(periods=h)
-            forward_ret = pd.Series(list(daily_ret)[h:] + [float("NaN")] * h, index=close.index)
-            labels = fixed_time_horizon(close, threshold_dynamic, h=h)
-            for i in range(len(labels)):
+        for lookfwd in [1, 3]:
+            # Apply standardization to the forward return
+            forward_ret = standardize(get_forward_return(close, lookfwd), mean_std)
+            labels = fixed_time_horizon(close, threshold_dynamic, lookfwd=lookfwd, standardized=mean_std)
+            for i, _ in enumerate(labels):
                 if labels[i] == 1:
                     self.assertTrue(forward_ret[i] > threshold_dynamic[i])
                 if labels[i] == 0:
                     self.assertTrue(np.abs(forward_ret[i]) <= threshold_dynamic[i])
                 if labels[i] == -1:
                     self.assertTrue(forward_ret[i] < -threshold_dynamic[i])
-        # check value error
+
+        # Check value error
         with self.assertRaises(ValueError):
-            fixed_time_horizon(close, 'string', h=1)
+            fixed_time_horizon(close, 'string', lookfwd=1)
 
 
 if __name__ == '__main__':
