ae412569eb157240c5ae775f3c4c65cd5067ab7d
jihobak <practicalquant@gmail.com>
2020-05-26 17:36:44 +0900

    Fix multiple erros and update comments.
    	- fix typos
    	- use passive voice for variable description
    	- add a flag paramet which indicates whether return is raw or log
    
    	edited:        mlfinlab/bet_sizing/kelly.py

--- mlfinlab/bet_sizing/kelly.py
+++ mlfinlab/bet_sizing/kelly.py
@@ -13,25 +13,23 @@ import pandas as pd
 
 def kelly_betting(win_probability: float, profit_unit: float, loss_unit: float) -> Tuple[float, float]:
     """
-    Calculates optimal bet size based on the kelly criterion.
+    Calculates optimal bet size based on the Kelly criterion.
 
-    This method assumes our strategy is used for the environment which have discrete and binary outcomes for each trial
-    such as coin tossing and sports betting. It returns the optimal betting size based on the kelly criterion.
+    This method assumes our strategy is used for the environment which has discrete and binary outcomes for each trial
+    such as coin tossing and sports betting. It returns the optimal betting size based on the Kelly criterion.
 
-    This method is designed to deal with not only the scenario that we loss all of wager when we loss(loss_unit=1)
+    This method is designed to deal with not only the scenario that we lose all of the wager when we lose(loss_unit=1)
     but also the general scenario. For example, when betting odds are 2, we should set 'profit_unit' to 2 and
     'loss_unit' to 1.
 
     The implementation is based on the paper, 'The Kelly Criterion in blackjack, sports betting, and the stock market'
     by Edward O. Thorp.
 
-    :param win_probability: (float) This is probability of a win of your strategy.
-    :param profit: (float) profit per unit bet.
-    :param loss: (float) loss per unit bet.
-    :return: (kelly_bet_size, expected_growth_rate) (tuple[float, float])
-            kelly_bet_size: (float) optimal bet size from kelly criterion.
-            expected_growth_rate: (float) (expected) growth rate of balance(capital) when you bet as much as kelly bet
-            size.
+    :param win_probability: (float) Probability of win for a strategy.
+    :param profit_unit: (float) Profit per unit bet.
+    :param loss_unit: (float) Loss per unit bet.
+    :return: (float, float) The first element is the optimal bet size from the Kelly criterion. The second is the
+    expected compound growth rate of balance(capital) when the bet is the Kelly bet size.
     """
     loss_probability = 1 - win_probability
 
@@ -42,27 +40,26 @@ def kelly_betting(win_probability: float, profit_unit: float, loss_unit: float) 
     return kelly_bet_size, expected_growth_rate
 
 
-def kelly_investing(returns: np.ndarray, risk_free_rate: float, annualize_factor: int) -> Tuple[float, float]:
+def kelly_investing(returns: np.ndarray, risk_free_rate: float, annualize_factor: int, raw_return: bool = True) \
+        -> Tuple[float, float]:
     """
-    Calculates optimal leverage of investing on a single asset based on the kelly criterion.
+    Calculates optimal leverage of investing in a single asset based on the Kelly criterion.
 
     This method assumes our strategy is used for a single asset in the environment where there is not a finite number
-    of outcomes like investing on securities in the stock market.
+    of outcomes like investing in securities in the stock market.
 
     The implementation is based on the paper, 'The Kelly Criterion in blackjack, sports betting, and the stock market'
     by Edward O. Thorp.
 
-    :param returns: (np.ndarray) This is series of returns of your strategy or a asset.
-    :param risk_free_rate: (float) this is the annualized return on the remaining capital which is not invested on your
-    main strategy. The rate of the risk free asset such as treasury bill rate is usually used.
-    :param annualize_factor: (int) It is a scale factor to estimate annualized return of the strategy which depends on
-    the period of returns.
-
-    :return: (kelly_leverage, expected_growth_rate) (tuple[float, float])
-            kelly_leverage: (float) optimal leverage size from kelly criterion.
-            expected_growth_rate: (float) (expected) growth rate of balance(capital) when you invest as much as optimal
-            leverage calculated from kelly criterion.
+    :param returns: (np.ndarray) Returns of your strategy or a asset.
+    :param risk_free_rate: (float) Annual return of the risk-free asset.
+    :param annualize_factor: (int) Scale factor to estimate annualized return of the strategy or asset.
+    :param raw_return: (bool) Flag whether returns are raw return or log return.
+    :return: (float, float) The first element is the optimal leverage from the Kelly criterion. The second is the
+    expected compound growth rate of balance(capital) from the Kelly criterion.
     """
+    if raw_return:
+        returns = np.log(1+returns)
     mean = np.mean(returns) * annualize_factor
     var = np.var(returns, ddof=1) * annualize_factor
 
@@ -74,27 +71,26 @@ def kelly_investing(returns: np.ndarray, risk_free_rate: float, annualize_factor
     return kelly_leverage, expected_growth_rate
 
 
-def kelly_allocation(returns: pd.DataFrame, risk_free_rate: float, annualize_factor: int) -> Tuple[np.ndarray, float]:
+def kelly_allocation(returns: pd.DataFrame, risk_free_rate: float, annualize_factor: int, raw_return: bool = True) \
+        -> Tuple[np.ndarray, float]:
     """
-    Calculates optimal leverage among the multiple assets or strategies based on the kelly criterion.
+    Calculates optimal leverage among the multiple assets or strategies based on the Kelly criterion.
 
     The implementation is based on the paper, 'The Kelly Criterion in blackjack, sports betting, and the stock market'
     by Edward O. Thorp.
 
-    :param returns: (pd.DataFrame) 2-d array of returns. The columns are assets(strategies) and rows are time.
-    :param risk_free_rate: (float) this is the annualized return on the remaining capital which is not invested on your
-    main strategy. The rate of the risk free asset such as treasury bill rate is usually used.
-    :param annualize_factor: (int) It is a scale factor to estimate annualized return of the strategy which depends on
-    the period of returns.
-
-    :return: (fractions, expected_growth_rate) (tuple[float, float])
-            fractions: (np.ndarray) An array which contains optimal fraction(leverage) of each asset(strategy)
-            from kelly criterion.
-            expected_growth_rate: (float) (expected) growth rate of balance(capital) when you distribute your wealth
-            according to kelly criterion.
+    :param returns: (pd.DataFrame) Dataframe where each column is a series of returns for an asset(strategy).
+    :param risk_free_rate: (float) Annual return of the risk-free asset.
+    :param annualize_factor: (int) Scale factor to estimate annualized return of the strategy or asset.
+    :param raw_return: (bool) Flag whether returns are raw return or log return.
+    :return: (np.ndarray, float) The first element is fractions(leverage) of each asset(strategy) from the Kelly
+    criterion. The second is the expected compound growth rate of balance(capital) by the Kelly criterion.
     """
-    means = returns.mean()*annualize_factor
-    cov_matrix = returns.cov()*annualize_factor
+    if raw_return:
+        returns = np.log(1+returns)
+
+    means = returns.mean() * annualize_factor
+    cov_matrix = returns.cov() * annualize_factor
 
     fractions = np.dot(inv(cov_matrix), means-risk_free_rate)
     expected_growth_rate = risk_free_rate + np.dot(fractions.T, cov_matrix.dot(fractions))/2
