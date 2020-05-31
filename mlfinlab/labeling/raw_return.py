7e24e2377c5924224b1a24f8160467589a090778
Ruifan Pei <ruifan.pei@gmail.com>
2020-05-30 20:40:18 -0400

    raw return

--- mlfinlab/labeling/raw_return.py
+++ mlfinlab/labeling/raw_return.py
@@ -1,4 +1,37 @@
+"""
+Labeling based on raw return of each observation relative to its previous value. User can specify how many steps back
+to calculate the raw return from.
+"""
+
+import numpy as np
+import warnings
 
 
-def raw_return():
-    return
+def raw_return(price, binary=False, logarithmic=False, lookback=1):
+    """
+    :param price: (pd.Series or pd.DataFrame) Price data for one (Series) or multiple tickers (DataFrame).
+    :param binary: (bool) If False, will return numerical returns. If True, will return the sign of the raw return.
+                    False by default.
+    :param logarithmic: (bool) If False, will calculate percentage returns. If True, will calculate logarithmic returns.
+                        False by default.
+    :param lookback: (int) Number of ticks back to calculate each observation's return from. 1 by default. The first
+                    lookback number of rows in the output will be NaN.
+    :return:(pd.Series or pd.DataFrame) Raw returns on market data. User can specify whether returns will be based on
+                percentage or logarithmic return, and whether the output will be numerical or categorical.
+    """
+    # Warning if lookback is greater than the number of rows
+    if lookback >= len(price):
+        warnings.warn('lookback period is greater than the length of the Series. All labels will be NaN.',
+                      UserWarning)
+
+    # Log returns or percentage returns
+    if logarithmic:
+        raw_returns = np.log(price) - np.log(price.shift(lookback))
+    if not logarithmic:
+        raw_returns = price.pct_change(lookback)
+
+    # Return sign only if categorical labels desired
+    if binary:
+        raw_returns = np.sign(raw_returns)
+
+    return raw_returns
--- mlfinlab/tests/test_labeling_raw_return.py
+++ mlfinlab/tests/test_labeling_raw_return.py
@@ -1,10 +1,58 @@
+# pylint: disable=missing-module-docstring
+
 import unittest
+import os
+import numpy as np
+import pandas as pd
+from mlfinlab.labeling.raw_return import raw_return
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestLabelingRawReturns(unittest.TestCase):
+    """
+    Tests for the raw returns labeling method.
+    """
 
+    def setUp(self):
+        """
+        Set the file path for the sample dollar bars data.
+        """
+        project_path = os.path.dirname(__file__)
+        self.path = project_path + '/test_data/stock_prices.csv'
+        self.data = pd.read_csv(self.path, index_col='Date')
+        self.idx5 = self.data[:5].index
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_series(self):
+        """
+        Verifies raw returns for a series for percentage/logarithmic returns, with numerical/binary labels.
+        """
+        # Takes series of 10 imaginary prices
+        price = pd.Series([100, 101, 102, 102, 102, 99, 19, 2000, 100, 105])
+
+        test1 = raw_return(price)
+        test2 = raw_return(price, binary=True)
+        test3 = raw_return(price, logarithmic=True)
+        test4 = raw_return(price, binary=True, logarithmic=True)
+        test5 = raw_return(price, binary=False, logarithmic=False, lookback=3)
+        test6 = raw_return(price, binary=False, logarithmic=True, lookback=4)
+        test1_actual = pd.Series([np.nan, 0.01, 0.009901, 0, 0, -0.029412, -0.808081, 104.263158, -0.95, 0.05])
+        test3_actual = pd.Series([np.nan, 0.00995, 0.009852, 0, 0, -0.029853, -1.650681, 4.656463, -2.995732, 0.048790])
+        test5_actual = pd.Series([np.nan, np.nan, np.nan, 0.02, 0.009901, -0.029412, -0.813725, 18.607843, 0.010101,
+                                  4.526316])
+        test6_actual = pd.Series([np.nan, np.nan, np.nan, np.nan, 0.019803, -0.020001, -1.680534, 2.975930, -0.019803,
+                                  0.058841])
+
+        # Check less precise for slight rounding differences between calculated and manually inputted values
+        pd.testing.assert_series_equal(test1, test1_actual, check_less_precise=True)
+        pd.testing.assert_series_equal(test2, np.sign(test1), check_less_precise=True)
+        pd.testing.assert_series_equal(test3, test3_actual, check_less_precise=True)
+        pd.testing.assert_series_equal(test4, np.sign(test3), check_less_precise=True)
+        pd.testing.assert_series_equal(test5, test5_actual, check_less_precise=True)
+        pd.testing.assert_series_equal(test6, test6_actual, check_less_precise=True)
+
+    def test_dataframe(self):
+        """
+        Verifies raw returns for a DataFrame.
+        """
+        price = self.data.iloc[0:5, 0:5]
+        print(price)
+        self.assertTrue(4==4)
