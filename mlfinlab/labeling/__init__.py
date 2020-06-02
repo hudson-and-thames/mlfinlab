83c49c0fda5d81c16bdcf521f552fef141392932
Ruifan Pei <ruifan.pei@gmail.com>
2020-06-02 15:02:19 -0400

    trailing newline

--- mlfinlab/labeling/__init__.py
+++ mlfinlab/labeling/__init__.py
@@ -8,4 +8,3 @@ from mlfinlab.labeling.trend_scanning import trend_scanning_labels
 from mlfinlab.labeling.tail_sets import TailSetLabels
 from mlfinlab.labeling.fixed_time_horizon import fixed_time_horizon
 from mlfinlab.labeling.excess_over_mean import excess_over_mean
-
