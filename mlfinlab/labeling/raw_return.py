7cbf579ac9c7fb234cbfff5e3d5e136e2c88f88a
Ruifan Pei <ruifan.pei@gmail.com>
2020-06-12 18:28:53 -0400

    added resample and forward and redid unit tests

--- mlfinlab/labeling/raw_return.py
+++ mlfinlab/labeling/raw_return.py
@@ -1,43 +1,51 @@
 """
+Labeling Raw Returns.
+
 Most basic form of labeling based on raw return of each observation relative to its previous value. User can specify
-percentage or logarithmic, numerical or categorical labels, and number of steps back to calculate the raw return from.
+simple or logarithmic returns, numerical or categorical labels, resampling periods, and whether to lag returns.
 """
 
 import warnings
 import numpy as np
 
 
-def raw_return(price, binary=False, logarithmic=False, lookback=1):
+def raw_return(prices, binary=False, logarithmic=False, resample_by=None, lag=False):
     """
     Raw returns labeling method.
 
     This is the most basic and ubiquitous labeling method used as a precursor to almost any kind of financial data
-    analysis or machine learning. User can specify percentage/logarithmic returns, numerical/binary labels, and
-    lookback period to calculate the return from.
+    analysis or machine learning. User can specify simple or logarithmic returns, numerical or binary labels, a
+    resample period, and whether returns are lagged to be forward looking.
 
-    :param price: (pd.Series or pd.DataFrame) Price data for one (Series) or multiple tickers (DataFrame).
+    :param prices: (pd.Series or pd.DataFrame) Time-indexed price data on stocks with which to calculate return.
     :param binary: (bool) If False, will return numerical returns. If True, will return the sign of the raw return.
-                    (False by default).
     :param logarithmic: (bool) If False, will calculate percentage returns. If True, will calculate logarithmic returns.
-                        (False by default).
-    :param lookback: (int) Lookback period to calculate each observation's return from. 1 by default. The first
-                    lookback number of rows in the output will be NaN.
+    :param resample_by: (str) If not None, the resampling period for price data prior to calculating returns. 'B' = per
+                        business day, 'W' = week, 'M' = month, etc. Will take the last observation for each period.
+                        For full details see `here.
+                        <https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects>`_
+    :param lag: (bool) If True, returns will be lagged to make them forward-looking.
     :return:  (pd.Series or pd.DataFrame) Raw returns on market data. User can specify whether returns will be based on
-                percentage or logarithmic return, and whether the output will be numerical or categorical.
+                simple or logarithmic return, and whether the output will be numerical or categorical.
     """
-    # Warning if lookback is greater than the number of rows
-    if lookback >= len(price):
-        warnings.warn('lookback period is greater than the length of the Series. All labels will be NaN.',
-                      UserWarning)
+    # Apply resample, if applicable.
+    if resample_by is not None:
+        prices = prices.resample(resample_by).last()
 
-    # Log returns or percentage returns
-    if logarithmic:
-        raw_returns = np.log(price) - np.log(price.shift(lookback))
-    if not logarithmic:
-        raw_returns = price.pct_change(lookback)
+    # Get return per period.
+    if logarithmic:  # Log returns
+        if lag:
+            returns = (np.log(prices) - np.log(prices.shift(1))).shift(-1)
+        else:
+            returns = np.log(prices) - np.log(prices.shift(1))
+    else:  # Simple returns
+        if lag:
+            returns = prices.pct_change(periods=1).shift(-1)
+        else:
+            returns = prices.pct_change(periods=1)
 
-    # Return sign only if categorical labels desired
+    # Return sign only if categorical labels desired.
     if binary:
-        raw_returns = raw_returns.apply(np.sign)
+        returns = returns.apply(np.sign)
 
-    return raw_returns
+    return returns
