73f04ecdd279286c6e9a4086a25b2111c64e2813
Alex Kwon <akwon@berkeley.edu>
2020-06-18 01:10:10 -0700

    Edit tests

--- mlfinlab/statistical_arbitrage/pca.py
+++ mlfinlab/statistical_arbitrage/pca.py
@@ -5,20 +5,34 @@ import pandas as pd
 import numpy as np
 
 
-def calc_all_pca():
+def calc_all_pca(data, num):
+    """
+    Calculate the
+
+    :param data: (pd.DataFrame) User given data.
+    :param num: (int) Number of top-principal components.
+    :return:
+    """
+    # Calculate the projection and eigenvector from the PCA of data.
+    data_proj, data_eigvec = calc_pca(data, num)
+
+    # Linear regression by matrix multiplication.
+    beta = np.linalg.inv(data_proj.T.dot(data_proj)).dot(data_proj.T).dot(np.array(data))
+
+    
     return
 
 
 def calc_pca(data, num):
     """
-    Calculates the PCA project of the data onto the n-top components.
+    Calculates the PCA projection of the data onto the num-top components.
 
     :param data: (np.array) Data to be projected.
     :param num: (int) Number of top-principal components.
     :return: (tuple) (np.array) Projected data, (np.array) Eigenvectors
     """
     # Standardize the data.
-    data = data - data.mean(axis=0) / np.std(data, axis=0)
+    data = (data - data.mean(axis=0)) / np.std(data, axis=0)
 
     # Calculate the covariance matrix.
     cov = data.T.dot(data) / data.shape[0]
--- mlfinlab/tests/test_pca.py
+++ mlfinlab/tests/test_pca.py
@@ -5,6 +5,7 @@ Test PCA for Statistical Arbitrage module.
 import unittest
 import os
 import pandas as pd
+import numpy as np
 
 from mlfinlab.statistical_arbitrage import calc_pca
 
@@ -30,7 +31,7 @@ class TestPCA(unittest.TestCase):
         Test PCA calculation.
         """
         # Calculate PCA for given data with 1 principal component.
-        res = calc_pca(self.data, 1)
+        res = calc_pca(np.log(self.data), 1)
 
         # There are two items in the tuple.
         self.assertEqual(len(res), 2)
@@ -39,14 +40,14 @@ class TestPCA(unittest.TestCase):
         self.assertEqual(len(res[0]), 2141)
 
         # Check some values for projection.
-        self.assertAlmostEqual(res[0][5][0], 312.28395, delta=1e-3)
-        self.assertAlmostEqual(res[0][100][0], 310.16131, delta=1e-3)
-        self.assertAlmostEqual(res[0][1000][0], 292.664276, delta=1e-3)
+        self.assertAlmostEqual(res[0][5][0], -3.719554, delta=1e-3)
+        self.assertAlmostEqual(res[0][100][0], -3.56338343, delta=1e-3)
+        self.assertAlmostEqual(res[0][1000][0], 2.6786415, delta=1e-3)
 
         # Check the length of the second output.
         self.assertEqual(len(res[1]), 23)
 
         # Check some values for the first principal component.
-        self.assertAlmostEqual(res[1][4][0], 0.161186943, delta=1e-3)
-        self.assertAlmostEqual(res[1][9][0], 0.20690915, delta=1e-3)
-        self.assertAlmostEqual(res[1][22][0], 0.4180378, delta=1e-3)
+        self.assertAlmostEqual(res[1][4][0], -0.24159355, delta=1e-3)
+        self.assertAlmostEqual(res[1][9][0], -0.233308519, delta=1e-3)
+        self.assertAlmostEqual(res[1][22][0], -0.2389528258, delta=1e-3)
