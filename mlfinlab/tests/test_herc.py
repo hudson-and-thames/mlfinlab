cfcb7e1d34c540e050cbbfd13cf8e79a4d5cf879
Illya Barziy <panpipp@gmail.com>
2020-06-22 11:16:01 +0200

    Change test names

--- mlfinlab/tests/test_herc.py
+++ mlfinlab/tests/test_herc.py
@@ -24,50 +24,50 @@ class TestHERC(unittest.TestCase):
         data_path = project_path + '/test_data/stock_prices.csv'
         self.data = pd.read_csv(data_path, parse_dates=True, index_col="Date")
 
-    def test_hcaa_equal_weight(self):
+    def test_herc_equal_weight(self):
         """
         Test the weights calculated by the HERC algorithm - if all the weights are positive and
         their sum is equal to 1.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       asset_names=self.data.columns,
                       optimal_num_clusters=5,
                       risk_measure='equal_weighting')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
 
-    def test_hcaa_min_variance(self):
+    def test_herc_min_variance(self):
         """
         Test the weights calculated by the HERC algorithm - if all the weights are positive and
         their sum is equal to 1.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       asset_names=self.data.columns,
                       optimal_num_clusters=5,
                       risk_measure='variance')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
 
-    def test_hcaa_min_standard_deviation(self):
+    def test_herc_min_standard_deviation(self):
         """
         Test the weights calculated by the HERC algorithm - if all the weights are positive and
         their sum is equal to 1.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       asset_names=self.data.columns,
                       optimal_num_clusters=4,
                       risk_measure='standard_deviation')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
@@ -79,39 +79,39 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
-            hcaa.allocate(asset_names=self.data.columns,
+            herc = HierarchicalEqualRiskContribution()
+            herc.allocate(asset_names=self.data.columns,
                           optimal_num_clusters=5,
                           risk_measure='expected_shortfall')
 
-    def test_hcaa_expected_shortfall(self):
+    def test_herc_expected_shortfall(self):
         """
         Test the weights calculated by the HERC algorithm - if all the weights are positive and
         their sum is equal to 1.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       asset_names=self.data.columns,
                       optimal_num_clusters=5,
                       risk_measure='expected_shortfall')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
 
-    def test_hcaa_conditional_drawdown_risk(self):
+    def test_herc_conditional_drawdown_risk(self):
         """
         Test the weights calculated by the HERC algorithm - if all the weights are positive and
         their sum is equal to 1.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       asset_names=self.data.columns,
                       optimal_num_clusters=5,
                       risk_measure='conditional_drawdown_risk')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
@@ -121,12 +121,12 @@ class TestHERC(unittest.TestCase):
         Test the quasi-diagnalisation step of HERC algorithm.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       linkage='single',
                       optimal_num_clusters=5,
                       asset_names=self.data.columns)
-        assert hcaa.ordered_indices == [13, 9, 10, 8, 14, 7, 1, 6, 4, 16, 3, 17,
+        assert herc.ordered_indices == [13, 9, 10, 8, 14, 7, 1, 6, 4, 16, 3, 17,
                                         12, 18, 22, 0, 15, 21, 11, 2, 20, 5, 19]
 
     def test_value_error_for_non_dataframe_input(self):
@@ -135,8 +135,8 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
-            hcaa.allocate(asset_prices=self.data.values, asset_names=self.data.columns)
+            herc = HierarchicalEqualRiskContribution()
+            herc.allocate(asset_prices=self.data.values, asset_names=self.data.columns)
 
     def test_value_error_for_non_date_index(self):
         """
@@ -144,9 +144,9 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
+            herc = HierarchicalEqualRiskContribution()
             data = self.data.reset_index()
-            hcaa.allocate(asset_prices=data, asset_names=self.data.columns)
+            herc.allocate(asset_prices=data, asset_names=self.data.columns)
 
     def test_all_inputs_none(self):
         """
@@ -154,50 +154,50 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
-            hcaa.allocate(asset_names=self.data.columns)
+            herc = HierarchicalEqualRiskContribution()
+            herc.allocate(asset_names=self.data.columns)
 
-    def test_hcaa_with_input_as_returns(self):
+    def test_herc_with_input_as_returns(self):
         """
         Test HERC when passing asset returns dataframe as input.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
+        herc = HierarchicalEqualRiskContribution()
         returns = ReturnsEstimators().calculate_returns(asset_prices=self.data)
-        hcaa.allocate(asset_returns=returns, asset_names=self.data.columns)
-        weights = hcaa.weights.values[0]
+        herc.allocate(asset_returns=returns, asset_names=self.data.columns)
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
 
-    def test_hcaa_with_asset_returns_as_none(self):
+    def test_herc_with_asset_returns_as_none(self):
         """
         Test HERC when asset returns are not required for calculating the weights.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
+        herc = HierarchicalEqualRiskContribution()
         returns = ReturnsEstimators().calculate_returns(asset_prices=self.data)
-        hcaa.allocate(asset_names=self.data.columns,
+        herc.allocate(asset_names=self.data.columns,
                       covariance_matrix=returns.cov(),
                       optimal_num_clusters=5,
                       risk_measure='equal_weighting')
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
 
-    def test_hcaa_with_input_as_covariance_matrix(self):
+    def test_herc_with_input_as_covariance_matrix(self):
         """
         Test HERC when passing a covariance matrix as input.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
+        herc = HierarchicalEqualRiskContribution()
         returns = ReturnsEstimators().calculate_returns(asset_prices=self.data)
-        hcaa.allocate(asset_names=self.data.columns,
+        herc.allocate(asset_names=self.data.columns,
                       covariance_matrix=returns.cov(),
                       optimal_num_clusters=6,
                       asset_returns=returns)
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
@@ -208,18 +208,18 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
-            hcaa.allocate(asset_names=self.data.columns, asset_prices=self.data, risk_measure='random_metric')
+            herc = HierarchicalEqualRiskContribution()
+            herc.allocate(asset_names=self.data.columns, asset_prices=self.data, risk_measure='random_metric')
 
     def test_no_asset_names(self):
         """
         Test HERC when not supplying a list of asset names.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data,
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data,
                       optimal_num_clusters=6)
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
@@ -229,11 +229,11 @@ class TestHERC(unittest.TestCase):
         Test HERC when not supplying a list of asset names and when the user passes asset_returns.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
+        herc = HierarchicalEqualRiskContribution()
         returns = ReturnsEstimators().calculate_returns(asset_prices=self.data)
-        hcaa.allocate(asset_returns=returns,
+        herc.allocate(asset_returns=returns,
                       optimal_num_clusters=6)
-        weights = hcaa.weights.values[0]
+        weights = herc.weights.values[0]
         assert (weights >= 0).all()
         assert len(weights) == self.data.shape[1]
         np.testing.assert_almost_equal(np.sum(weights), 1)
@@ -244,9 +244,9 @@ class TestHERC(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            hcaa = HierarchicalEqualRiskContribution()
+            herc = HierarchicalEqualRiskContribution()
             returns = ReturnsEstimators().calculate_returns(asset_prices=self.data)
-            hcaa.allocate(asset_returns=returns.values,
+            herc.allocate(asset_returns=returns.values,
                           optimal_num_clusters=6)
 
     def test_dendrogram_plot(self):
@@ -254,9 +254,9 @@ class TestHERC(unittest.TestCase):
         Test if dendrogram plot object is correctly rendered.
         """
 
-        hcaa = HierarchicalEqualRiskContribution()
-        hcaa.allocate(asset_prices=self.data, optimal_num_clusters=5)
-        dendrogram = hcaa.plot_clusters(assets=self.data.columns)
+        herc = HierarchicalEqualRiskContribution()
+        herc.allocate(asset_prices=self.data, optimal_num_clusters=5)
+        dendrogram = herc.plot_clusters(assets=self.data.columns)
         assert dendrogram.get('icoord')
         assert dendrogram.get('dcoord')
         assert dendrogram.get('ivl')
