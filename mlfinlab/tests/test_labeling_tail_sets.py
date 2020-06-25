878778adc572cc46c611019b7cab080b141f4751
Ruifan Pei <ruifan.pei@gmail.com>
2020-06-25 17:16:57 -0400

    pylint errs and update doc

--- mlfinlab/tests/test_labeling_tail_sets.py
+++ mlfinlab/tests/test_labeling_tail_sets.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-module-docstring
+# pylint: disable=protected-access
 
 import unittest
 import os
@@ -68,7 +69,7 @@ class TestTailSets(unittest.TestCase):
         label4 = TailSetLabels(self.data[:100], n_bins=4, vol_adj='mean_abs_dev', window=20)
         returns4 = label4.vol_adj_rets
         test4 = label4._extract_tail_sets(row=returns4.iloc[25])
-        test4_actual = pd.Series([0, 0, 1, -1, 0, 1, 0, 0, -1, -1, 1, 0, 0, 0, -1, 0, 0, -1,  0,  1,  1,  1, -1],
+        test4_actual = pd.Series([0, 0, 1, -1, 0, 1, 0, 0, -1, -1, 1, 0, 0, 0, -1, 0, 0, -1, 0, 1, 1, 1, -1],
                                  index=returns4.iloc[25].index)
         test5 = label4._positive_tail_set(test4)
         test5_actual = ['TIP', 'IEF', 'XLF', 'TLT', 'BND', 'CSJ']
@@ -83,9 +84,7 @@ class TestTailSets(unittest.TestCase):
         Tests the overall output of the tail set labels.
         """
         label7 = TailSetLabels(self.data[:100], n_bins=10, vol_adj='mean_abs_dev', window=30)
-        test7_pos, test7_neg, test7_mat = label7.get_tail_sets()
+        test7_pos, test7_neg, _ = label7.get_tail_sets()
 
         self.assertEqual(test7_pos[1], ['XLU', 'EPP', 'FXI'])
         self.assertEqual(test7_neg[1], ['EWU', 'XLK', 'DIA'])
-
-
