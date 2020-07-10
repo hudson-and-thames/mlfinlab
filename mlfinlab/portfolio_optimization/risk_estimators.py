Removed: param num_facts: (float) Threshold for eigenvalues to be fixed. since num_facts is used by _denoised_corr(..) to "Replac[e] eigenvalues after num_facts to their average value" - therefore preserving the trace but removing noice. Since _detone_corr(..) doesnt denoise anymore - its not needed.

mlfinlab/portfolio_optimization/risk_estimators.py
@@ -344,7 +344,7 @@ class RiskEstimators:
 
         # Detone the correlation matrix if needed
         if detone:
-            corr = self._detoned_corr(corr, eigenval, eigenvec, num_facts, market_component)
+            corr = self._detoned_corr(corr, eigenval, eigenvec, market_component)
 
         # Calculating the covariance matrix from the de-noised correlation matrix
         cov_denoised = self.corr_to_cov(corr, np.diag(cov) ** (1 / 2))
@@ -618,7 +618,7 @@ class RiskEstimators:
 
         return corr
 
-    def _detoned_corr(self, corr, eigenvalues, eigenvectors, num_facts, market_component=1):
+    def _detoned_corr(self, corr, eigenvalues, eigenvectors, market_component=1):
         """
         De-tones the correlation matrix by removing the market component.
 
@@ -629,7 +629,6 @@ class RiskEstimators:
         :param corr: (np.array) Correlation matrix to detone.
         :param eigenvalues: (np.array) Matrix with eigenvalues on the main diagonal.
         :param eigenvectors: (float) Eigenvectors array.
-        :param num_facts: (float) Threshold for eigenvalues to be fixed.
         :param market_component: (int) Number of fist eigevectors related to a market component. (1 by default)
         :return: (np.array) De-toned correlation matrix.
         """

mlfinlab/tests/test_risk_estimators.py
@@ -276,7 +276,7 @@ class TestRiskEstimators(unittest.TestCase):
                                   [0.39391882, 0.6897809, 1]])
         
         # Finding the de-toned correlation matrix
-        corr_matrix = risk_estimators._detoned_corr(corr, eigenvalues, eigenvectors, 1)
+        corr_matrix = risk_estimators._detoned_corr(corr, eigenvalues, eigenvectors)
 
         # Testing if the de-toned correlation matrix is right
         np.testing.assert_almost_equal(corr_matrix, expected_corr, decimal=4)