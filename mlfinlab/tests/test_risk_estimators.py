ca9c2638c819261741588c26680736d5e79c98eb
Illya Barziy <panpipp@gmail.com>
2020-07-13 12:28:32 +0200

    Remove unused unit test part

--- mlfinlab/tests/test_risk_estimators.py
+++ mlfinlab/tests/test_risk_estimators.py
@@ -257,19 +257,6 @@ class TestRiskEstimators(unittest.TestCase):
                          [0.1, 1, -0.3],
                          [-0.1, -0.3, 1]])
 
-        e_val, e_vec = np.linalg.eigh(corr)
-
-        # Eigenvalues and eigenvectors to use
-        eigenvalues = np.array([[1.3562, 0, 0],
-                                [0, 0.9438, 0],
-                                [0, 0, 0.7]])
-        eigenvectors = np.array([[-3.69048184e-01, -9.29410263e-01, 1.10397126e-16],
-                                 [-6.57192300e-01, 2.60956474e-01, 7.07106781e-01],
-                                 [6.57192300e-01, -2.60956474e-01, 7.07106781e-01]])
-
-        np.testing.assert_almost_equal(np.diag(np.flip(e_val)), eigenvalues, decimal=4)
-        np.testing.assert_almost_equal(np.fliplr(e_vec), eigenvectors, decimal=4)
-
         # Expected correlation matrix
         expected_corr = np.array([[1, -0.39391882, 0.39391882],
                                   [-0.39391882, 1, 0.6897809],
