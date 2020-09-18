f2fa5ae6b99a8bca3ef07e3493d2dfb910b83da1
Illya Barziy <panpipp@gmail.com>
2020-09-18 20:03:37 +0200

    Update OU-model code

--- mlfinlab/optimal_mean_reversion/ou_model.py
+++ mlfinlab/optimal_mean_reversion/ou_model.py
@@ -35,21 +35,37 @@ class OrnsteinUhlenbeck:
 
         pass
 
-    def fit(self, data, data_frequency, discount_rate, transaction_cost,
+    def fit(self, data, data_frequency, discount_rate, transaction_cost, start=None, end=None,
             stop_loss=None):
         """
         Fits the Ornstein-Uhlenbeck model to given data and assigns the discount rates,
         transaction costs and stop-loss level for further exit or entry-level calculation.
 
+        :param data: (np.array/pd.DataFrame) An array with time series of portfolio prices / An array with
+            time series of of two assets prices. The dimensions should be either nx1 or nx2.
         :param data_frequency: (str) Data frequency ["D" - daily, "M" - monthly, "Y" - yearly].
-        :param data: (np.array) An array with time series of portfolio prices / An array with
-            time series of of two assets prices.
         :param discount_rate: (float/tuple) A discount rate either for both entry and exit time
             or a list/tuple of discount rates with exit rate and entry rate in respective order.
         :param transaction_cost: (float/tuple) A transaction cost either for both entry and exit time
             or a list/tuple of transaction costs with exit cost and entry cost in respective order.
+        :param start: (Datetime) A date from which you want your training data to start.
+        :param end: (Datetime) A date at which you want your training data to end.
         :param stop_loss: (float/int) A stop-loss level - the position is assumed to be closed
             immediately upon reaching this pre-defined price level.
+        """
+
+        pass
+
+    def _fit_data(self, start, end):
+        """
+        Allocates all the training data parameters
+        and fits the OU model to this data.
+
+        Helper function used in self.fit().
+
+        :param start: (Datetime) A date from which you want your training data to start.
+        :param end: (Datetime) A date at which you want your training data to end.
+        :return: (np.array) An array of prices to construct a portfolio from.
         """
 
         pass
@@ -57,7 +73,7 @@ class OrnsteinUhlenbeck:
     @staticmethod
     def _fit_rate_cost(input_data):
         """
-        Function that sets the value for cost and rate parameters.
+        Sets the value for cost and rate parameters.
 
         Helper function used in self.fit().
 
@@ -69,7 +85,7 @@ class OrnsteinUhlenbeck:
 
     def _fit_delta(self, data_frequency):
         """
-        Function that sets the value of the delta-t parameter,
+        Sets the value of the delta-t parameter,
         depending on data frequency input.
 
         Helper function used in self.fit().
@@ -80,12 +96,14 @@ class OrnsteinUhlenbeck:
 
         pass
 
-    def fit_to_portfolio(self, portfolio):
+    def fit_to_portfolio(self, data=None, start=None, end=None):
         """
-        Function that fits the Ornstein-Uhlenbeck model to time series
+        Fits the Ornstein-Uhlenbeck model to time series
         for portfolio prices.
 
-        :param portfolio: (np.array) Portfolio prices.
+        :param data: (np.array) All given prices of two assets to construct a portfolio from.
+        :param start: (Datetime) A date from which you want your training data to start.
+        :param end: (Datetime) A date at which you want your training data to end.
         """
 
         pass
@@ -105,38 +123,29 @@ class OrnsteinUhlenbeck:
 
         pass
 
-    def fit_to_assets(self, prices):
+    def def fit_to_assets(self, data=None, start=None, end=None):
         """
         Creates the optimal portfolio in terms of Ornstein-Uhlenbeck model
         from two given time series for asset prices and fits the values
         of the model's parameters. (p.13)
 
-        :param prices: (np.array) Prices of two assets to construct a portfolio from.
+        :param data: (np.array) All given prices of two assets to construct a portfolio from.
+        :param start: (Datetime) A date from which you want your training data to start.
+        :param end: (Datetime) A date at which you want your training data to end.
         """
 
         pass
 
-    def plot_levels(self, data, stop_loss=False):
+    def ou_model_simulation(self, n, theta_given=None, mu_given=None, sigma_given=None, delta_t_given=None):
         """
-        plot_levelss the found optimal exit and entry levels on the graph
-        alongside with the given data.
+        Simulates values of an OU process with given parameters or parameters fitted to our data
 
-        :param data: (np.array) An array with time series of portfolio prices / An array with
-            time series of of two assets prices.
-        :param stop_loss: (bool) A flag whether to take stop-loss level into account.
-            when showcasing the results.
-        """
-
-        pass
-
-    @staticmethod
-    def _compute_log_likelihood(params, *args):
-        """
-        Computes the average Log Likelihood. (p.13)
-
-        :params: (tuple) A tuple of three elements representing theta, mu and sigma_squared.
-        :args: (tuple) A
-        :returns: (float) The average log likelihood from given parameters.
+        :param n: (int) Number of simulated values.
+        :param theta_given: (float) Long-term mean.
+        :param mu_given: (float) Mean revesion speed.
+        :param sigma_given: (float) The amplitude of randomness in the system.
+        :param delta_t_given: (float) Delta between observations, calculated in years.
+        :return: (np.array) simulated portfolio prices.
         """
 
         pass
@@ -147,8 +156,61 @@ class OrnsteinUhlenbeck:
         on the portfolio prices time series given.(p.13)
 
         :param portfolio: (np.array) Portfolio prices.
-        :return: (tuple) Optimal parameters (theta, mu, sigma_square).
-            and max_LL function value.
+        :return: (tuple) Optimal parameters (theta, mu, sigma_square, max_LL)
+        """
+
+        pass
+
+
+    def check_fit(self):
+        """
+        Shows the parameters of the OU model fitted to the data and parameters achieved
+        from the simulated process with the fitted coefficients for further assessment. The main
+        incentive is to have simulated max log-likelihood (mll) function close to a fitted one.
+        If the simulated mll is much greater it means that data provided to
+        the model is not good enough to be modeled by an OU process.
+
+        Note: The check can occasionally return numbers significantly different from the fitted mll
+        due to the random nature of the simulated process and the possibility of outliers. It is advised
+        to perform the check multiple times and if it's consistently showing very different values of mll
+
+        you might suspect the unsuitability of your data.
+        :return: (pd.Dataframe) Values for fitted and simulated OU model parameters
+        """
+
+        pass
+
+    @staticmethod
+    def _compute_log_likelihood(params, *args):
+        """
+        Computes the average Log Likelihood. (p.13)
+
+        :params: (tuple) A tuple of three elements representing theta, mu and sigma_squared.
+        :args: (tuple) All other values that to be passed to self._compute_log_likelihood()
+        :returns: (float) The average log likelihood from given parameters.
+        """
+
+        pass
+
+    def description(self):
+        """
+        Returns all the general parameters of the model, training interval timestamps if provided, the goodness of fit,
+        allocated trading costs and discount rates, stop-loss level, beta, which stands for the optimal ratio between
+        two assets in the created portfolio, and optimal levels calculated. If the stop-loss level was given optimal
+        levels that account for stop-loss would be added to the list.
+
+        :return: (pd.Series) Summary data for all model parameters and optimal levels.
+        """
+
+    def plot_levels(self, data, stop_loss=False):
+        """
+        Plots the found optimal exit and entry levels on the graph
+        alongside with the given data.
+
+        :param data: (np.array) An array with time series of portfolio prices / An array with
+            time series of of two assets prices.
+        :param stop_loss: (bool) A flag whether to take stop-loss level into account.
+            when showcasing the results.
         """
 
         pass
@@ -317,14 +379,3 @@ class OrnsteinUhlenbeck:
 
         pass
 
-    def description(self):
-        """
-        Returns all the general parameters of the model, allocated trading costs and discount rates,
-        stp-loss level, beta, which stands for the optimal ratio between two assets in created portfolio,
-        and optimal levels calculated. If the stop-loss level was given optimal levels that account for stop-loss
-        would be added to the list.
-
-        :return: (pd.Series) Summary data for all model parameters and optimal levels.
-        """
-
-        pass
