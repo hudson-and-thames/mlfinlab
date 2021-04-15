bfe33569774ffe4b9e72e57f787c76492cb1f08c
PanPip <panpipp@gmail.com>
2021-04-15 20:27:13 +0200

    Removed PortOpt and OLPS modules code

--- mlfinlab/portfolio_optimization/estimators/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Classes derived from the Estimators sub-module of Portfolio Optimisation.
-"""
-
-from mlfinlab.portfolio_optimization.estimators.returns_estimators import ReturnsEstimators
-from mlfinlab.portfolio_optimization.estimators.risk_estimators import RiskEstimators
-from mlfinlab.portfolio_optimization.estimators.tic import TheoryImpliedCorrelation
